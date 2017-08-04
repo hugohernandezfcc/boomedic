86095f1ba2335eaae84101f6f80004206017599a
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2017-08-04 08:21:29 -0500

    Solucionando http

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,17 +6,14 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-     /**
+    /**
      * Bootstrap any application services.
      *
      * @return void
      */
-    public function boot(UrlGenerator $url)
+    public function boot()
     {
-        if(env('APP_ENV') !== 'local')
-        {
-            $url->forceSchema('https');
-        }
+        //
     }
 
     /**
