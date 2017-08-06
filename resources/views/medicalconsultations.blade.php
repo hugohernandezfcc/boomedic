9a1837cb026f672d73854cf2e414f560d4afc505
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2017-08-06 02:52:25 -0500

    Parametros

--- resources/views/medicalconsultations.blade.php
+++ resources/views/medicalconsultations.blade.php
@@ -3,9 +3,9 @@
 @section('title', 'AdminLTE')
 
 @section('content_header')
-    <h1>{{KEY}}</h1>
+    <h1>{{$KEY}}</h1>
 @stop
 
 @section('content')
-    <p>space::29i320e0329ie20ei2039ei203e9i2309ei203e</p>
+    <p>The current UNIX timestamp is {{ time() }}.</p>
 @stop
