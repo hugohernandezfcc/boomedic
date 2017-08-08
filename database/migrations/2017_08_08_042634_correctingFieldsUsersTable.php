d6582f64e24b3d9b2effa8e6a0e30643f0726441
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2017-08-07 23:33:06 -0500

    Corrigiendo etiqueta de campo

--- database/migrations/2017_08_08_042634_correctingFieldsUsersTable.php
+++ database/migrations/2017_08_08_042634_correctingFieldsUsersTable.php
@@ -14,8 +14,7 @@ class CorrectingFieldsUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->renameColumn('dateBirth', 'birthDate');
-
+            $table->dropColumn('dateBirth');
         });
     }
 
