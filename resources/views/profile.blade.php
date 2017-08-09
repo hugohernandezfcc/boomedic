486f1bf3244a7553e968b36bd3b42892b9baebc2
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2017-08-09 12:40:02 -0500

    agregando mapa

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -110,41 +110,41 @@
               	<div class="callout callout-default" align="right">
 	                <b>Dirección</b>
 	            </div>
-              	<div id="locationField" class="col-sm-10">
-			      	<input id="autocomplete" class="form-control" placeholder="Ingresa tu dirección" onFocus="geolocate()" type="text"></input>
-			    </div>
+	            <div class="form-group">
+	            	<label for="autocomplete" class="col-sm-2 control-label">
+	            		<i class="fa fa-location-arrow"></i>
+	            	</label>
+	            	<div id="locationField" class="col-sm-10">
+				      	<input id="autocomplete" class="form-control" placeholder="Ingresa tu dirección" onFocus="geolocate()" type="text"></input>
+				    </div>
+	            </div>
 
-			    <table id="address">
-			      <tr>
-			        <td class="label">Street address</td>
-			        <td class="slimField"><input class="field" id="street_number"
-			              disabled="true"></input></td>
-			        <td class="wideField" colspan="2"><input class="field" id="route"
-			              disabled="true"></input></td>
-			      </tr>
-			      <tr>
-			        <td class="label">City</td>
-			        <!-- Note: Selection of address components in this example is typical.
-			             You may need to adjust it for the locations relevant to your app. See
-			             https://developers.google.com/maps/documentation/javascript/examples/places-autocomplete-addressform
-			        -->
-			        <td class="wideField" colspan="3"><input class="field" id="locality"
-			              disabled="true"></input></td>
-			      </tr>
-			      <tr>
-			        <td class="label">State</td>
-			        <td class="slimField"><input class="field"
-			              id="administrative_area_level_1" disabled="true"></input></td>
-			        <td class="label">Zip code</td>
-			        <td class="wideField"><input class="field" id="postal_code"
-			              disabled="true"></input></td>
-			      </tr>
-			      <tr>
-			        <td class="label">Country</td>
-			        <td class="wideField" colspan="3"><input class="field"
-			              id="country" disabled="true"></input></td>
-			      </tr>
-			    </table>
+	            <div class="row">
+	            	<div class="col-sm-6">
+	            		<input type="text" class="form-control" id="street_number"  placeholder="Número de calle" disabled="true"></input>
+	            	</div>
+	            	<div class="col-sm-6">
+	            		<input type="text" class="form-control" id="route" disabled="true">
+	            	</div>
+	            </div>
+              	
+              	<div class="row">
+	            	<div class="col-sm-6">
+	            		<input type="text" class="form-control" id="locality" disabled="true" placeholder="Ciudad"></input>
+	            	</div>
+	            	<div class="col-sm-6">
+	            		<input type="text" class="form-control" id="administrative_area_level_1" placeholder="Estado" disabled="true"></input>
+	            	</div>
+	            </div>
+
+	            <div class="row">
+	            	<div class="col-sm-6">
+	            		<input type="text" class="form-control" id="postal_code" disabled="true" placeholder="Código postal"></input>
+	            	</div>
+	            	<div class="col-sm-6">
+	            		<input type="text" class="form-control" id="country" placeholder="País" disabled="true"></input>
+	            	</div>
+	            </div>
     		</form>
 
     		<script>
