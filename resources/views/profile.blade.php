c8115d2ef23a5536090852b58c40474496d1fc05
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2017-08-09 13:07:55 -0500

    terminando formulario para insertar

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -235,6 +235,16 @@
 		          });
 		        }
 		      }
+
+
+		       $('form').on('keyup keypress', function(e) {
+		        	var keyCode = e.keyCode || e.which;
+		          	if (keyCode === 13) { 
+		            	e.preventDefault();
+		            	return false;
+		          	}
+		        });
+
 		    </script>
 
 
