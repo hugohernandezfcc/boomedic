c6e7ffd8c646b9f773f69ff8551a597aa28a7e6f
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2017-08-09 13:40:47 -0500

    intentando guardar formulario

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -22,7 +22,7 @@
 	            </div>
     		@endif
 
-    		<form action="user/update/{{$userId}}" method="post" class="form-horizontal">
+    		<form action="/user/update/{{$userId}}" method="post" class="form-horizontal">
     			{{ csrf_field() }}
 
     			<div class="form-group has-feedback {{ $errors->has('email') ? 'has-error' : '' }}">
