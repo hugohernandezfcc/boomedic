35a5278379eba44ac862bf03e4caf13db5420e8a
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2017-08-22 17:30:27 -0500

    profile view

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -15,166 +15,167 @@
 	  	</div>
 	  	<!-- /.box-header -->
 	  	<div class="box-body">
-	  		@if ($status == "In Progress")
-	  			<div class="callout callout-success">
-	                <h4>Ya casi estamos listos {{ $firstname }} !!!</h4>
+	  		@if( !empty($status) )
+	  		
+		  		@if ($status == "In Progress")
+		  			<div class="callout callout-success">
+		                <h4>Ya casi estamos listos {{ $firstname }} !!!</h4>
 
-	                <p>Confirma y completa la información que esta debajo</p>
-	            </div>
+		                <p>Confirma y completa la información que esta debajo</p>
+		            </div>
+	    		@endif
+
+	    	
+	    		<form action="/user/update/{{$userId}}" method="post" class="form-horizontal">
+	    			{{ csrf_field() }}
+
+	    			<div class="form-group has-feedback {{ $errors->has('email') ? 'has-error' : '' }}">
+	                    <label for="firstname" class="col-sm-2 control-label">Nombre</label>
+	                	<div class="col-sm-10">
+	                  		<input type="text" name="firstname" class="form-control" id="firstname" value="{{ $firstname }}">
+	                	</div>
+	              	</div>
+
+
+	              	<div class="form-group has-feedback {{ $errors->has('email') ? 'has-error' : '' }}">
+	                    <label for="lastname" class="col-sm-2 control-label">Apellidos</label>
+	                	<div class="col-sm-10">
+	                  		<input type="text" name="lastname" class="form-control" id="lastname" value="{{ $lastname }}">
+	                	</div>
+	              	</div>
+
+	              	<div class="form-group has-feedback {{ $errors->has('email') ? 'has-error' : '' }}">
+	                    <label for="email" class="col-sm-2 control-label">Corre electrónico</label>
+	                	<div class="col-sm-10">
+	                  		<input type="email" name="email" class="form-control" id="email" value="{{ $email }}">
+	                	</div>
+	              	</div>
+
+	              	<div class="form-group has-feedback {{ $errors->has('email') ? 'has-error' : '' }}">
+	                    <label for="username" class="col-sm-2 control-label">Nombre de usuario</label>
+	                	<div class="col-sm-10">
+	                  		<input type="email" name="username" class="form-control" id="username" value="{{ $username }}">
+	                	</div>
+	              	</div>
+
+	              	<div class="form-group has-feedback {{ $errors->has('email') ? 'has-error' : '' }}">
+	                    <label for="age" class="col-sm-2 control-label">Edad</label>
+	                	<div class="col-sm-10">
+	                  		<input type="text" name="age" class="form-control" id="age" value="{{ $age }}">
+	                	</div>
+	              	</div>
+
+	              	<div class="callout callout-default" align="right">
+		                <b>Información personal</b>
+		            </div>
+
+		            <div class="form-group has-feedback {{ $errors->has('occupation') ? 'has-error' : '' }}">
+	                    <label for="occupation" class="col-sm-2 control-label">Ocupación</label>
+	                	<div class="col-sm-10">
+	                  		<input type="text" name="occupation" class="form-control" id="occupation" value="{{ $occupation }}">
+	                	</div>
+	              	</div>
+	              	
+	              	<div class="form-group has-feedback {{ $errors->has('gender') ? 'has-error' : '' }}">
+	                  <label for="gender" class="col-sm-2 control-label">Genero</label>
+	                  <div class="col-sm-10">
+		                  <select class="form-control" name="gender">
+		                    <option value="female">Femenino</option>
+		                    <option value="male">Masculino</option>
+		                  </select>
+	                  </div>
+	                </div>
+
+	                <div class="form-group has-feedback {{ $errors->has('scholarship') ? 'has-error' : '' }}">
+	                    <label for="scholarship" class="col-sm-2 control-label">Escolaridad</label>
+	                	<div class="col-sm-10">
+	                  		<input type="text" name="scholarship" class="form-control" id="scholarship" value="{{ $scholarship }}">
+	                	</div>
+	              	</div>
+
+	              	<div class="form-group has-feedback {{ $errors->has('maritalstatus') ? 'has-error' : '' }}">
+	                  <label for="maritalstatus" class="col-sm-2 control-label">Estado civil</label>
+	                  <div class="col-sm-10">
+		                  <select class="form-control" name="maritalstatus">
+		                    <option value="female">Soltero</option>
+		                    <option value="male">Casado</option>
+		                  </select>
+	                  </div>
+	                </div>
+
+	                <div class="form-group">
+	                	<label for="mobile" class="col-sm-2 control-label"># Móvil</label>
+	                	<div class="col-sm-10">
+		                  	<input type="text" name="mobile" id="mobile" value="{{ $mobile }}" class="form-control" data-inputmask='"mask": "(999) 999-9999"' data-mask>
+	                	</div>
+	                	<!-- /.input group -->
+	              	</div>
+
+	              	<div class="callout callout-default" align="right">
+		                <b>Dirección</b>
+		            </div>
+		            <div class="form-group">
+		            	<label for="autocomplete" class="col-sm-2 control-label">
+		            		<i class="fa fa-location-arrow"></i>
+		            	</label>
+		            	<div id="locationField" class="col-sm-10">
+					      	<input id="autocomplete" class="form-control" placeholder="Ingresa tu dirección" onFocus="geolocate()" type="text"></input>
+					    </div>
+		            </div>
+
+		            <div align="right">
+		            	<div class="row" style="width: 90%;" >
+			            	<div class="col-sm-6">
+			            		<input type="text" class="form-control" name="street" id="street_number"  placeholder="Número de calle" disabled="true"></input>
+			            	</div>
+			            	<div class="col-sm-6">
+			            		<input type="text" class="form-control" name="colony" id="route" disabled="true">
+			            	</div>
+			            </div>
+						<br />              	
+		              	<div class="row" style="width: 90%;" >
+			            	<div class="col-sm-6">
+			            		<input type="text" class="form-control" name="delegation" id="locality" disabled="true" placeholder="Ciudad"></input>
+			            	</div>
+			            	<div class="col-sm-6">
+			            		<input type="text" class="form-control" name="state" id="administrative_area_level_1" placeholder="Estado" disabled="true"></input>
+			            	</div>
+			            </div>
+						<br />
+			            <div class="row" style="width: 90%;" >
+			            	<div class="col-sm-6">
+			            		<input type="text" class="form-control" name="postalcode" id="postal_code" disabled="true" placeholder="Código postal"></input>
+			            	</div>
+			            	<div class="col-sm-6">
+			            		<input type="text" class="form-control" name="country" id="country" placeholder="País" disabled="true"></input>
+			            	</div>
+			            </div>
+		            </div>
+
+		            <input type="hidden" name="latitude" id="latitudeFend" />
+		            <input type="hidden" name="longitude" id="longitudeFend" />
+
+		            <!-- /.box-body -->
+				  	<div class="box-footer">
+				    	<div class="row">
+				    		<div class="col-sm-4">
+				            	&nbsp;
+				            </div>
+				    		<div class="col-sm-4">
+					    		<button type="submit" class="btn btn-secondary btn-block btn-flat">
+					                Guardar
+					            </button>
+				            </div>
+				            <div class="col-sm-4">
+				            	&nbsp;
+				            </div>
+				    	</div>
+				  	</div>
+				  	<!-- box-footer -->
+
+	    		</form>
+
     		@endif
-
-
-
-
-    		<form action="/user/update/{{$userId}}" method="post" class="form-horizontal">
-    			{{ csrf_field() }}
-
-    			<div class="form-group has-feedback {{ $errors->has('email') ? 'has-error' : '' }}">
-                    <label for="firstname" class="col-sm-2 control-label">Nombre</label>
-                	<div class="col-sm-10">
-                  		<input type="text" name="firstname" class="form-control" id="firstname" value="{{ $firstname }}">
-                	</div>
-              	</div>
-
-
-              	<div class="form-group has-feedback {{ $errors->has('email') ? 'has-error' : '' }}">
-                    <label for="lastname" class="col-sm-2 control-label">Apellidos</label>
-                	<div class="col-sm-10">
-                  		<input type="text" name="lastname" class="form-control" id="lastname" value="{{ $lastname }}">
-                	</div>
-              	</div>
-
-              	<div class="form-group has-feedback {{ $errors->has('email') ? 'has-error' : '' }}">
-                    <label for="email" class="col-sm-2 control-label">Corre electrónico</label>
-                	<div class="col-sm-10">
-                  		<input type="email" name="email" class="form-control" id="email" value="{{ $email }}">
-                	</div>
-              	</div>
-
-              	<div class="form-group has-feedback {{ $errors->has('email') ? 'has-error' : '' }}">
-                    <label for="username" class="col-sm-2 control-label">Nombre de usuario</label>
-                	<div class="col-sm-10">
-                  		<input type="email" name="username" class="form-control" id="username" value="{{ $username }}">
-                	</div>
-              	</div>
-
-              	<div class="form-group has-feedback {{ $errors->has('email') ? 'has-error' : '' }}">
-                    <label for="age" class="col-sm-2 control-label">Edad</label>
-                	<div class="col-sm-10">
-                  		<input type="text" name="age" class="form-control" id="age" value="{{ $age }}">
-                	</div>
-              	</div>
-
-              	<div class="callout callout-default" align="right">
-	                <b>Información personal</b>
-	            </div>
-
-	            <div class="form-group has-feedback {{ $errors->has('occupation') ? 'has-error' : '' }}">
-                    <label for="occupation" class="col-sm-2 control-label">Ocupación</label>
-                	<div class="col-sm-10">
-                  		<input type="text" name="occupation" class="form-control" id="occupation" value="{{ $occupation }}">
-                	</div>
-              	</div>
-              	
-              	<div class="form-group has-feedback {{ $errors->has('gender') ? 'has-error' : '' }}">
-                  <label for="gender" class="col-sm-2 control-label">Genero</label>
-                  <div class="col-sm-10">
-	                  <select class="form-control" name="gender">
-	                    <option value="female">Femenino</option>
-	                    <option value="male">Masculino</option>
-	                  </select>
-                  </div>
-                </div>
-
-                <div class="form-group has-feedback {{ $errors->has('scholarship') ? 'has-error' : '' }}">
-                    <label for="scholarship" class="col-sm-2 control-label">Escolaridad</label>
-                	<div class="col-sm-10">
-                  		<input type="text" name="scholarship" class="form-control" id="scholarship" value="{{ $scholarship }}">
-                	</div>
-              	</div>
-
-              	<div class="form-group has-feedback {{ $errors->has('maritalstatus') ? 'has-error' : '' }}">
-                  <label for="maritalstatus" class="col-sm-2 control-label">Estado civil</label>
-                  <div class="col-sm-10">
-	                  <select class="form-control" name="maritalstatus">
-	                    <option value="female">Soltero</option>
-	                    <option value="male">Casado</option>
-	                  </select>
-                  </div>
-                </div>
-
-                <div class="form-group">
-                	<label for="mobile" class="col-sm-2 control-label"># Móvil</label>
-                	<div class="col-sm-10">
-	                  	<input type="text" name="mobile" id="mobile" value="{{ $mobile }}" class="form-control" data-inputmask='"mask": "(999) 999-9999"' data-mask>
-                	</div>
-                	<!-- /.input group -->
-              	</div>
-
-              	<div class="callout callout-default" align="right">
-	                <b>Dirección</b>
-	            </div>
-	            <div class="form-group">
-	            	<label for="autocomplete" class="col-sm-2 control-label">
-	            		<i class="fa fa-location-arrow"></i>
-	            	</label>
-	            	<div id="locationField" class="col-sm-10">
-				      	<input id="autocomplete" class="form-control" placeholder="Ingresa tu dirección" onFocus="geolocate()" type="text"></input>
-				    </div>
-	            </div>
-
-	            <div align="right">
-	            	<div class="row" style="width: 90%;" >
-		            	<div class="col-sm-6">
-		            		<input type="text" class="form-control" name="street" id="street_number"  placeholder="Número de calle" disabled="true"></input>
-		            	</div>
-		            	<div class="col-sm-6">
-		            		<input type="text" class="form-control" name="colony" id="route" disabled="true">
-		            	</div>
-		            </div>
-					<br />              	
-	              	<div class="row" style="width: 90%;" >
-		            	<div class="col-sm-6">
-		            		<input type="text" class="form-control" name="delegation" id="locality" disabled="true" placeholder="Ciudad"></input>
-		            	</div>
-		            	<div class="col-sm-6">
-		            		<input type="text" class="form-control" name="state" id="administrative_area_level_1" placeholder="Estado" disabled="true"></input>
-		            	</div>
-		            </div>
-					<br />
-		            <div class="row" style="width: 90%;" >
-		            	<div class="col-sm-6">
-		            		<input type="text" class="form-control" name="postalcode" id="postal_code" disabled="true" placeholder="Código postal"></input>
-		            	</div>
-		            	<div class="col-sm-6">
-		            		<input type="text" class="form-control" name="country" id="country" placeholder="País" disabled="true"></input>
-		            	</div>
-		            </div>
-	            </div>
-
-	            <input type="hidden" name="latitude" id="latitudeFend" />
-	            <input type="hidden" name="longitude" id="longitudeFend" />
-
-	            <!-- /.box-body -->
-			  	<div class="box-footer">
-			    	<div class="row">
-			    		<div class="col-sm-4">
-			            	&nbsp;
-			            </div>
-			    		<div class="col-sm-4">
-				    		<button type="submit" class="btn btn-secondary btn-block btn-flat">
-				                Guardar
-				            </button>
-			            </div>
-			            <div class="col-sm-4">
-			            	&nbsp;
-			            </div>
-			    	</div>
-			  	</div>
-			  	<!-- box-footer -->
-
-    		</form>
-
 
 
 
