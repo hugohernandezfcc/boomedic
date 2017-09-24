08376834488b210a498b176bbdf567025c97ea0e
PruebaHRK <pruebasHRK@gmail.com>
2017-09-24 13:53:52 -0500

    Table

--- database/migrations/2017_09_11_182349_create_labor_information.php
+++ database/migrations/2017_09_11_182349_create_labor_information.php
@@ -13,7 +13,11 @@ class CreateLaborInformation extends Migration
      */
     public function up()
     {
-        Schema::table('LaborInformation', function (Blueprint $table) {
+        Schema::create('LaborInformation', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('workplace');
+            $table->string('professionalPosition');
+
             $table->text('country')->nullable();
             $table->text('state')->nullable();
             $table->text('delegation')->nullable();
@@ -22,7 +26,10 @@ class CreateLaborInformation extends Migration
             $table->text('streetNumber')->nullable();
             $table->text('interiorNumber')->nullable();
             $table->text('phone')->nullable();
-            $table->dropColumn('address');
+
+            $table->integer('profInformation')->unsigned();
+            $table->foreign('profInformation')->references('id')->on('ProfessionalInformation');
+            $table->timestamps();
         });
     }
 
