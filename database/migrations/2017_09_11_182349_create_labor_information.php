4b79868f10c082582c947c3c9ee91bf89cef905d
PruebaHRK <pruebasHRK@gmail.com>
2017-09-23 23:59:57 -0500

    Update table

--- database/migrations/2017_09_11_182349_create_labor_information.php
+++ database/migrations/2017_09_11_182349_create_labor_information.php
@@ -14,11 +14,6 @@ class CreateLaborInformation extends Migration
     public function up()
     {
         Schema::table('LaborInformation', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('workplace');
-            $table->string('professionalPosition');
-            $table->string('address');
-
             $table->text('country')->nullable();
             $table->text('state')->nullable();
             $table->text('delegation')->nullable();
@@ -28,10 +23,6 @@ class CreateLaborInformation extends Migration
             $table->text('interiorNumber')->nullable();
             $table->text('phone')->nullable();
             $table->dropColumn('address');
-
-            $table->integer('profInformation')->unsigned();
-            $table->foreign('profInformation')->references('id')->on('ProfessionalInformation');
-            $table->timestamps();
         });
     }
 
