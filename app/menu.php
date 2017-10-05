5ef34dc940cae1818f47ef6656d0c9b12b5ab8f6
Hugo Hernández <hugo_hernandez@fastcodecloud.com>
2017-10-05 13:22:58 -0500

    menus personalizados v3 con modelo

---
+++ app/menu.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class menu extends Model
+{
+    protected $table = 'menus';
+
+
+    protected $fillable = ['text', 'label', 'icon', 'label_color', 'url', 'to', 'typeitem', 'parent'];
+
+    public function menu()
+    {
+        return $this->belongsTo('App\menu', 'parent');
+    }
+}
