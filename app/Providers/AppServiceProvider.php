bffed40bd96aaa6854b9b363874abb3e2141f38f
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2017-10-05 11:43:51 -0500

    Custom Menu

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,9 @@ namespace App\Providers;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Contracts\Events\Dispatcher;
+use JeroenNoten\LaravelAdminLte\Events\BuildingMenu;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -16,9 +19,13 @@ class AppServiceProvider extends ServiceProvider
 
         URL::forceScheme('https');
 
-        // if(env( 'APP_ENV') !== 'local'){
-            //URL::forceScheme('https');
-        // }
+        $events->listen(BuildingMenu::class, function (BuildingMenu $event) {
+            $event->menu->add('MAIN NAVIGATION');
+            $event->menu->add([
+                'text' => 'Blog',
+                'url' => 'admin/blog',
+            ]);
+        });
     }
 
     /**
