103ec4cb1c6d70a39f46a1828e9ab29a68938026
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2017-10-05 11:57:36 -0500

    Trying again

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Events\Dispatcher;
+use JeroenNoten\LaravelAdminLte\Events\BuildingMenu;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,13 +14,16 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(){
-
+    public function boot(Dispatcher $events)
+    {
         URL::forceScheme('https');
-
-        // if(env( 'APP_ENV') !== 'local'){
-            //URL::forceScheme('https');
-        // }
+        $events->listen(BuildingMenu::class, function (BuildingMenu $event) {
+            $event->menu->add('MAIN NAVIGATION');
+            $event->menu->add([
+                'text' => 'Blog',
+                'url' => 'admin/blog',
+            ]);
+        });
     }
  
     /**
