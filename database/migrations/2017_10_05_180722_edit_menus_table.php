510337505cecb03645814cbc1f2ae92d9386355d
Hugo Hernández <hugo_hernandez@fastcodecloud.com>
2017-10-05 13:09:02 -0500

    menus personalizados v2

---
+++ database/migrations/2017_10_05_180722_edit_menus_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class EditMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('menus', function (Blueprint $table) {
+            $table->integer('parent')->unsigned()->nullable();
+            $table->foreign('menus')->references('id')->on('menus');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('menus', function (Blueprint $table) {
+            //
+        });
+    }
+}
