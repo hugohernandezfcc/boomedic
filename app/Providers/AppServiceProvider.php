4e70d40a07aad8ad9cef96bcb41cb0b10f5bcf76
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2017-10-04 23:34:29 -0500

    review

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,8 +12,7 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
-    {
+    public function boot(){
 
         if(env( 'APP_ENV') !== 'local'){
             URL::forceScheme('https');
