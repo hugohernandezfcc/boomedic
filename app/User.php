e60cc391d7551d8e2bf37112d16c145f9c65ecc4
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2017-10-16 17:45:46 -0500

    prueba 2

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name', 
-        'email', 
+        'email',  
         'password', 
         'birthdate',
         'age',                  
