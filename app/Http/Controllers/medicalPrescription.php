881bcee25f9c9b947d7594c256f6a51eb6c23eee
GTRebbeca <rebbecagt@gmail.com>
2017-10-30 18:04:53 -0600

    Facturación formato PDF y conexión PAC

--- app/Http/Controllers/medicalPrescription.php
+++ app/Http/Controllers/medicalPrescription.php
@@ -120,11 +120,69 @@ class medicalPrescription extends Controller
 
    
     }
-    public function PDFGenerator (Request $request){
-    $data =  array('medico' => $request->medico);
- 
-     $pdf = \PDF::loadView('medicalPrescriptionPDF', $data);
-     return $pdf->download('RESULT.pdf');
+
+
+        public function PDFGenerator (Request $request){
+
+            /* Cuerpo del Xml CFDI de petición */
+$cfd = <<<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<cfdi:Comprobante LugarExpedicion="CIUDAD DE MEXICO - D.F." metodoDePago="NO IDENTIFICADO"
+  tipoDeComprobante="egreso" total="208197.77" Moneda="MXP" TipoCambio="1.0000" subTotal="208197.77"
+  condicionesDePago="PAGO EN UNA SOLA EXHIBICION"
+  certificado="MIIEdDCCA1ygAwIBAgIUMjAwMDEwMDAwMDAxMDAwMDU4NjcwDQYJKoZIhvcNAQEFBQAwggFvMRgwFgYDVQQDDA9BLkMuIGRlIHBydWViYXMxLzAtBgNVBAoMJlNlcnZpY2lvIGRlIEFkbWluaXN0cmFjacOzbiBUcmlidXRhcmlhMTgwNgYDVQQLDC9BZG1pbmlzdHJhY2nDs24gZGUgU2VndXJpZGFkIGRlIGxhIEluZm9ybWFjacOzbjEpMCcGCSqGSIb3DQEJARYaYXNpc25ldEBwcnVlYmFzLnNhdC5nb2IubXgxJjAkBgNVBAkMHUF2LiBIaWRhbGdvIDc3LCBDb2wuIEd1ZXJyZXJvMQ4wDAYDVQQRDAUwNjMwMDELMAkGA1UEBhMCTVgxGTAXBgNVBAgMEERpc3RyaXRvIEZlZGVyYWwxEjAQBgNVBAcMCUNveW9hY8OhbjEVMBMGA1UELRMMU0FUOTcwNzAxTk4zMTIwMAYJKoZIhvcNAQkCDCNSZXNwb25zYWJsZTogSMOpY3RvciBPcm5lbGFzIEFyY2lnYTAeFw0xMjA3MjcxNzAyMDBaFw0xNjA3MjcxNzAyMDBaMIHbMSkwJwYDVQQDEyBBQ0NFTSBTRVJWSUNJT1MgRU1QUkVTQVJJQUxFUyBTQzEpMCcGA1UEKRMgQUNDRU0gU0VSVklDSU9TIEVNUFJFU0FSSUFMRVMgU0MxKTAnBgNVBAoTIEFDQ0VNIFNFUlZJQ0lPUyBFTVBSRVNBUklBTEVTIFNDMSUwIwYDVQQtExxBQUEwMTAxMDFBQUEgLyBIRUdUNzYxMDAzNFMyMR4wHAYDVQQFExUgLyBIRUdUNzYxMDAzTURGUk5OMDkxETAPBgNVBAsTCFVuaWRhZCAxMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC2TTQSPONBOVxpXv9wLYo8jezBrb34i/tLx8jGdtyy27BcesOav2c1NS/Gdv10u9SkWtwdy34uRAVe7H0a3VMRLHAkvp2qMCHaZc4T8k47Jtb9wrOEh/XFS8LgT4y5OQYo6civfXXdlvxWU/gdM/e6I2lg6FGorP8H4GPAJ/qCNwIDAQABox0wGzAMBgNVHRMBAf8EAjAAMAsGA1UdDwQEAwIGwDANBgkqhkiG9w0BAQUFAAOCAQEATxMecTpMbdhSHo6KVUg4QVF4Op2IBhiMaOrtrXBdJgzGotUFcJgdBCMjtTZXSlq1S4DG1jr8p4NzQlzxsdTxaB8nSKJ4KEMgIT7E62xRUj15jI49qFz7f2uMttZLNThipunsN/NF1XtvESMTDwQFvas/Ugig6qwEfSZc0MDxMpKLEkEePmQwtZD+zXFSMVa6hmOu4M+FzGiRXbj4YJXn9Myjd8xbL/c+9UIcrYoZskxDvMxc6/6M3rNNDY3OFhBK+V/sPMzWWGt8S1yjmtPfXgFs1t65AZ2hcTwTAuHrKwDatJ1ZPfa482ZBROAAX1waz7WwXp0gso7sDCm2/yUVww=="
+  noCertificado="20001000000100005867" formaDePago="CONTADO"
+  sello=""
+  fecha="2014-08-18T11:11:59" folio="1" serie="EDR" version="3.2"
+  xsi:schemaLocation="http://www.buzonfiscal.com/ns/addenda/bf/2 http://www.buzonfiscal.com/schema/xsd/Addenda_BF_v20.xsd http://www.sat.gob.mx/cfd/3 http://www.sat.gob.mx/sitio_internet/cfd/3/cfdv32.xsd http://www.sat.gob.mx/consumodecombustibles http://www.sat.gob.mx/sitio_internet/cfd/consumodecombustibles/consumodecombustibles.xsd"
+  xmlns:bfa2="http://www.buzonfiscal.com/ns/addenda/bf/2" xmlns:cfdi="http://www.sat.gob.mx/cfd/3"
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xmlns:consumodecombustibles="http://www.sat.gob.mx/consumodecombustibles">
+  <cfdi:Emisor nombre="EMISOR DEMO S.A. DE C.V." rfc="AAA010101AAA">
+    <cfdi:DomicilioFiscal codigoPostal="64000" pais="México" estado="Nuevo León"
+      municipio="Monterrey" colonia="Centro" noInterior="4to Piso" noExterior="1921"
+      calle="Diego Rivera"/>
+    <cfdi:RegimenFiscal Regimen="Regimen General de Ley Personas Morales"/>
+  </cfdi:Emisor>
+  <cfdi:Receptor nombre="EMPRESA RECEPTORA S.A." rfc="BBB010101BBB">
+    <cfdi:Domicilio codigoPostal="64000" pais="México" estado="Nuevo León"
+      municipio="San Pedro Garza García" colonia="Magistrales" noInterior="3C" noExterior="1432"
+      calle="Insurgentes"/>
+  </cfdi:Receptor>
+  <cfdi:Conceptos>
+    <cfdi:Concepto importe="0.00" valorUnitario="0.00" descripcion="COMBUSTIBLE"
+      unidad="PZA" cantidad="1"/>
+  </cfdi:Conceptos>
+  <cfdi:Impuestos totalImpuestosTrasladados="0.00">
+    <cfdi:Traslados>
+      <cfdi:Traslado importe="0.00" tasa="16" impuesto="IVA"/>
+    </cfdi:Traslados>
+  </cfdi:Impuestos>
+</cfdi:Comprobante>
+EOF;
+            /* Petición al Pac de facturación */
+
+                            $request_options = array(
+                              'http' => array(
+                                'method' => "POST",
+                                'header' => "Content-Type: text/xml\r\n"."x-auth-token: ABCD1234\r\n",
+                                'content' => $cfd,
+                                'ignore_errors' => true
+
+                            ));
+                            $stream_context = stream_context_create($request_options);
+                            $response = file_get_contents('https://staging.diverza.com/issue', false, $stream_context);
+                            $response_code = $http_response_header[0];
+                            $stamp = $response;
+
+        $data =  array(
+                        'medico' => $request->medico,
+                        'response' => $response_code,
+                        'stamp'  => $stamp
+                    ); 
+
+        $pdf = \PDF::loadView('medicalPrescriptionPDF', $data);
+        return $pdf->stream('RESULT.pdf');
  
 }
 }
--- resources/views/medicalPrescriptionPDF.blade.php
+++ resources/views/medicalPrescriptionPDF.blade.php
@@ -17,7 +17,117 @@
 
 @section('body')
 <div>
-	<label class="col-sm-2 control-label">Médico</label>
-	<labelclass="col-sm-2 control-label">{{ $medico }}</label>
+
+
+
+
+                <style>
+                .bordes{
+                        background-color: #3E3D3D;
+                        border-radius: 85px;
+                        -moz-border-radius: 85px;
+                        -webkit-border-radius: 85px;
+                        border: 0px solid #3E3D3D;
+                        border-collapse: separate;
+                        border-spacing:0px 5px;
+                        color: white;
+                        font-family: sans-serif;
+                        font-size: 13px;
+                        text-align: center;
+                        font-weight: bold;
+                }
+                .letras {
+                        font-family: sans-serif;
+                        font-size: 11px;
+                }
+                </style>
+
+
+        <table width="100%" class="letras">
+         <tr style="text-align: center;">
+            <td width="25%" rowspan="8" align="top"><IMG SRC="https://upload.wikimedia.org/wikipedia/commons/a/ab/Logo_TV_2015.png" width=200 height=200></td>
+            <td width="35%" rowspan="8" align="top">Name<br/>Rfc<br/>Dirección: ePais, eEstado, Municipio eMunicipio, Colonia eColonia, Calle eCalle, eNoEX <br/> Código Postal: eCP</td>
+            <td width="40%">Comprobante Fiscal Digital a través de internet</td></tr>
+        <tr><td class="bordes">FACTURA</td></tr>
+        <tr style="text-align: center;">
+            <td style="color: #545454"><br/>Folio Fiscal Digital:</td></tr>
+        <tr style="text-align: center;">
+            <td>hgj343532k52k3523</td></tr>
+        <tr style="text-align: center"><td style="color: #545454">No. de Serie del Certificado SAT</td></tr> 
+        <tr style="text-align: center"><td>fsdkfy873wf2iu3hfkfhfhfshdf</td></tr>
+        <tr style="text-align: center"><td style="color: #545454">Fecha y Hora de Certificación</td></tr>
+        <tr style="text-align: center"><td>2017kjnsfnwefw</td></tr>         
+       </table>
+       <table width="100%" class="letras">
+               <tr style="color: #545454">
+                <td align="center">Fecha y Hora de Emisión</td>
+                <td align="center">Serie</td>
+                <td align="center">Folio</td>
+                <td align="center">No de Certificado del Emisor</td>
+                <td align="center">Moneda</td>
+                <td align="center">Tipo de Comprobante</td>
+               </tr>
+               <tr>
+                <td align="center">fecha</td>
+                <td align="center">serie</td>
+                <td align="center">folio</td>
+                <td align="center">noCertificado</td>
+                <td align="center">moneda</td>
+                <td align="center">tipoComprobante</td>
+               </tr>
+       </table><br/><br/>
+       <table width="100%" class="letras">
+               <tr class="bordes"><td colspan="4" align="center">FACTURAR A:</td></tr>
+               <tr><td style="color: #545454">Razón Social:</td><td>razoncuenta</td><td style="color: #545454">RFC:</td><td>rfc</td></tr>
+               <tr><td style="color: #545454">Domicilio:</td><td>pais, Municipio municipio, Colonia colonia, Nro noExterior noInterior</td><td style="color: #545454">Referencia:</td><td>No aplica</td></tr>
+               <tr><td style="color: #545454">Ciudad:</td><td>estado</td><td style="color: #545454">Código Postal:</td><td>postal</td></tr>
+       </table><br/>
+       <table width="100%" class="letras">
+        
+                <tr >
+                <td class="bordes">CLAVE</td>
+                <td class="bordes">CANTIDAD</td>
+                <td class="bordes">UNIDAD DE MEDIDA</td>
+                <td class="bordes">DESCRIPCIÓN</td>
+                <td class="bordes">PRECIO UNITARIO</td>
+                <td class="bordes">IMPORTE</td>
+               </tr>
+
+               <tr >
+                <td align="center">Product2.Name</td>
+                <td align="center">Quantity</td>
+                <td align="center">UnidadMedida</td>
+                <td align="center">Product.Name</td>
+                <td align="center">UnitPrice</td>
+                <td align="center">{TotalPrice</td>
+               </tr>   
+
+       </table>
+       <table  class="letras">
+               <tr>
+                <td style="color: #545454" width="40%">Importe con Letra:</td><td width="40%">variable</td><td style="color: #545454" width="20%">SUB-TOTAL:</td><td>subtotal</td>
+               </tr>
+               <tr>
+                <td style="color: #545454" width="40%">Forma de Pago:</td><td width="40%">formap</td><td style="color: #545454" width="20%">IVA:</td><td>totalIva</td>
+               </tr>
+               <tr>
+                <td style="color: #545454" align="right" colspan="2">TOTAL:</td><td align="right" colspan="2">total</td>
+               </tr>
+       </table>
+
+
+       <!-- SELLO FISCAL -->
+       <br/><br/><br/><br/><br/><br/>
+       <table width="100%" class="letras">
+               <tr><td width="80%" align = "center">Lugar de Expedición:</td><td rowspan="7" width="20%">codigo</td></tr>
+                <tr><td class="bordes">SELLO DIGITAL DEL EMISOR</td></tr>
+                <tr><td>string</td></tr>
+                <tr><td class="bordes">SELLO DIGITAL DEL SAT</td></tr>
+                <tr><td>string</td></tr>
+                <tr><td class="bordes">CADENA ORIGINAL DEL COMPLEMENTO DE CERTIFICACIÓN DIGITAL DEL SAT</td></tr>
+                <tr><td>{!uuid}</td></tr>
+               
+       </table>
+
 </div>
 @stop
