3df416bd28d9f174c158223ea5d02cf2b38cb706
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2017-10-31 14:19:24 -0600

    redirec

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirectredirect()->secure('/medicalconsultations');
+            return redirect()->secure('/medicalconsultations');
         }
 
         return $next($request);
