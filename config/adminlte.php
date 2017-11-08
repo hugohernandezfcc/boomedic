91bd1ca3e40b92c1cfc3514b641204703900fa49
Cristina Pioquinto <cristina@doitcloud.consulting>
2017-11-08 11:20:26 -0600

    the view changes

--- config/adminlte.php
+++ config/adminlte.php
@@ -89,7 +89,7 @@ return [
     'logout_url'           => 'bye',
     'logout_method'        => null,
     'login_url'            => 'login',
-    'register_doctor_url'  => 'medicalRegister',
+    'register_doctor_url'  => 'homemedical',/*'medicalRegister',*/
     'register_url'         => 'register',
 
     /*
