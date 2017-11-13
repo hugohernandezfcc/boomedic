6d5a45db16d0eba2f1a9ec3c7ab4ecf70ff344f6
GTRebbeca <rebbecagt@gmail.com>
2017-11-13 17:18:08 -0600

    text align Privacy Statement

--- resources/views/privacyStatement.blade.php
+++ resources/views/privacyStatement.blade.php
@@ -13,8 +13,11 @@
 	  	<div class="box-header with-border">
 		    <h3 class="box-title">Aviso de Privacidad</h3>
 	  	</div>
+	  <div style="margin-left: 100px; margin-top: 50px; margin-right: 100px; text-align: justify;">
+	  		{{ $privacy->description }}
+	  </div>
 </div>	  	
 
- {{ $privacy->description }}
+ 
 
 @stop
