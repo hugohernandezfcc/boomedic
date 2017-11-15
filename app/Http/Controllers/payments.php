f96c66487bbc3426887ad35d9fb0560c5e0e76c5
GTRebbeca <rebbecagt@gmail.com>
2017-11-15 16:57:39 -0600

    cambios

--- app/Http/Controllers/payments.php
+++ app/Http/Controllers/payments.php
@@ -8,6 +8,19 @@ use Illuminate\Http\Request;
 use App\User;
 use App\PaymentMethod;
 use App\VisaAPIClient;
+use PayPal\Rest\ApiContext;
+use PayPal\Auth\OAuthTokenCredential;
+use PayPal\Api\Amount;
+use PayPal\Api\Details;
+use PayPal\Api\Item;
+use PayPal\Api\ItemList;
+use PayPal\Api\Payer;
+use PayPal\Api\Payment;
+use PayPal\Api\RedirectUrls;
+use PayPal\Api\ExecutePayment;
+use PayPal\Api\PaymentExecution;
+use PayPal\Api\Transaction;
+use config;
 
 class payments extends Controller
 {
