96ff47ad869bf2e494d3ff47eaf4d3301cb3fbe8
GTRebbeca <rebbecagt@gmail.com>
2017-11-15 16:36:30 -0600

    cambios...

--- resources/views/payments.blade.php
+++ resources/views/payments.blade.php
@@ -221,19 +221,8 @@
 	                </div>
 
 	                <div id="paypalButton" style="display: none;">
-	                	<form action="payment2" method="post" target="_top">
-							<input type="hidden" name="cmd" value="_xclick">
-							<input type="hidden" name="business" value="hugo@doitcloud.consulting">
-							<input type="hidden" name="lc" value="AL">
-							<input type="hidden" name="item_name" value="Consulta medica">
-							<input type="hidden" name="item_number" value="200">
-							<input type="hidden" name="amount" value="200.00">
-							<input type="hidden" name="currency_code" value="USD">
-							<input type="hidden" name="button_subtype" value="services">
-							<input type="hidden" name="no_note" value="0">
-							<input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynowCC_LG.gif:NonHostedGuest">
-							<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-							<img alt="" border="0" src="https://www.paypalobjects.com/es_XC/i/scr/pixel.gif" width="1" height="1">
+	                	<form action="payment2" method="post">
+						<input type="submit" name="Paypal Buy!">	
 						</form>
 
 	                </div>
