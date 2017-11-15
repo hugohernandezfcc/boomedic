7e70cb5446dcac40ef8c5016e9e1860ff6808b5b
Cristina Pioquinto <cristina@doitcloud.consulting>
2017-11-15 15:27:21 -0600

    ticket05

--- app/Http/Controllers/supportTickets.php
+++ app/Http/Controllers/supportTickets.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use App\User;
+use App\SupportTicket;
 
 class supportTickets extends Controller
 {
--- resources/views/tickets.blade.php
+++ resources/views/tickets.blade.php
@@ -8,6 +8,27 @@
 
 @section('content')
 
-Hola
+	<div class="lockscreen-item" style="margin: 10px 0 30px auto;">
+	    <!-- lockscreen image -->
+	    <div class="lockscreen-image">
+	      <img src="https://adminlte.io/themes/AdminLTE/dist/img/user1-128x128.jpg" alt="User Image">
+	    </div>
+	    <!-- /.lockscreen-image -->
+
+	    <!-- lockscreen credentials (contains the form) -->
+	    <form class="lockscreen-credentials" action="create" method="get">
+	    	{{ csrf_field() }}
+	      	<div class="input-group">
+	        	<div class="form-control">{{ $username }}</div>
+	        	<input type="hidden" name="id" value="{{ $userId }}">
+	        	<div class="input-group-btn">
+		          	<button type="submit" class="btn">
+		          		<i class="fa fa-plus text-muted"></i>
+		          	</button>
+	        	</div>
+	      	</div>
+	    </form>
+	    <!-- /.lockscreen credentials -->
+	</div>
 
 @stop
