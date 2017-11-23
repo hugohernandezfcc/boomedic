21c85a39190328ed65c029f6d0f0f3a3277c9e5e
GTRebbeca <rebbecagt@gmail.com>
2017-11-23 10:57:20 -0600

    cambios en update profile

--- app/Http/Controllers/profile.php
+++ app/Http/Controllers/profile.php
@@ -50,36 +50,7 @@ class profile extends Controller
      */
     public function store(Request $request)
     {
-        $path = $request->photo->store('images', 's3');
-
-        $user = User::find($id);
-
-        $user->status        = $request->status;         
-        $user->firstname     = $request->firstname;         
-        $user->lastname      = $request->lastname;         
-        $user->email         = $request->email;         
-        $user->username      = $request->username;         
-        $user->age           = $request->age;         
-        $user->gender        = $request->gender;         
-        $user->occupation    = $request->occupation;         
-        $user->scholarship   = $request->scholarship;         
-        $user->maritalstatus = $request->maritalstatus;         
-        $user->mobile        = $request->mobile;         
-        $user->status        = 'Complete';
-
-        $user->country       = $request->country; 
-        $user->state         = $request->state; 
-        $user->delegation    = $request->delegation; 
-        $user->colony        = $request->colony; 
-        $user->street        = $request->street; 
-
-
-        $user->postalcode    = $request->postalcode; 
-        $user->latitude      = $request->latitude; 
-        $user->longitude     = $request->longitude; 
-
-        if ( $user->save() ) 
-            return redirect('user/profile/' . $id );
+        
     }
 
     /**
@@ -202,7 +173,7 @@ class profile extends Controller
      */
     public function update(Request $request, $id)
     {
-        $path = $request->photo->store('images', 's3');
+       // $path = $request->photo->store('images', 's3');
 
         $user = User::find($id);
 
