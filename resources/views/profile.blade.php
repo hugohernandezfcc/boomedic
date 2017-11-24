273163506886580331faa132f59bcb87e1618c56
GTRebbeca <rebbecagt@gmail.com>
2017-11-24 15:14:48 -0600

    prueba 11

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -45,38 +45,12 @@
 		<script type="text/javascript">
         Dropzone.options.imageUpload = {
   // The configuration we've talked about above
+  			  paramName: "file",
 			  autoProcessQueue: false,
 			  uploadMultiple: true,
-			  parallelUploads: 100,
-			  maxFiles: 100,
+			  maxFiles: 1,
 
 			  // The setting up of the dropzone
-			  init: function() {
-			    var imageUpload = this;
-
-			    // First change the button to actually tell Dropzone to process the queue.
-			    this.element.querySelector("button[type=submit]").addEventListener("click", function(e) {
-			      // Make sure that the form isn't actually being sent.
-			      e.preventDefault();
-			      e.stopPropagation();
-			      myDropzone.processQueue();
-			    });
-
-			    // Listen to the sendingmultiple event. In this case, it's the sendingmultiple event instead
-			    // of the sending event because uploadMultiple is set to true.
-			    this.on("sendingmultiple", function() {
-			      // Gets triggered when the form is actually being sent.
-			      // Hide the success button or the complete form.
-			    });
-			    this.on("successmultiple", function(files, response) {
-			      // Gets triggered when the files have successfully been sent.
-			      // Redirect user or notify of success.
-			    });
-			    this.on("errormultiple", function(files, response) {
-			      // Gets triggered when there was an error sending the files.
-			      // Maybe show form again, and notify user of error
-			    });
-			  }
         };
 </script>
 	<br/>
@@ -129,9 +103,9 @@
 
 
 
-	    		<form enctype="multipart/form-data" action="/user/update/{{$userId}}" method="post" class="dropzone" id="imageUpload">
+	    		<form enctype="multipart/form-data" action="/user/update/{{$userId}}" method="post" >
 	    			<div class="col-sm-12">	
-	    				 <div class="fallback">
+	    				 <div class="dropzone" id="imageUpload">
 						    <input name="file" type="file" multiple />
 						  </div></div>
 	    			{{ csrf_field() }}
