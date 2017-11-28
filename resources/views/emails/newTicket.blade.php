e7ce4884b2dcc70a78f283e093ccbbe7a0be83b6
Cristina Pioquinto <cristina@doitcloud.consulting>
2017-11-28 12:16:16 -0600

    email01

--- resources/views/emails/newTicket.blade.php
+++ resources/views/emails/newTicket.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
 </head>
 <body>
-	<img src="logoD.png" style="width:200px; height:200px; background-color: black">
+	<img src="logoD.png" style="width:200px; height:200px; ">
     <h2>El usuario {{ $user->name }} ha creado un ticket.</h2>
 </body>
 </html>
