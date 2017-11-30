e7904f1c1a250798d24bcc63433b9edca8b9c1b2
GTRebbeca <rebbecagt@gmail.com>
2017-11-30 11:07:39 -0600

    Cambios  monto de pago paypal

--- app/Http/Controllers/payments.php
+++ app/Http/Controllers/payments.php
@@ -388,7 +388,7 @@ class payments extends Controller
 
                               $notification = array(
                                         //If it has been rejected, the internal error code is sent.
-                                    'message' => 'Procesado su pago de paypal Payer Id: ' .$request->input('PayerID'). ', ' .$result->getTransactions()->getAmount()->getTotal(), 
+                                    'message' => 'Procesado su pago de paypal Payer Id: ' .$request->input('PayerID'). ', ' .$result->getTransactions()->get(0)->getAmount()->getTotal(), 
                                     'success' => 'success'
                                 );
                               return redirect('payment/index')->with($notification);
