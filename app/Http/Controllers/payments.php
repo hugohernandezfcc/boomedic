0b56bbb5b829d9476234432e9510311dc84bc5f1
GTRebbeca <rebbecagt@gmail.com>
2017-11-30 11:55:11 -0600

    cambios del monto correcto

--- app/Http/Controllers/payments.php
+++ app/Http/Controllers/payments.php
@@ -382,13 +382,13 @@ class payments extends Controller
                                             $Trans = new transaction_bank;
                                             $Trans->paymentmethod = $paypalExist2->id;
                                             $Trans->receiver = 'receiver prueba';
-                                            $Trans->amount = '1';
+                                            $Trans->amount = $payment->transactions[0]->amount->total;
                                             $Trans->save();    
                                         
 
                               $notification = array(
                                         //If it has been rejected, the internal error code is sent.
-                                    'message' => 'Procesado su pago de paypal Payer Id: ' .$request->input('PayerID'). ', ' . $payment->transactions[0]->amount->total, 
+                                    'message' => 'Procesado su pago de paypal Payer Id: ' .$request->input('PayerID'). ', ' .$result->getPayer()->getPayerInfo()->getEmail(), 
                                     'success' => 'success'
                                 );
                               return redirect('payment/index')->with($notification);
