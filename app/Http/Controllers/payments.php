c5eb993a85cf8ef98df78033809f70d42ed0536e
GTRebbeca <rebbecagt@gmail.com>
2017-11-30 11:22:23 -0600

    3

--- app/Http/Controllers/payments.php
+++ app/Http/Controllers/payments.php
@@ -388,7 +388,7 @@ class payments extends Controller
 
                               $notification = array(
                                         //If it has been rejected, the internal error code is sent.
-                                    'message' => 'Procesado su pago de paypal Payer Id: ' .$request->input('PayerID'). ', ' .$result->getTransactions(), 
+                                    'message' => 'Procesado su pago de paypal Payer Id: ' .$request->input('PayerID'). ', ' .$result->getTransactions(0)->getAmount()->getTotal(), 
                                     'success' => 'success'
                                 );
                               return redirect('payment/index')->with($notification);
