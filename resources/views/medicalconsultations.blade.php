cf0bb3eb20e02346d9df911950afdde935a6f96f
GTRebbeca <rebbecagt@gmail.com>
2017-11-30 16:03:54 -0600

    Geolocalización busqueda

--- resources/views/medicalconsultations.blade.php
+++ resources/views/medicalconsultations.blade.php
@@ -559,8 +559,28 @@
             infoWindow.setContent(message03);
         }
 
+        document.getElementById('submit').addEventListener('click', function() {
+        geocodeAddress(geocoder, map);
+        });
+
       }
 
+
+      //Filter geocode Address
+        function geocodeAddress(geocoder, resultsMap) {
+        var address = document.getElementById('address').value;
+        geocoder.geocode({'address': address}, function(results, status) {
+          if (status === 'OK') {
+            resultsMap.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+              map: resultsMap,
+              position: results[0].geometry.location
+            });
+          } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+          }
+        });
+      }
   
       //Filter of Speciality
       function functionEsp(specialityValue, keyWordValue, positionValue, rangeValue) {
