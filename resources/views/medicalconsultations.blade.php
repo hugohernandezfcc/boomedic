27d588d24e34aebbe03ac4ba668f0060d5dbfe3b
GTRebbeca <rebbecagt@gmail.com>
2017-12-01 12:40:28 -0600

    stilo10

--- resources/views/medicalconsultations.blade.php
+++ resources/views/medicalconsultations.blade.php
@@ -296,21 +296,22 @@
                 <!-- Modal content-->
                 <div class="modal-content">
                   <div class="modal-header">   
-                      Busqueda de lugar:
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
                   </div>
                   <div class="modal-body">
-                        <div id="floating-panel">
+         
         <div class="input-group input-group-sm">
+          
           <input id="address" type="textbox" value=" " class="form-control">
           <span class="input-group-btn">
           <button type="submit" class="btn btn-info btn-flat"><span class="fa fa-map-marker"></span></button></span>
         </div>
       <!--<input id="submit" type="button" value="Buscar" class="map-marker text-muted">-->
-    </div>
+   
                   </div>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                      <span aria-hidden="true">&times;</span>
-                    </button>
+
                 </div>
 
               </div>
