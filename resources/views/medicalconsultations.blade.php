96a98b3a91bac7025890d77e525c2d687090567e
GTRebbeca <rebbecagt@gmail.com>
2017-12-01 13:44:14 -0600

    subiendo estilo y ubicación en el mapa

--- resources/views/medicalconsultations.blade.php
+++ resources/views/medicalconsultations.blade.php
@@ -283,15 +283,16 @@
         <center><h1><i class="fa fa-refresh fa-spin"></i> Cargando ...</h1></center>
       </div>
 
-          <div class="checkStyl2">      
+
+
+    <div id="map"></div>
+         <div class="checkStyl2">      
       <a class="btn btn-info" data-backdrop="static" data-toggle="modal" data-target="#modal">Busqueda</a>
     </div>
     <div id='rango'>
         <strong><label for="rango01" id="label04" class="textStyle01"></label> <span id="rango03"></span></strong><br/>
         <input type="range" name="rango01" id="rango01" value="1000" min="1000" max="10000" step="50" autocomplete="off" onchange="start();" class="rangeStyle"/>
       </div>
-
-    <div id="map"></div>
   </div> 
 
       
