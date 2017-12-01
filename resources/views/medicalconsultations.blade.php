1c5e998b8dc7f0eaadb62707365db22261fe76b8
GTRebbeca <rebbecagt@gmail.com>
2017-12-01 15:55:25 -0600

    prueba 2

--- resources/views/medicalconsultations.blade.php
+++ resources/views/medicalconsultations.blade.php
@@ -845,7 +845,6 @@
         });
     });
     </script>
-    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBfhtvjKVp3M9PJ5JLYZXzMzhizRYxkz6Y&callback=initMap"
-  type="text/javascript"></script>
+
 
 @stop
