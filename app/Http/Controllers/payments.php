76fcdb719cd04317188f8e6b59c6bdb8bd95e0b1
GTRebbeca <rebbecagt@gmail.com>
2017-12-04 16:26:11 -0600

    subiendo cambios id transacción paypal

--- app/Http/Controllers/payments.php
+++ app/Http/Controllers/payments.php
@@ -389,7 +389,7 @@ class payments extends Controller
 
                               $notification = array(
                                         //If it has been rejected, the internal error code is sent.
-                                    'message' => 'Procesado su pago de paypal Payer Id: ' .$request->input('PayerID'). ', ' .$result->getPayer()->getPayerInfo()->getEmail(), 
+                                    'message' => 'Procesado su pago de paypal Payer Id: ' .$request->input('PayerID'). ', ' .$result->getPayer()->getPayerInfo()->getEmail().' tran: '.$payment->related_resources[0]->sale->id, 
                                     'success' => 'success'
                                 );
                               return redirect('payment/index')->with($notification);
