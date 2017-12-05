aa575896788223b6b3d608bbf4b3b90185b54041
GTRebbeca <rebbecagt@gmail.com>
2017-12-05 16:00:21 -0600

    subiendo cambios en posicionamiento de busqueda

--- resources/views/medicalconsultations.blade.php
+++ resources/views/medicalconsultations.blade.php
@@ -581,7 +581,7 @@
             });
                 var geocoder = new google.maps.Geocoder();
                 document.getElementById('submit').addEventListener('click', function() {
-                geocodeAddress(geocoder, map);
+                geocodeAddress(geocoder, map, markerP);
                 });
 
 
@@ -610,15 +610,12 @@
 
 
       //Filter geocode Address
-        function geocodeAddress(geocoder, resultsMap) {
+        function geocodeAddress(geocoder, resultsMap, markerP) {
         var address = document.getElementById('address').value;
         geocoder.geocode({'address': address}, function(results, status) {
           if (status === 'OK') {
             resultsMap.setCenter(results[0].geometry.location);
-             var marker = new google.maps.Marker({
-              map: resultsMap,
-              position: results[0].geometry.location
-            });
+            markerP.setPosition(results[0].geometry.location);
           } else {
             alert('Geocode was not successful for the following reason: ' + status);
           }
