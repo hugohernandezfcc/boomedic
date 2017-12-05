cd6113096e4d79369bfb8748597cbd1e9087be0d
GTRebbeca <rebbecagt@gmail.com>
2017-12-05 15:52:47 -0600

    prueba 12

--- resources/views/medicalconsultations.blade.php
+++ resources/views/medicalconsultations.blade.php
@@ -616,9 +616,11 @@
         geocoder.geocode({'address': address}, function(results, status) {
           if (status === 'OK') {
             resultsMap.setCenter(results[0].geometry.location);
-            var results = results[0].geometry.location;
-           return results;
-
+             var marker = new google.maps.Marker({
+              map: resultsMap,
+              position: results[0].geometry.location,
+              icon: markerUser
+            });
           } else {
             alert('Geocode was not successful for the following reason: ' + status);
           }
