ae8682d8b51ef2436bcbbd5f8a576e1d0a8a79fc
GTRebbeca <rebbecagt@gmail.com>
2017-12-05 15:14:30 -0600

    prueba 7 posicion puntero

--- resources/views/medicalconsultations.blade.php
+++ resources/views/medicalconsultations.blade.php
@@ -567,6 +567,7 @@
         var geocoder = new google.maps.Geocoder();
         document.getElementById('submit').addEventListener('click', function() {
         geocodeAddress(geocoder, map);
+
         });
             var markerUser = "{{ asset('markerUser.png') }}";
 
@@ -612,12 +613,8 @@
         geocoder.geocode({'address': address}, function(results, status) {
           if (status === 'OK') {
             resultsMap.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-              draggable: true,
-              map: resultsMap,
-              position: results[0].geometry.location,
-              icon: markerUser
-            });
+            markerP.setPosition( resultsMap);
+
           } else {
             alert('Geocode was not successful for the following reason: ' + status);
           }
