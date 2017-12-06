5e62fda955f842dfb771f403f5d1c1b6ea519bd5
GTRebbeca <rebbecagt@gmail.com>
2017-12-06 15:48:31 -0600

    subiendo cambios prueba

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -10,7 +10,7 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.0.1/min/dropzone.min.css" rel="stylesheet">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.2.0/min/dropzone.min.js"></script>
     
-		<script type="text/javascript">
+			<script type="text/javascript">
 			    jQuery(function(){ jQuery('#target').Jcrop(); });
 			     jQuery('#target').Jcrop({
 			      aspectRatio: 1,
@@ -22,7 +22,7 @@
 			      jQuery('#w').val(c.w);
 			      jQuery('#h').val(c.h);
 			     };
-		</script>
+			</script>
 @stop
 
 @section('content')
