796e67aae64f99fa548f69c9369b0e3452e02d5c
GTRebbeca <rebbecagt@gmail.com>
2017-12-06 15:19:41 -0600

    prueba cambiando ubicación de los jscript

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,14 +3,17 @@
 @section('title', 'Boomedic')
 
 @section('content_header')
-    <!-- <h1>Perfil de usuario</h1> -->
+  	<script src="https://sbx00.herokuapp.com/public/js/jquery.Jcrop.min.js"></script>
+	<script src="https://sbx00.herokuapp.com/public/js/jquery.color.js"></script>
+	<script src="https://sbx00.herokuapp.com/public/js/jquery.Jcrop.js"></script>
+	<script type="text/javascript" src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.0.1/min/dropzone.min.css" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.2.0/min/dropzone.min.js"></script>
 @stop
 
 @section('content')
-	<script src="https://sbx00.herokuapp.com/public/js/jquery.Jcrop.min.js"></script>
-	<script src="https://sbx00.herokuapp.com/public/js/jquery.color.js"></script>
-	<script src="https://sbx00.herokuapp.com/public/js/jquery.Jcrop.js"></script>
 
+    
 <script type="text/javascript">
     jQuery(function(){ jQuery('#target').Jcrop(); });
      jQuery('#target').Jcrop({
@@ -25,9 +28,7 @@
      };
 </script>
 
-	<script type="text/javascript" src="//code.jquery.com/jquery-1.11.3.min.js"></script>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.0.1/min/dropzone.min.css" rel="stylesheet">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.2.0/min/dropzone.min.js"></script>
+
 		<script type="text/javascript">
 
 				Dropzone.options.myAwesomeDropzone = { 
