26991363ca3158f3874a75442ddbbe9dc9f68eba
GTRebbeca <rebbecagt@gmail.com>
2017-12-06 16:13:09 -0600

    prueba

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -5,7 +5,7 @@
 @section('content_header')
 
     <link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.0.1/min/dropzone.min.css" rel="stylesheet">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.2.0/min/dropzone.min.js"></script>
+
     <script src="{{ asset('js/jquery.Jcrop.min.js') }}"></script>
 	<script src="{{ asset('js/jquery.color.js') }}"></script>
 	<script src="{{ asset('js/jquery.Jcrop.js') }}"></script>
@@ -136,7 +136,7 @@
 	    			@if($photo == '')
 		    	 		<img src="https://s3.amazonaws.com/abiliasf/profile-42914_640.png" alt="User Image"  style="width:150px; height: 150px;">
 					@else
-						<img src="{{ $photo }}" alt="User Image">			
+						<img src="{{ $photo }}" alt="User Image" id="target">			
 			    	@endif 
 	    			
 	    		</div>
