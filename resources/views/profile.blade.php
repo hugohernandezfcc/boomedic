b2015407c1669bf74f215c938aa2d549ed4eb672
GTRebbeca <rebbecagt@gmail.com>
2017-12-06 15:53:53 -0600

    actualización jquery

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,13 +3,14 @@
 @section('title', 'Boomedic')
 
 @section('content_header')
-  	<script type="text/javascript" src="{{ asset('js/jquery.Jcrop.min.js') }}"></script>
-	<script type="text/javascript" src="{{ asset('js/jquery.color.js') }}"></script>
-	<script type="text/javascript" src="{{ asset('js/jquery.Jcrop.js') }}"></script>
-	<script type="text/javascript" src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.0.1/min/dropzone.min.css" rel="stylesheet">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.2.0/min/dropzone.min.js"></script>
-    
+    <script type="text/javascript" src="{{ asset('js/jquery.Jcrop.min.js') }}"></script>
+	<script type="text/javascript" src="{{ asset('js/jquery.color.js') }}"></script>
+	<script type="text/javascript" src="{{ asset('js/jquery.Jcrop.js') }}"></script>
+
 			<script type="text/javascript">
 			    jQuery(function(){ jQuery('#target').Jcrop(); });
 			     jQuery('#target').Jcrop({
