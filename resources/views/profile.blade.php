1f20b2763457094ab0b61fa19356b1531fc512c1
GTRebbeca <rebbecagt@gmail.com>
2017-12-06 14:14:36 -0600

    PRUEBA 99

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -7,9 +7,9 @@
 @stop
 
 @section('content')
-	<script type="text/javascript" src="{{ URL::asset('js/jquery.Jcrop.min.js') }}"></script>
-	<script type="text/javascript" src="{{ URL::asset('js/jquery.color.js') }}"></script>
-	<script type="text/javascript" src="{{ URL::asset('js/jquery.Jcrop.js') }}"></script>
+	<script type="text/javascript" src="{{ URL::asset('/js/jquery.Jcrop.min.js') }}"></script>
+	<script type="text/javascript" src="{{ URL::asset('/js/jquery.color.js') }}"></script>
+	<script type="text/javascript" src="{{ URL::asset('/js/jquery.Jcrop.js') }}"></script>
 	<script type="text/javascript" src="//code.jquery.com/jquery-1.11.3.min.js"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.0.1/min/dropzone.min.css" rel="stylesheet">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.2.0/min/dropzone.min.js"></script>
