3833787301b0e3830046762dca3c74329e54149a
Cristina Pioquinto <cristina@doitcloud.consulting>
2017-12-06 16:31:31 -0600

    em

--- app/Http/Controllers/emailInboundController.php
+++ app/Http/Controllers/emailInboundController.php
@@ -91,12 +91,12 @@ class emailInboundController extends Controller
             ], 406);
         }
 
-        /*$nTicket = new SupportTicket();        
+        $nTicket = new SupportTicket();        
             $nTicket->userId    = 1;
             $nTicket->status    = 'New';
             $nTicket->ticketDescription      = 'Creado por email';
 
-            $nTicket->save();*/
+            $nTicket->save();
 
         /*$message = (new Client())->get($file['url'], [
             'auth' => ['api', 'key-f3d340554fdb2c32590a9d4ace93027a'],
