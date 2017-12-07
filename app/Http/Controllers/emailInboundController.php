d3d79f947fec7d22ab6d989cf9c137d48393a9cf
Cristina Pioquinto <cristina@doitcloud.consulting>
2017-12-06 18:07:30 -0600

    em

--- app/Http/Controllers/emailInboundController.php
+++ app/Http/Controllers/emailInboundController.php
@@ -53,11 +53,11 @@ class emailInboundController extends Controller
             $attachs = request('attachments');
 
             if(!is_null($attachs)) {
-                /*$attachments = json_decode($attachs, true);
+                /*$attachments = json_decode($attachs, true);*/
 
-                foreach($attachments as $k => $a) {
+                /*foreach($attachments as $k => $a) {
                     $httpClient = new Client();
-                    $resp = $httpClient->request('GET', $a['url'], ['auth' => ['api' => 'key-example']]);
+                    $resp = $httpClient->request('GET', $a['url'], ['auth' => ['api' => 'key-f3d340554fdb2c32590a9d4ace93027a']]);
                     $imageData = $resp->getBody();
                     $base64 = base64_encode($imageData);
                     $this->saveTicketAttachment($base64, $a['name'], $ticket);
@@ -65,7 +65,7 @@ class emailInboundController extends Controller
 
                 $nTicket = new email();
                 $nTicket->userId      = 1;                
-                $nTicket->message      = 'Nuevo';                
+                $nTicket->message      = $attachs;           
                 $nTicket->save();
             }
             return response()->json(['status' => 'ok']);
