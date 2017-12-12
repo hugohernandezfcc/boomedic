ae07b2da9defb6de52f3f3ca0f1d0ae359c32992
Cristina Pioquinto <cristina@doitcloud.consulting>
2017-12-12 16:57:01 -0600

    send02

--- config/mail.php
+++ config/mail.php
@@ -66,7 +66,7 @@ return [
     ],*/
 
     'from' => [
-        'address' => 'postmaster@sandboxad4b45c4083c44e487d79a7b15429da6.mailgun.org',
+        'address' => 'daniel@doitcloud.consulting',
         'name' => 'Boomedic',
     ],
 
