a5f49c47a2c1238c3f592e7961d1845f30a70065
Cristina Pioquinto <cristina@doitcloud.consulting>
2017-12-12 17:25:40 -0600

    prueba email

--- config/mail.php
+++ config/mail.php
@@ -66,7 +66,7 @@ return [
     ],*/
 
     'from' => [
-        'address' => 'postmaster@sandboxad4b45c4083c44e487d79a7b15429da6.mailgun.org',
+        'address' => 'boomedic@contacto.email',
         'name' => 'Boomedic',
     ],
 
