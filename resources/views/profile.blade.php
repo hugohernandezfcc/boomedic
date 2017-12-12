fec3431c71831153c6c4759362c279e2e07ecf85
GTRebbeca <rebbecagt@gmail.com>
2017-12-12 16:13:21 -0600

    Subiendo corrección de apis por error en los scripts

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'Boomedic')
 
 @section('content_header')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script type="text/javascript">
 
     			window.onload = function(){
