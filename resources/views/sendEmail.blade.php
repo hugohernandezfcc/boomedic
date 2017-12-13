4d1dc52a1aa3af432620c85e04c0543eb612283e
Cristina Pioquinto <cristina@doitcloud.consulting>
2017-12-13 12:44:34 -0600

    send09

--- resources/views/sendEmail.blade.php
+++ resources/views/sendEmail.blade.php
@@ -1,10 +1,28 @@
-@extends('adminlte::page')
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+</head>
+<body style="background-color: #696363">
+	<img src="{{ $message->embed(public_path() . '/images/logoD.png') }}" width="170" height="150" style="background-color: black; align-self: right" />
+    
+    <h2>El usuario {!!$name!!} envía este correo.</h2>
+    
 
-@section('title', 'Boomedic')
+    <p>
+    <strong>Detalles del usuario</strong><br/>
+    <strong>Nombre de Usuario:</strong> {!!$username!!} <br/>
+    <strong>Nombre:</strong> {!!$firstname!!} {!!$lastname!!} <br/>
 
-@section('content')
+    <strong>Edad:</strong> {!!$age!!} <br/>
+    <strong>Género:</strong> {!!$gender!!} <br/>
+    <strong>Ocupación:</strong> {!!$occupation!!} <br/>
+    
+    <strong>Dirección:</strong> {!!$country!!}, {!!$state!!}, {!!$delegation!!}, {!!$colony!!}, {!!$postalcode!!}, {!!$street!!} {!!$streetnumber!!} {!!$interiornumber!!} <br/>
+    <strong>Teléfono:</strong> {!!$mobile!!} <br/>
 
+    </p>
 
-	{{textE}}
-
-@stop
+    
+</body>
+</html>
