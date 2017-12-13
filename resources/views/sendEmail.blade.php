d0ff739964005981c8f3881ceedc072003a00b80
Cristina Pioquinto <cristina@doitcloud.consulting>
2017-12-13 14:17:41 -0600

    enviar email desde app

--- resources/views/sendEmail.blade.php
+++ resources/views/sendEmail.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
 </head>
 <body style="background-color: #696363">
-	<!-- <img src="{{ $message->embed(public_path() . '/images/logoD.png') }}" width="170" height="150" style="background-color: black; align-self: right" /> -->
+	<img src="{{ $message->embed(public_path() . '/images/logoD.png') }}" width="170" height="150" style="background-color: black; align-self: right" />
     
     <h2>El usuario {!!$name!!} envía este correo.</h2>
     
