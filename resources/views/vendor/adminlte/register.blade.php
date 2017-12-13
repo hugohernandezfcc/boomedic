a329d427b497fccbab5d61a7dcff3864b3bc1d6f
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2017-12-12 23:53:59 -0600

    botones

--- resources/views/vendor/adminlte/register.blade.php
+++ resources/views/vendor/adminlte/register.blade.php
@@ -77,15 +77,15 @@
                 </button>
 
             </form>
-
+            <br/>
             <div class="row">
-                <div class="col-sm-6">
-                    <a class="btn btn-block btn-default" href="{{ url(config('adminlte.login_url', 'login')) }}" class="text-center">
+                <div class="col-sm-6" align="center">
+                    <a class="btn btn-default" href="{{ url(config('adminlte.login_url', 'login')) }}" class="text-center">
                         {{ trans('adminlte::adminlte.i_already_have_a_membership') }}
                     </a> 
                 </div>
-                <div class="col-sm-6">
-                    <a class="btn btn-block btn-default" href="{{ url(config('adminlte.login_url', 'login')) }}" class="text-center">
+                <div class="col-sm-6" align="center">
+                    <a class="btn btn-default" href="{{ url(config('adminlte.login_url', 'login')) }}" class="text-center">
                         {{ trans('adminlte::adminlte.i_already_have_a_membership') }}
                     </a> 
                 </div>
