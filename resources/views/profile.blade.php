2fed3046622dd420569295f0807194724850152f
GTRebbeca <rebbecagt@gmail.com>
2017-12-14 12:51:40 -0600

    prueba acordión

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -308,89 +308,90 @@
 
 	    	@else
     			<!-- Custom Tabs -->
-		        <div class="nav-tabs-custom">
-		            <ul class="nav nav-tabs">
-		              	<li class="active"><a href="#tab_1" data-toggle="tab">Información personal</a></li>
-		              	<li><a href="#tab_2" data-toggle="tab">Familia</a></li>
-		              	<li><a href="#tab_3" onclick="initMapAddressUser();" data-toggle="tab">Dirección de usuario</a></li>
-		            </ul>
-		            <div class="tab-content">
-		              	<div class="tab-pane active" id="tab_1">
-		              		<br/>
+		       <!-- START ACCORDION & CAROUSEL-->
+      <h2 class="page-header">Bootstrap Accordion & Carousel</h2>
 
-			                <div class="row">
-			                	<div class="col-sm-4">
-			                		<div class="row">
-			                			<div class="col-sm-6" align="left"><b>Corre electrónico:</b></div>
-			                			<div class="col-sm-6" align="left">{{ $email }}</div>
-			                		</div>
-			                	</div>
-			                	<div class="col-sm-4">
-			                		<div class="row">
-			                			<div class="col-sm-6" align="left"><b>Nombre de usuario:</b></div>
-			                			<div class="col-sm-6" align="left">{{ $username }}</div>
-			                		</div>
-			                	</div>
-			                	<div class="col-sm-4">
-			                		<div class="row">
-			                			<div class="col-sm-6" align="left"><b>Edad:</b></div>
-			                			<div class="col-sm-6" align="left">{{ $age }}</div>
-			                		</div>
-			                	</div>
-			                </div>
-			                <br/>
-			                <div class="row">
-			                	<div class="col-sm-4">
-			                		<div class="row">
-			                			<div class="col-sm-6" align="left"><b>Ocupación:</b></div>
-			                			<div class="col-sm-6" align="left">{{ $occupation }}</div>
-			                		</div>
-			                	</div>
-			                	<div class="col-sm-4">
-			                		<div class="row">
-			                			<div class="col-sm-6" align="left"><b>Genero:</b></div>
-			                			<div class="col-sm-6" align="left">{{ $gender }}</div>
-			                		</div>
-			                	</div>
-			                	<div class="col-sm-4">
-			                		<div class="row">
-			                			<div class="col-sm-6" align="left"><b>Escolaridad:</b></div>
-			                			<div class="col-sm-6" align="left">{{ $scholarship }}</div>
-			                		</div>
-			                	</div>
-			                </div>
-			                <br/>
-			                <div class="row">
-			                	<div class="col-sm-4">
-			                		<div class="row">
-			                			<div class="col-sm-6" align="left"><b>Estado civil:</b></div>
-			                			<div class="col-sm-6" align="left">{{ $maritalstatus }}</div>
-			                		</div>
-			                	</div>
-			                	<div class="col-sm-4">
-			                		<div class="row">
-			                			<div class="col-sm-6" align="left"><b># Móvil:</b></div>
-			                			<div class="col-sm-6" align="left">{{ $mobile }}</div>
-			                		</div>
-			                	</div>
-			                	<div class="col-sm-4">
-			                		<div class="row">
-			                			<div class="col-sm-6" align="left"><b>Ultima modificación:</b></div>
-			                			<div class="col-sm-6" align="left">{{ $updated_at }}</div>
-			                		</div>
-			                	</div>
-			                </div>
-
-
-		              	</div>
-		              	<div class="tab-pane" id="tab_2">
-		                	space..
-		              	</div>
-		              	<div class="tab-pane" id="tab_3" style="height: 250px;">
-		              		<div id="mapAddressUser" ></div>
-		              	</div>
-		            </div>
-		        </div>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="box box-solid">
+            <div class="box-header with-border">
+              <h3 class="box-title">Collapsible Accordion</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              <div class="box-group" id="accordion">
+                <!-- we are adding the .panel class so bootstrap.js collapse plugin detects it -->
+                <div class="panel box box-primary">
+                  <div class="box-header with-border">
+                    <h4 class="box-title">
+                      <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
+                        Collapsible Group Item #1
+                      </a>
+                    </h4>
+                  </div>
+                  <div id="collapseOne" class="panel-collapse collapse in">
+                    <div class="box-body">
+                      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3
+                      wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum
+                      eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
+                      assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
+                      nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer
+                      farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
+                      labore sustainable VHS.
+                    </div>
+                  </div>
+                </div>
+                <div class="panel box box-danger">
+                  <div class="box-header with-border">
+                    <h4 class="box-title">
+                      <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
+                        Collapsible Group Danger
+                      </a>
+                    </h4>
+                  </div>
+                  <div id="collapseTwo" class="panel-collapse collapse">
+                    <div class="box-body">
+                      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3
+                      wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum
+                      eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
+                      assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
+                      nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer
+                      farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
+                      labore sustainable VHS.
+                    </div>
+                  </div>
+                </div>
+                <div class="panel box box-success">
+                  <div class="box-header with-border">
+                    <h4 class="box-title">
+                      <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+                        Collapsible Group Success
+                      </a>
+                    </h4>
+                  </div>
+                  <div id="collapseThree" class="panel-collapse collapse">
+                    <div class="box-body">
+                      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3
+                      wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum
+                      eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
+                      assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
+                      nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer
+                      farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
+                      labore sustainable VHS.
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+        <!-- /.col -->
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+      <!-- END ACCORDION & CAROUSEL-->
     		@endif
 
 
