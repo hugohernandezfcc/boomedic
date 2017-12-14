ff9b8640881c8fc8b6b572a28d0050683e129576
GTRebbeca <rebbecagt@gmail.com>
2017-12-13 18:01:31 -0600

    subiendo corrección profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -6,7 +6,6 @@
     <!-- <h1>Perfil de usuario</h1> -->
 @stop
 
-
 @section('content')
     <link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.0.1/min/dropzone.min.css" rel="stylesheet">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.2.0/min/dropzone.min.js"></script>
@@ -42,11 +41,6 @@
 				
 				    
 	</script>
-
-@stop
-
-@section('content')
-
 
 	<br/>
 
