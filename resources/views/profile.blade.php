daa50b208e6b88e5f45e75207dff170149816afa
GTRebbeca <rebbecagt@gmail.com>
2017-12-14 13:42:31 -0600

    colores de borde negro

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -314,7 +314,7 @@
             <div class="box-body">
               <div class="box-group" id="accordion">
                 <!-- we are adding the .panel class so bootstrap.js collapse plugin detects it -->
-                <div class="panel box box-default" style="border-top-color: #3c8dbc;">
+                <div class="panel box box-default" style="border-top-color: #111111;">
                   <div class="box-header with-border">
                     
                       <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" class="text-black">
@@ -406,7 +406,7 @@
                     </div>
                   </div>
                 </div>
-                <div class="panel box box-default">
+                <div class="panel box box-default" style="border-top-color: #111111;">
                   <div class="box-header with-border">
                 
                       <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree" onclick="initMapAddressUser();" class="text-black">
