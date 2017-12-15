ac62f7c095c0fb9089f17e7fdc7764cede1d022e
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2017-12-15 17:21:27 -0600

    probando preguntas en serie

--- resources/views/clinichistory.blade.php
+++ resources/views/clinichistory.blade.php
@@ -7,7 +7,8 @@
 @stop
 
 @section('content')
-
+	<script type="text/javascript" src="{{asset('jquery_steps/jquery.steps.js')}}"></script>
+	<script type="text/javascript" src="{{asset('jquery_steps/jquery.steps.js')}}"></script>
 	<div class="box">
 	  	<div class="box-header with-border">
 		    <h3 class="box-title">Historía clínica</h3>
@@ -25,13 +26,26 @@
                   </div>
                   <div id="collapseOne" class="panel-collapse collapse in">
                     <div class="box-body">
-                      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3
-                      wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum
-                      eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-                      assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
-                      nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer
-                      farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
-                      labore sustainable VHS.
+                      
+
+
+					    <div id="example-basic">
+					        <h3>Keyboard</h3>
+					        <section>
+					            <p>Try the keyboard navigation by clicking arrow left or right!</p>
+					        </section>
+					        <h3>Effects</h3>
+					        <section>
+					            <p>Wonderful transition effects.</p>
+					        </section>
+					        <h3>Pager</h3>
+					        <section>
+					            <p>The next and previous buttons help you to navigate through your content.</p>
+					        </section>
+					    </div>
+
+
+
                     </div>
                   </div>
                 </div>
@@ -79,6 +93,23 @@
 		</div>	  	
 	</div>
 
+	<script type="text/javascript">
+	    $("#example-basic").steps({
+	        headerTag: "h3",
+	        bodyTag: "section",
+	        transitionEffect: "slideLeft",
+	        autoFocus: true,
+	        labels: {
+		        cancel: "Cancelar",
+		        current: "Paso actual:",
+		        pagination: "Paginación",
+		        finish: "Final",
+		        next: "Siguiente",
+		        previous: "Regresar",
+		        loading: "Cargando ..."
+		    }
+	    });
+	</script>
 	 
 
 @stop
