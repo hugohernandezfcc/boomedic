894bc32507b06f7d63dd3ec5f3317cd68fda9cf6
unknown <axel.valle@doitcloud.consulting>
2017-12-20 16:50:23 -0600

    Regreso a la versión con botón mediano

--- resources/views/vendor/adminlte/login.blade.php
+++ resources/views/vendor/adminlte/login.blade.php
@@ -87,7 +87,7 @@
         <!-- /.login-box-body -->
         <div class="row">
             <div class="col-xs-6">
-                <div class="fb-login-button" data-size="small" data-button-type="login_with" 
+                <div class="fb-login-button" data-size="medium" data-button-type="login_with" 
                     data-scope="public_profile,email" onlogin="checkLoginState();"></div>
             </div>
         </div>
