eeb22ede65b028086a2679c4c4420ad9cb66eb8f
GTRebbeca <rebbecagt@gmail.com>
2017-12-20 17:13:05 -0600

    arreglando botones del login 4

--- resources/views/vendor/adminlte/login.blade.php
+++ resources/views/vendor/adminlte/login.blade.php
@@ -53,17 +53,18 @@
                 <button type="submit" class="btn btn-secondary btn-block btn-flat">{{ trans('adminlte::adminlte.sign_in') }}</button>
             </form>
             <div class="auth-links">
-            <div class="form-group">
-                
+            <div class="row">
+                <div class="col-sm-6" align="center">
                     <a href="{{ url(config('adminlte.password_reset_url', 'password/reset')) }}" class="btn btn-default btn-block btn-flat">
                         {{ trans('adminlte::adminlte.i_forgot_my_password') }}
                     </a>
-                
-               
+                </div>
                     @if (config('adminlte.register_url', 'register'))
+                <div class="col-sm-6" align="center">
                     <a href="{{ url(config('adminlte.register_url', 'register')) }}" class="btn btn-default btn-block btn-flat">
                         {{ trans('adminlte::adminlte.register_a_new_membership') }}
                     </a>
+                </div>
                     @endif
                 
             </div>
