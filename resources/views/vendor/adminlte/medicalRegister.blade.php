29ab9ada2e4ac5131a27b574a40d7051f22205a5
GTRebbeca <rebbecagt@gmail.com>
2017-12-20 17:19:23 -0600

    subiendo cambios botones links del login

--- resources/views/vendor/adminlte/medicalRegister.blade.php
+++ resources/views/vendor/adminlte/medicalRegister.blade.php
@@ -93,7 +93,7 @@
 
             </form>
             <div class="col-sm-6">
-            <a href="{{ url(config('adminlte.login_url', 'login')) }}" class="class="btn btn-default btn-block btn-flat>{{ trans('adminlte::adminlte.i_already_have_a_membership') }}</a> 
+            <a href="{{ url(config('adminlte.login_url', 'login')) }}" class="btn btn-default btn-block btn-flat">{{ trans('adminlte::adminlte.i_already_have_a_membership') }}</a> 
             <div class="auth-links">
                  <br/>
                 <!-- <a href="{{ url(config('adminlte.login_url', 'login')) }}" class="text-center">{{ trans('adminlte::adminlte.i_am_a_doctor') }}</a> -->
