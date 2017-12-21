e60278f7874259730d3a7ef3c21e466ce1ed38bd
GTRebbeca <rebbecagt@gmail.com>
2017-12-21 12:44:43 -0600

    corrigiendo errores de visualización

--- resources/views/vendor/adminlte/page.blade.php
+++ resources/views/vendor/adminlte/page.blade.php
@@ -77,13 +77,13 @@
                                     <div class="pull-center">
                                  @if(config('adminlte.logout_method') == 'GET' || !config('adminlte.logout_method') && version_compare(\Illuminate\Foundation\Application::VERSION, '5.3.0', '<'))
                                 <a href="{{ url(config('adminlte.logout_url', 'auth/logout')) }}" class="btn btn-secondary btn-block btn-flat">
-                                    <i class="fa fa-fw fa-sign-out"></i> {{ trans('adminlte::adminlte.log_out') }}
+                                    <i class="fa fa-sign-out"></i> {{ trans('adminlte::adminlte.log_out') }}
                                 </a>
                             @else
                                 <a href="#"
                                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
                                  class="btn btn-secondary btn-block btn-flat">
-                                    <i class="fa fa-fw fa-sign-out"></i> {{ trans('adminlte::adminlte.log_out') }}
+                                    <i class="fa fa-sign-out"></i> {{ trans('adminlte::adminlte.log_out') }}
                                 </a>
                                 <form id="logout-form" action="{{ url(config('adminlte.logout_url', 'auth/logout')) }}" method="POST" style="display: none;">
                                     @if(config('adminlte.logout_method'))
