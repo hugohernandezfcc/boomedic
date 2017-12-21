615da26cf03634d2c8ae2676c0b5fa16e37594ea
GTRebbeca <rebbecagt@gmail.com>
2017-12-21 12:08:30 -0600

    profile 5

--- resources/views/vendor/adminlte/page.blade.php
+++ resources/views/vendor/adminlte/page.blade.php
@@ -77,7 +77,7 @@
                                     <div class="pull-center">
                                       <a href="#" class="btn btn-secondary btn-block btn-flat"><i class="fa fa-fw fa-user"></i> Perfil</a>
                                     </div>
-                                    <div class="pull-center">
+                                    <div class="pull-right">
                                  @if(config('adminlte.logout_method') == 'GET' || !config('adminlte.logout_method') && version_compare(\Illuminate\Foundation\Application::VERSION, '5.3.0', '<'))
                                 <a href="{{ url(config('adminlte.logout_url', 'auth/logout')) }}" class="btn btn-secondary btn-block btn-flat">
                                     <i class="fa fa-fw fa-power-off"></i> {{ trans('adminlte::adminlte.log_out') }}
