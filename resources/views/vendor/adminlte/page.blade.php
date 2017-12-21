78d61bc90e67d4c7b7bea5085391bbcd074d289e
GTRebbeca <rebbecagt@gmail.com>
2017-12-21 10:58:33 -0600

    agregando datos

--- resources/views/vendor/adminlte/page.blade.php
+++ resources/views/vendor/adminlte/page.blade.php
@@ -59,16 +59,16 @@
                     <ul class="nav navbar-nav">
                         <li class="dropdown user user-menu">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                                  <img src="dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-                                  <span class="hidden-xs">Alexander Pierce</span>
+                                  <img src="{{ $photo }}" class="user-image" alt="User Image">
+                                  <span class="hidden-xs">{{ $name }}</span>
                                 </a>
                                 <ul class="dropdown-menu">
                                   <!-- User image -->
                                   <li class="user-header">
-                                    <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                                    <img src="{{ $photo }}" class="img-circle" alt="User Image">
 
                                     <p>
-                                      Alexander Pierce - Web Developer
+                                        {{ $name }}
                                       <small>Member since Nov. 2012</small>
                                     </p>
                                   </li>
