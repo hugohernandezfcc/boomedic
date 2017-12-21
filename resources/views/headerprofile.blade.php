77377c802a3ca681844546ce9c765a2af81041b3
GTRebbeca <rebbecagt@gmail.com>
2017-12-21 13:12:00 -0600

    estilo de estiquetas

--- resources/views/headerprofile.blade.php
+++ resources/views/headerprofile.blade.php
@@ -2,16 +2,14 @@
 <head></head>
 <body>
 	<div class="lockscreen-item" style="margin: 10px 0 30px auto;">
-	    <!-- lockscreen image -->
-	    <!-- /.lockscreen-image -->
 
 	    <!-- lockscreen credentials (contains the form) -->
 	    <form class="lockscreen-credentials" action="create" method="get" id="form_profile">
 	    	{{ csrf_field() }}
 	      	<div class="input-group">
-	        	<div class="form-control"><label id="labeltext">Agregar</label></div>
+	        	<div class="pull-left"><label id="labeltext">Agregar</label></div>
 	        	<input type="hidden" name="id" value="{{ $userId }}">
-	        	<div class="input-group-btn" id="div_profile">
+	        	<div class="input-group-btn" id="div_profile" style=" border-radius: 50%;">
 		          	<button type="submit" class="btn">
 		          		<i class="fa fa-plus text-muted" id="i_button"></i>
 		          	</button>
