7911317c7a104aeb6e7738fa2a74b52a2bf1ec45
unknown <axel.valle@doitcloud.consulting>
2017-12-21 17:32:16 -0600

    mas test

--- resources/views/vendor/adminlte/login.blade.php
+++ resources/views/vendor/adminlte/login.blade.php
@@ -1,5 +1,7 @@
 @extends('adminlte::master')
-
+<head>
+    <meta name="google-signin-client_id" content="627103508601-mstgbse0thdiv2qcn2dop6pn0u28gc31.apps.googleusercontent.com">
+</head>
 
 @section('adminlte_css')
     <link rel="stylesheet" href="{{ asset('vendor/adminlte/plugins/iCheck/square/blue.css') }}">
@@ -89,7 +91,7 @@
                     data-scope="public_profile,email" onlogin="checkLoginState();"></div>
             </div>
             <div class="col-xs-6">
-                <div class="g-signin2" lang="es-419" data-width="165" data-height="27" data-longtitle="true" data-scope="https://www.googleapis.com/auth/plus.login" data-clientid="627103508601-mstgbse0thdiv2qcn2dop6pn0u28gc31.apps.googleusercontent.com  "data-onsuccess="onSignInG"></div>
+                <div class="g-signin2" lang="es-419" data-width="165" data-height="27" data-longtitle="true" data-clientid="627103508601-mstgbse0thdiv2qcn2dop6pn0u28gc31.apps.googleusercontent.com  "data-onsuccess="onSignInG"></div>
             </div>
         </div>
     </div><!-- /.login-box -->
