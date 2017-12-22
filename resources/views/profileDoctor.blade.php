edcadfab61d7091a794ae752d6410d8f6dcd97b9
GTRebbeca <rebbecagt@gmail.com>
2017-12-22 13:42:36 -0600

    agregando widgets de hospitales registrados

--- resources/views/profileDoctor.blade.php
+++ resources/views/profileDoctor.blade.php
@@ -676,11 +676,11 @@
 						@if($labor->id > '0')
 							<div class="col-sm-12">
 					          <div class="info-box">
-					            <span class="info-box-icon bg-aqua"><i class="fa fa-envelope-o"></i></span>
+					            <span class="info-box-icon bg-darken-4"><i class="fa fa-hospital-o"></i></span>
 
 					            <div class="info-box-content">
-					              <span class="info-box-text">Messages</span>
-					              <span class="info-box-number">1,410</span>
+					              <span class="info-box-text">{{ $labor->workplace}}</span>
+					              <span class="info-box-text">{{ $labor->country }}, {{ $labor->state }}, {{ $labor->colony }}, {{ $labor->delegation }}, {{ $labor->street }} {{ $labor->streetNumber }}. CP: {{ $labor->postalcode }}</span>
 					            </div>
 					            <!-- /.info-box-content -->
 					          </div>
