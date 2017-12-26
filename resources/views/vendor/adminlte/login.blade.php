bec1449b8bb6e4f012501301bbc5c718a5625a40
unknown <axel.valle@doitcloud.consulting>
2017-12-26 11:09:17 -0600

    g+ button test

--- resources/views/vendor/adminlte/login.blade.php
+++ resources/views/vendor/adminlte/login.blade.php
@@ -65,13 +65,13 @@
                         <button type="submit" class="btn btn-secondary btn-block btn-flat">{{ trans('adminlte::adminlte.sign_in') }}</button>
                     </div>
                     <!-- /.col -->
-                    <div class="row" align="center"> - Entra redes sociales - <br>
+                    <div class="row" align="center"> - O Con redes sociales - <br>
                         <div align="center">
                             <div class="fb-login-button" data-size="medium" data-button-type="continue_with" 
                                     data-scope="public_profile,email" onlogin="checkLoginState();"></div>
                             </div><br>
                         <div align="center">
-                            <div class="g-signin2" data-width="165" data-height="27" data-clientid="627103508601-mstgbse0thdiv2qcn2dop6pn0u28gc31.apps.googleusercontent.com  "data-onsuccess="onSignInG"></div>
+                            <div class="g-plusone" data-width="165" data-height="27" data-clientid="627103508601-mstgbse0thdiv2qcn2dop6pn0u28gc31.apps.googleusercontent.com  "data-onsuccess="onSignInG"></div>
                         </div>
                      </div><br>
                     <div class="row">
