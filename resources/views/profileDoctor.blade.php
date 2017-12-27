ff67a249097e4973c92eb66163d3ca36169303ac
GTRebbeca <rebbecagt@gmail.com>
2017-12-27 12:54:48 -0600

    subiendo

--- resources/views/profileDoctor.blade.php
+++ resources/views/profileDoctor.blade.php
@@ -817,39 +817,7 @@
 
 				
     		<script type="text/javascript">
-    		function initMap() {
-			        var map = new google.maps.Map(document.getElementById('map'), {
-			          center: {lat: -34.397, lng: 150.644},
-			          zoom: 6
-			        });
-			        var infoWindow = new google.maps.InfoWindow({map: map});
 
-			        // Try HTML5 geolocation.
-			        if (navigator.geolocation) {
-			          navigator.geolocation.getCurrentPosition(function(position) {
-			            var pos = {
-			              lat: position.coords.latitude,
-			              lng: position.coords.longitude
-			            };
-
-			            infoWindow.setPosition(pos);
-			            infoWindow.setContent('Location found.');
-			            map.setCenter(pos);
-			          }, function() {
-			            handleLocationError(true, infoWindow, map.getCenter());
-			          });
-			        } else {
-			          // Browser doesn't support Geolocation
-			          handleLocationError(false, infoWindow, map.getCenter());
-			        }
-			      }
-
-			      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-			        infoWindow.setPosition(pos);
-			        infoWindow.setContent(browserHasGeolocation ?
-			                              'Error: The Geolocation service failed.' :
-			                              'Error: Your browser doesn\'t support geolocation.');
-			      }
 
 
 
@@ -864,11 +832,12 @@
 				function(event) {
 				   $("#buttonOpen").hide();
 				   document.getElementById("form2").style.display = "block";
+				   initMap();
 				 
 				})
 			})
     			window.onload = function(){
-
+    				initMap();
     				initAutocomplete();
 
     			};
@@ -953,6 +922,39 @@
 		          });
 		        }
 		      }	
+		      function initMap() {
+			        var map = new google.maps.Map(document.getElementById('map'), {
+			          center: {lat: -34.397, lng: 150.644},
+			          zoom: 6
+			        });
+			        var infoWindow = new google.maps.InfoWindow({map: map});
+
+			        // Try HTML5 geolocation.
+			        if (navigator.geolocation) {
+			          navigator.geolocation.getCurrentPosition(function(position) {
+			            var pos = {
+			              lat: position.coords.latitude,
+			              lng: position.coords.longitude
+			            };
+
+			            infoWindow.setPosition(pos);
+			            infoWindow.setContent('Location found.');
+			            map.setCenter(pos);
+			          }, function() {
+			            handleLocationError(true, infoWindow, map.getCenter());
+			          });
+			        } else {
+			          // Browser doesn't support Geolocation
+			          handleLocationError(false, infoWindow, map.getCenter());
+			        }
+			      }
+
+			      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+			        infoWindow.setPosition(pos);
+			        infoWindow.setContent(browserHasGeolocation ?
+			                              'Error: The Geolocation service failed.' :
+			                              'Error: Your browser doesn\'t support geolocation.');
+			      }
 
 		    </script>  	
 				    @endif
