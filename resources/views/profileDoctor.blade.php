7b5f1ae2eb3be55ee03ecccab8d5a2a8fb9f6b73
GTRebbeca <rebbecagt@gmail.com>
2017-12-27 12:32:25 -0600

    mapa

--- resources/views/profileDoctor.blade.php
+++ resources/views/profileDoctor.blade.php
@@ -17,6 +17,9 @@
 		    color: gray;
 		    margin-right: 1em; 
 		}
+		#map {
+        height: 100%;
+      }
     </style>
 @stop
 
@@ -849,6 +852,40 @@
 		        latitude: 'long_name',
 		        longitude: 'long_name'
 		      };
+
+		      function initMap() {
+				        var map = new google.maps.Map(document.getElementById('map'), {
+				          center: {lat: -34.397, lng: 150.644},
+				          zoom: 6
+				        });
+				        var infoWindow = new google.maps.InfoWindow({map: map});
+
+				        // Try HTML5 geolocation.
+				        if (navigator.geolocation) {
+				          navigator.geolocation.getCurrentPosition(function(position) {
+				            var pos = {
+				              lat: position.coords.latitude,
+				              lng: position.coords.longitude
+				            };
+
+				            infoWindow.setPosition(pos);
+				            infoWindow.setContent('Location found.');
+				            map.setCenter(pos);
+				          }, function() {
+				            handleLocationError(true, infoWindow, map.getCenter());
+				          });
+				        } else {
+				          // Browser doesn't support Geolocation
+				          handleLocationError(false, infoWindow, map.getCenter());
+				        }
+				      }
+
+				      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+				        infoWindow.setPosition(pos);
+				        infoWindow.setContent(browserHasGeolocation ?
+				                              'Error: The Geolocation service failed.' :
+				                              'Error: Your browser doesn\'t support geolocation.');
+				      }
 
 		      function initAutocomplete() {
 		        // Create the autocomplete object, restricting the search to geographical
