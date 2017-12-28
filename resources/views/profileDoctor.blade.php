a021575e9a29a99c3190c94893bfdbae0196d955
GTRebbeca <rebbecagt@gmail.com>
2017-12-28 16:25:43 -0600

    corrección

--- resources/views/profileDoctor.blade.php
+++ resources/views/profileDoctor.blade.php
@@ -533,7 +533,7 @@
 									   @if($loop->iteration > 2)
 									   <div class="col-sm-12" style="text-align: right;">
 									   	<form action="/doctor/laborInformation/{{$userId}}" method="post">
-									   	<button type="submit" class="btn btn-secondary"><i class="fa fa-plus"> Agregar otro centro</button>
+									   	<button type="submit" class="btn btn-secondary"><i class="fa fa-plus"></i>Agregar otro centro</button>
 									   </form>
 									   	<a href="{{ url('doctor/laborInformationView') }}/{{ $userId }}" class="btn btn-default">
 									   Ver todos... <i class="fa fa-arrow-right"></i>
