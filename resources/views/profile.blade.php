31d1d9265c9b7d466e3d888255ba7bd17684998a
GTRebbeca <rebbecagt@gmail.com>
2017-12-29 13:20:43 -0600

    cambios

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -437,7 +437,7 @@
                  <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree" onclick="initMapAddressUser();" aria-expanded="false" aria-controls="collapseThree" class="a text-black" style="display:block; height:100%; width:100%;font-size: 17px;">	
                         Dirección de usuario      
 
-                        @php echo session()->get('it')[0]; @endphp   
+                        @php echo session()->get('it')[5]; @endphp   
                   </a> 
                   </h4> 
                   </div>
