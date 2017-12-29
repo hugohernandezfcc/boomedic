e94e798a52d4089c1ba5650f020296d06a42697b
GTRebbeca <rebbecagt@gmail.com>
2017-12-29 12:50:46 -0600

    arreglando vista

--- app/Http/Controllers/profile.php
+++ app/Http/Controllers/profile.php
@@ -65,19 +65,19 @@ class profile extends Controller
      */
     public function show($id)
     {
-        $users = DB::table('users')->where('id', Auth::id() )->get();
-        return view('profile', [
-                'name' => DB::table('users')->where('id', Auth::id() )->value('name'),
- $join = DB::table('professional_information')
+         $join = DB::table('professional_information')
             ->join('labor_information', 'professional_information.id', '=', 'labor_information.profInformation')
             ->join('users', 'professional_information.user', '=', 'users.id')
             ->select('labor_information.*', 'users.name', 'professional_information.specialty')
             ->get();
-
-             foreach($join as $labor){
+                         foreach($join as $labor){
             $it[] = '["'.$labor->specialty.'","'.$labor->latitude.'","'.$labor->longitude.'", "'.$labor->name.'", "'.$labor->workplace.'"],'; 
              }
              Session(['it' => $it]);
+
+        $users = DB::table('users')->where('id', Auth::id() )->get();
+        return view('profile', [
+                'name' => DB::table('users')->where('id', Auth::id() )->value('name'),
                  /** SYSTEM INFORMATION */
 
                 'userId'        => Auth::id(),
