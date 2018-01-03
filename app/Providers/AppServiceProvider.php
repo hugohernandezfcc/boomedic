e22bf1acd069c65ba59edf4e5347723e96b60e9d
GTRebbeca <rebbecagt@gmail.com>
2018-01-03 12:44:55 -0600

    subiendo cambios

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(Dispatcher $events)
     {
         
+        \Carbon::setLocale(config('app.locale'));
         \URL::forceScheme('https');
 
 
