a4c2bc879fe6249d8b4034ca9e0c0a562fc30d44
unknown <axel.valle@doitcloud.consulting>
2018-01-04 11:24:25 -0600

    z?

--- public/js/FacebookLogin.js
+++ public/js/FacebookLogin.js
@@ -39,7 +39,7 @@ function checkLoginState() {
     })
 }
 
-function RegistroporFB(){
+function fbRegister(){
     FB.getLoginStatus(function (response) {
         if (response.status == "connected"){
             FB.api('/me?fields=id,name,first_name,last_name,email,picture.type(large)', function (userData){
