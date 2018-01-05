c6f00460401a48bad885adcdc6fdcc3d7b96a27e
unknown <axel.valle@doitcloud.consulting>
2018-01-05 15:14:56 -0600

    test acordeon

--- resources/views/vendor/adminlte/login.blade.php
+++ resources/views/vendor/adminlte/login.blade.php
@@ -67,91 +67,69 @@
                     </div><br><br><br>
                     <!-- /.col -->
                     <div class="box" align="center" style="border-style: none; box-shadow: none;">     - REDES SOCIALES -  <br>
-                        <div class="row" align="center">
-                            <div class="fb-login-button" id="botonfacebook_tocustom" data-size="medium" data-button-type="login_with" 
+                        <div class="box-group" id="accordion">
+                        <div class="panel box box-primary">
+                          <div class="box-header with-border">
+                            <h4 class="box-title">
+                              <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
+                                Facebook
+                              </a>
+                            </h4>
+                          </div>
+                          <div id="collapseOne" class="panel-collapse collapse in">
+                            <div class="box-body">
+                                <div class="row" align="center">
+                                    <div class="fb-login-button" id="botonfacebook_tocustom" data-size="medium" data-button-type="login_with" 
                                     data-scope="public_profile,email" onlogin="checkLoginState();"></div>
-                        </div><br>
-                        <div class="row" align="center">
-                            <div class="g-signin2"  data-width="165" data-height="27" data-clientid="627103508601-mstgbse0thdiv2qcn2dop6pn0u28gc31.apps.googleusercontent.com  "data-onsuccess="onSignInG"></div><br>
+                                </div><br>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="panel box box-danger">
+                          <div class="box-header with-border">
+                            <h4 class="box-title">
+                              <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
+                                Google
+                              </a>
+                            </h4>
+                          </div>
+                          <div id="collapseTwo" class="panel-collapse collapse">
+                            <div class="box-body">
+                                <div class="row" align="center">
+                                    <div class="g-signin2"  data-width="165" data-height="27" data-clientid="627103508601-mstgbse0thdiv2qcn2dop6pn0u28gc31.apps.googleusercontent.com  "data-onsuccess="onSignInG"></div><br>
                             <!--<div class="g-plusone" id="myButton" data-onload="renderG"></div>-->
+                                </div>
+                            </div>
+                          </div>
                         </div>
-                        <div class="row">
-                            <script type="text/javascript" src="//platform.linkedin.com/in.js" async defer>
-                                api_key: 78ezayhtq8yi4u
-                                authorize: true
-                                onLoad: onLinkedInLoad
-                                scope: r_basicprofile r_emailaddress
-                                lang: es_ES
-                            </script>
-                            <div align="center"><script type="in/Login"></script></div>
+                        <div class="panel box box-success">
+                          <div class="box-header with-border">
+                            <h4 class="box-title">
+                              <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+                                LinkedIn
+                              </a>
+                            </h4>
+                          </div>
+                          <div id="collapseThree" class="panel-collapse collapse">
+                            <div class="box-body">
+                                <div class="row">
+                                    <script type="text/javascript" src="//platform.linkedin.com/in.js" async defer>
+                                        api_key: 78ezayhtq8yi4u
+                                        authorize: true
+                                        onLoad: onLinkedInLoad
+                                        scope: r_basicprofile r_emailaddress
+                                        lang: es_ES
+                                    </script>
+                                    <div align="center"><script type="in/Login"></script></div>
+                                </div>
+                            </div>
+                          </div>
                         </div>
+                      </div>
                         <div class="overlay" id="cargar">
                             <div id="carga2"class="fa fa-refresh fa-spin"></div>
                         </div>
                      </div>
-                     <div class="box-group" id="accordion">
-                <!-- we are adding the .panel class so bootstrap.js collapse plugin detects it -->
-                <div class="panel box box-primary">
-                  <div class="box-header with-border">
-                    <h4 class="box-title">
-                      <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
-                        Collapsible Group Item #1
-                      </a>
-                    </h4>
-                  </div>
-                  <div id="collapseOne" class="panel-collapse collapse in">
-                    <div class="box-body">
-                      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3
-                      wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum
-                      eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-                      assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
-                      nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer
-                      farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
-                      labore sustainable VHS.
-                    </div>
-                  </div>
-                </div>
-                <div class="panel box box-danger">
-                  <div class="box-header with-border">
-                    <h4 class="box-title">
-                      <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
-                        Collapsible Group Danger
-                      </a>
-                    </h4>
-                  </div>
-                  <div id="collapseTwo" class="panel-collapse collapse">
-                    <div class="box-body">
-                      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3
-                      wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum
-                      eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-                      assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
-                      nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer
-                      farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
-                      labore sustainable VHS.
-                    </div>
-                  </div>
-                </div>
-                <div class="panel box box-success">
-                  <div class="box-header with-border">
-                    <h4 class="box-title">
-                      <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
-                        Collapsible Group Success
-                      </a>
-                    </h4>
-                  </div>
-                  <div id="collapseThree" class="panel-collapse collapse">
-                    <div class="box-body">
-                      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3
-                      wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum
-                      eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-                      assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
-                      nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer
-                      farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
-                      labore sustainable VHS.
-                    </div>
-                  </div>
-                </div>
-              </div>
                 </div>
             </form>
             <div class="" id="loginload">
