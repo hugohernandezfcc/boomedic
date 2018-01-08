0c3efc58b7be3918159818c5d6f3b0bbe9ae97e0
GTRebbeca <rebbecagt@gmail.com>
2018-01-08 13:14:14 -0600

    botones

--- resources/views/profileDoctor.blade.php
+++ resources/views/profileDoctor.blade.php
@@ -896,10 +896,11 @@
 					            <div class="info-box-content sm">
 					              <b>{{ $labor->workplace}}</b><br/>
 					              <span class="text-black">{{ $labor->country }}, {{ $labor->state }}, {{ $labor->colony }}, {{ $labor->delegation }}, {{ $labor->street }} {{ $labor->streetNumber }}. Código Postal: {{ $labor->postalcode }}</span>
-					           <div align="right">
-					          	<a href = "{{ url('doctor/delete') }}/{{ $labor->id }}" class="btn" onclick ="return confirm('¿Seguro desea eliminar este lugar?')"><i class="fa fa-trash text-muted"></i></a></div>
+
 					            </div>
 					            <!-- /.info-box-content -->
+					         <div align="right">
+					          	<a href = "{{ url('doctor/delete') }}/{{ $labor->id }}" class="btn" onclick ="return confirm('¿Seguro desea eliminar este lugar?')"><i class="fa fa-trash text-muted"></i></a></div>
 					          </div>
 					        </div>
 					   @endif	
@@ -1225,9 +1226,10 @@
 					            <div class="info-box-content">
 					              <b>{{ $labor->workplace}}</b><br/>
 					              <span class="text-black">{{ $labor->country }}, {{ $labor->state }}, {{ $labor->colony }}, {{ $labor->delegation }}, {{ $labor->street }} {{ $labor->streetNumber }}. Código Postal: {{ $labor->postalcode }}</span>
-					            <div align="right">
-					          	<a href = "{{ url('doctor/delete') }}/{{ $labor->id }}" class="btn" onclick ="return confirm('¿Seguro desea eliminar este lugar?')"><i class="fa fa-trash text-muted"></i></a></div>
+
 					            </div>
+					           <div align="right">
+					          	<a href = "{{ url('doctor/delete') }}/{{ $labor->id }}" class="btn" onclick ="return confirm('¿Seguro desea eliminar este lugar?')"><i class="fa fa-trash text-muted"></i></a></div>
 					            <!-- /.info-box-content -->
 					          </div>
 
