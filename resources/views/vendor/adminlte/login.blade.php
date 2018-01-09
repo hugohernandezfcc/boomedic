26471666a03e1a8d3b0e3cb03bc6cc83a4b3b069
unknown <axel.valle@doitcloud.consulting>
2018-01-09 12:51:37 -0600

    xnfnf

--- resources/views/vendor/adminlte/login.blade.php
+++ resources/views/vendor/adminlte/login.blade.php
@@ -21,16 +21,6 @@
 
 
 @section('body')
-    <div id="myModal" class="modal2">
-        <div class="modal-content2">
-            <div class="modal-header2">
-                <span class="close2">&times;</span>
-                <h3>Error: </h3>
-            </div>
-        <div class="modal-body2">
-            <h4>Los datos no corresponden con nuestra base de datos, asegúrese de estar registrado.</h4>
-        </div>
-  </div>
     </div>
     <div class="login-box">
         <div class="login-logo">
@@ -39,6 +29,16 @@
         <!-- /.login-logo -->
         <div class="login-box-body box">
             <p class="login-box-msg">{{ trans('adminlte::adminlte.login_message') }}</p>
+                <div id="myModal" class="modal2">
+            <div class="modal-content2">
+                <div class="modal-header2">
+                    <span class="close2">&times;</span>
+                    <h3>Error: </h3>
+                </div>
+                <div class="modal-body2">
+                    <h4>Los datos no corresponden con nuestra base de datos, asegúrese de estar registrado.</h4>
+                </div>
+            </div>
             <form action="{{ url(config('adminlte.login_url', 'login')) }}" method="post">
                 {!! csrf_field() !!}
 
