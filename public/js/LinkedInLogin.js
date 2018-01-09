a705ab5aeed38d9a4632f29400d831da0ae70b86
unknown <axel.valle@doitcloud.consulting>
2018-01-09 13:26:18 -0600

    jdfjdfjdf

--- public/js/LinkedInLogin.js
+++ public/js/LinkedInLogin.js
@@ -51,6 +51,7 @@ var span = document.getElementsByClassName("close-danger2")[0];
                     //console.log(errorThrown);
                     document.getElementById("loginload").removeAttribute("class");
                     document.getElementById("loginload2").removeAttribute("class");
+                    document.getElementById('collapseThree').classList.add("collapse");
                     modal.style.display = "block";
                 }
         });
