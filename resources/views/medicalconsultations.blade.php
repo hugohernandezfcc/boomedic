96730a68ca0f2e34629c23cb9ab14188c33ff48c
GTRebbeca <rebbecagt@gmail.com>
2018-01-09 16:55:07 -0600

    show alert cuando no hay citas

--- resources/views/medicalconsultations.blade.php
+++ resources/views/medicalconsultations.blade.php
@@ -240,9 +240,9 @@
 
   </script>
  @if($appointments->isEmpty())
- <div  class="box-body" align="center">
-   No hay citas registradas para los próximos días...
- </div>
+<div class="alert alert-info alert-dismissible" id="alert">
+                <h4><i class="icon fa fa-info"></i> No hay citas registradas para los próximos días...</h4>               
+</div>
  @else
       <div class="box-group" id="accordion">
                 <div class="panel box box-default" style="border-top-color: gray;">
@@ -431,6 +431,9 @@
     </form>
 
     <script type="text/javascript">
+    $("#alert").fadeTo(3000, 500).fadeOut(500, function(){
+    $("#alert").fadeOut(500);
+});
       function infoSelect(){
         var x = document.getElementById("mySelect");
         for (var i = 0; i < specialities.length; i++) {
