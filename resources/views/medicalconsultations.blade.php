b63b9f8ff2bfadd282e2a7efade336f4cdf22471
GTRebbeca <rebbecagt@gmail.com>
2018-01-11 14:20:34 -0600

    change

--- resources/views/medicalconsultations.blade.php
+++ resources/views/medicalconsultations.blade.php
@@ -229,9 +229,9 @@
             </div>
              @else
              <div class="box-group" id="accordion">
-                 <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="false" class="a text-black" style="font-size: 13px;">
+                 <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="false" class="a text-black" style="font-size: 12px;">
                  <div class="box-header with-border"> 
-                   <div align="left"><i class="fa fa-chevron-down text-muted"></i> Citas médicas registradas</div>
+                   <div align="left"><i class="fa fa-chevron-down text-muted"></i> <b>Citas médicas registradas</b></div>
                     </div> 
                  </a>
                   <div id="collapseOne" class="panel-collapse collapse" >
