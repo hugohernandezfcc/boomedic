0b672c248893a6b52c3c20041f74e4dc199210d8
unknown <axel.valle@doitcloud.consulting>
2018-01-12 13:16:33 -0600

    v0.0.3

--- app/Http/Controllers/Api/ProfessionalInfoController.php
+++ app/Http/Controllers/Api/ProfessionalInfoController.php
@@ -25,13 +25,13 @@ class ProfessionalInfoController extends Controller
     public function store(StoreProfInfoRequest $request){
     	$pInfo = new professional_information;
 
-    	$pInfo->specialty = ($request->has('specialty')) ? $request->specialty;
-        $pInfo->schoolOfMedicine = ($request->has('schoolOfMedicine')) ? $request->schoolOfMedicine;
-        $pInfo->facultyOfSpecialization = ($request->has('facultyOfSpecialization')) ? $request->facultyOfSpecialization : ;
-        $pInfo->practiseProfessional = ($request->has('practiseProfessional')) ? $request->practiseProfessional;
+    	$pInfo->specialty = ($request->has('specialty')) ? $request->specialty : $pInfo->specialty;
+        $pInfo->schoolOfMedicine = ($request->has('schoolOfMedicine')) ? $request->schoolOfMedicine : $pInfo->schoolOfMedicine;
+        $pInfo->facultyOfSpecialization = ($request->has('facultyOfSpecialization')) ? $request->facultyOfSpecialization : $pInfo->facultyOfSpecialization;
+        $pInfo->practiseProfessional = ($request->has('practiseProfessional')) ? $request->practiseProfessional : $pInfo->practiseProfessional;
         $pInfo->user = $request->user;
-        $pInfo->professional_license = $request->professional_license;
-        $pInfo->medical_society = ($request->has('medical_society')) ? $request->medical_society;
+        $pInfo->professional_license = $request->professional_license : $pInfo->specialty;
+        $pInfo->medical_society = ($request->has('medical_society')) ? $request->medical_society : $pInfo->medical_society;
 
         $pInfo->save();
 
