44cbb02eef0fdaca1d78161379cef6409656015f
GTRebbeca <rebbecagt@gmail.com>
2018-01-12 17:00:23 -0600

    cambios para rangos de cita

--- resources/views/workboard.blade.php
+++ resources/views/workboard.blade.php
@@ -24,48 +24,62 @@
   		<label>Seleccione los días de la semana que va a trabajar:</label><br/>
   	    </div>
   	<div class="col-sm-8">	
-  		<div class="btn-group" data-toggle="buttons">
+  		<div class="btn-group" data-toggle="buttons" style="font-size: 12px">
 
   			<label class="btn btn-secondary">
 				<input type="checkbox" value="Domingo" name="Domingo">
 				<span class="glyphicon glyphicon-ok"></span>
-				 <b>Domingo</b>
+				 <b>Dom</b>
 			</label>		
-	  		<label class="btn btn-secondary">
+	  		<label class="btn btn-default">
 				<input type="checkbox" value="Lunes" name="Lunes">
 				<span class="glyphicon glyphicon-ok"></span>
-				<b>Lunes</b>
+				<b>Lun</b>
 			</label>
-			<label class="btn btn-secondary">
+			<label class="btn btn-default">
 				<input type="checkbox" value="Martes" name="Martes">
 				<span class="glyphicon glyphicon-ok"></span>
-				<b>Martes</b>
+				<b>Mar</b>
 			</label>
-			<label class="btn btn-secondary">
+			<label class="btn btn-default">
 				<input type="checkbox" value="Miercoles" name="Miercoles">
 				<span class="glyphicon glyphicon-ok"></span>
-				<b>Miercoles</b>
+				<b>Mier</b>
 			</label>
-			<label class="btn btn-secondary">
+			<label class="btn btn-default">
 				<input type="checkbox" value="Jueves" name="Jueves">
 				<span class="glyphicon glyphicon-ok"></span>
-				<b>Jueves</b>
+				<b>Jue</b>
 			</label>
-			<label class="btn btn-secondary">
+			<label class="btn btn-default">
 				<input type="checkbox" value="Viernes" name="Viernes">
 				<span class="glyphicon glyphicon-ok"></span>
-				<b>Viernes</b>
+				<b>Vier</b>
 			</label>
-			<label class="btn btn-secondary">
+			<label class="btn btn-default">
 				<input type="checkbox" value="Sábado" name="Sábado">
 				<span class="glyphicon glyphicon-ok"></span>
-				<b>Sábado</b>
+				<b>Sáb</b>
 			</label>				
 		</div>
 	</div><br/>
-	<div class="col-sm-12">
-		Rango de citas
+	<div class="col-sm-4">
+		<label>Rango de citas: </label>
 	</div>
+	<!--Radio group-->
+	<div class="col-sm-8">
+			<div class="form-group ">
+			    <input name="group100" type="radio" id="radio100">
+			    <label for="radio100">Option 1</label>
+			</div>
+
+			<div class="form-group">
+			    <input name="group100" type="radio" id="radio101" checked>
+			    <label for="radio101">Option 2</label>
+			</div>
+
+	</div>		
+			<!--Radio group-->
  	</div>
 </div>	  	
 @stop
