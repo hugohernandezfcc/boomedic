ac6217c43ed5337d295dfd8043d4242662731adf
unknown <axel.valle@doitcloud.consulting>
2018-01-12 14:37:05 -0600

    v0.1.5

--- app/Transformers/ProfessionalInformationTransformer.php
+++ app/Transformers/ProfessionalInformationTransformer.php
@@ -39,7 +39,7 @@ class ProfessionalInformationTransformer extends TransformerAbstract
             'schoolOfMedicine'  => $pi->schoolOfMedicine,
             'facultyOfSpecialization'  => $pi->facultyOfSpecialization,
             'practiseProfessional'  => $pi->practiseProfessional,
-            'user'  => $pi->user,
+            //'user'  => $pi->user,
             'professional_license'  => $pi->professional_license,
             'medical_society' => $pi->medical_society
 			
