fb3221c299b31afaa1c3955a483d9ec2194dccbd
GTRebbeca <rebbecagt@gmail.com>
2018-01-16 12:27:33 -0600

    cambios en calendar workboard

--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -133,7 +133,10 @@
                }
           .info-box-content {
                margin-left: 90px; 
-               }      
+               }    
+          .info-box-content.sm {
+               margin-left: 50px; 
+               }    
               .info-box-icon.sm {
                   height: 45px;
                   width: 45px;
