f8f87ffd005fb0957f1b49ef290c19b1747115f4
GTRebbeca <rebbecagt@gmail.com>
2018-01-16 12:25:54 -0600

    calendario

--- resources/views/profileDoctor.blade.php
+++ resources/views/profileDoctor.blade.php
@@ -540,7 +540,7 @@
 							<div class="form-group">	
 							<div class="col-sm-8" style="padding-right: 0; padding-left: 0;">
 									          <div class="info-box sm bg-gray">
-									          	<a href="{{ url('workboardDr/index') }}/{{$labor->id}}"><span class="info-box-icon sm bg-black"><i class="fa fa-calendar-plus-o"></i></span></a> 
+									          	<a href="{{ url('workboardDr/index') }}/{{$labor->id}}"><span class="info-box-icon sm bg-black"><i class="fa fa-calendar"></i></span></a> 
 									            <div class="info-box-content sm">
 									              <b>{{ $labor->workplace}}</b> 
 									              <span class="text-black">{{ $labor->country }}, {{ $labor->state }}, {{ $labor->colony }}, {{ $labor->delegation }}, {{ $labor->street }} {{ $labor->streetNumber }}. Código Postal: {{ $labor->postalcode }}</span>
@@ -928,7 +928,7 @@
 						<div class="form-group">
 							<div class="col-sm-8" style="padding-right: 0; padding-left: 0;">
 					          <div class="info-box sm bg-gray">
-					          	<a href="{{ url('workboardDr/index') }}/{{$labor->id}}"><span class="info-box-icon sm bg-black"><i class="fa fa-calendar-plus-o"></i></span></a>
+					          	<a href="{{ url('workboardDr/index') }}/{{$labor->id}}"><span class="info-box-icon sm bg-black"><i class="fa fa-calendar"></i></span></a>
 					            <div class="info-box-content sm">
 					              <b> {{ $labor->workplace}}</b>
 					             <span class="text-black">{{ $labor->country }}, {{ $labor->state }}, {{ $labor->colony }}, {{ $labor->delegation }}, {{ $labor->street }} {{ $labor->streetNumber }}. CP: {{ $labor->postalcode }}</span><a href = "{{ url('doctor/delete') }}/{{ $labor->id }}" class="btn" onclick ="return confirm('¿Seguro desea eliminar este lugar?')"><i class="fa fa-trash text-muted"></i></a>
@@ -1267,7 +1267,7 @@
 						<div class="form-group">
 							<div class="col-sm-8" style="padding-right: 0; padding-left: 0;">
 					          <div class="info-box bg-gray">
-					          	<a href="{{ url('workboardDr/index') }}/{{$labor->id}}"><span class="info-box-icon bg-black"><i class="fa fa-calendar-plus-o"></i></span></a>
+					          	<a href="{{ url('workboardDr/index') }}/{{$labor->id}}"><span class="info-box-icon bg-black"><i class="fa fa-calendar"></i></span></a>
 					            <div class="info-box-content">
 					              <b>{{ $labor->workplace}}</b><br/>
 					             <span class="text-black">{{ $labor->country }}, {{ $labor->state }}, {{ $labor->colony }}, {{ $labor->delegation }}, {{ $labor->street }} {{ $labor->streetNumber }}. CP: {{ $labor->postalcode }}</span><a href = "{{ url('doctor/delete') }}/{{ $labor->id }}" class="btn" onclick ="return confirm('¿Seguro desea eliminar este lugar?')"><i class="fa fa-trash text-muted"></i></a>
