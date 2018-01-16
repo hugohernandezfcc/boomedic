73e2de82a7e63797c4e4d2de8eb07b39da4f2938
unknown <axel.valle@doitcloud.consulting>
2018-01-16 12:39:49 -0600

    v0.1.5b

--- app/Http/Controllers/Api/DiagnosticsController.php
+++ app/Http/Controllers/Api/DiagnosticsController.php
@@ -9,6 +9,7 @@ use App\Http\Requests\Api\Diagnostics\updateDiagnosticRequest;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Cyvelnet\Laravel5Fractal\Facades\Fractal;
+use App\Http\Controllers\Api\DB;
 
 class DiagnosticsController extends Controller
 {
