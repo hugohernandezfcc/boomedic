9ec70ef9dba3ab2bf3d1550fa4955cb9063ef0b4
unknown <axel.valle@doitcloud.consulting>
2018-01-16 18:00:44 -0600

    v0.2.0

--- app/cli_recipes_tests.php
+++ app/cli_recipes_tests.php
@@ -18,4 +18,12 @@ class cli_recipes_tests extends Model
     public function recipes_tests(){
 	  return $this->belongsTo('App\recipes_tests', 'recipes_tests');
 	}
+
+	public function medicine(){
+	  return $this->belongsTo('App\medicines', 'medicine');
+	}
+
+	public function test(){
+	  return $this->belongsTo('App\diagnostic_tests', 'test');
+	}
 }
