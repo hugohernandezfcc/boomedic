3257c3d6e6b82a8727a9e8d7b50f38ffb176846d
GTRebbeca <rebbecagt@gmail.com>
2018-02-01 12:01:08 -0600

    respuesta del console

--- resources/views/medicalconsultations.blade.php
+++ resources/views/medicalconsultations.blade.php
@@ -1190,6 +1190,8 @@
                  $('#timesByDay').children().remove();
                   for (var y = optionhour.length - 1; y >= 0; y--) { 
                      resp = optionhour[y].split(":"); 
+                      console.log(resp);
+                      console.log(optionhour[y]);  
                     console.log(resp[1] + resp[2]);
                       if(resp[0] == 'Dom'){
                         var Dom = resp[1];
@@ -1246,22 +1248,9 @@
                     option.value = resp[1];
                     x.add(option);
                   }
-                     $('#calendar1').datepicker({ daysOfWeekDisabled: days,  language: 'es', onSelect: function(dateText){
-                      var seldate = $(this).datepicker('getDate');
-                      seldate = seldate.toDateString();
-                      seldate = seldate.split(' ');
-                      var weekday=new Array();
-                          weekday['Mon']="Lunes";
-                          weekday['Tue']="Martes";
-                          weekday['Wed']="Miércoles";
-                          weekday['Thu']="Jueves";
-                          weekday['Fri']="Viernes";
-                          weekday['Sat']="Sábado";
-                          weekday['Sun']="Domingo";
-                      var dayOfWeek = weekday[seldate[0]];
-                     console.log(dayOfWeek);
-                  }
-                })
+                     $('#calendar1').datepicker({ daysOfWeekDisabled: days,  language: 'es'}).on('changeDate',function(e){
+                        console.log(e);
+                      });
              }
           })(marker, i));
           setTimeout(dropMarker(i), i * 250);
