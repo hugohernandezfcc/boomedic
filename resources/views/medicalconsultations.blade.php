223e28aa720552f4278126eb4706aa99efbb9343
GTRebbeca <rebbecagt@gmail.com>
2018-02-01 11:51:54 -0600

    prueba uno obteniendo día seleccionado

--- resources/views/medicalconsultations.blade.php
+++ resources/views/medicalconsultations.blade.php
@@ -1246,9 +1246,22 @@
                     option.value = resp[1];
                     x.add(option);
                   }
-                     $('#calendar1').datepicker({ daysOfWeekDisabled: days,  language: 'es' }).on('changeDate',function(e){
-                        console.log(e);
-                      });
+                     $('#calendar1').datepicker({ daysOfWeekDisabled: days,  language: 'es', onSelect: function(dateText){
+                      var seldate = $(this).datepicker('getDate');
+                      seldate = seldate.toDateString();
+                      seldate = seldate.split(' ');
+                      var weekday=new Array();
+                          weekday['Mon']="Lunes";
+                          weekday['Tue']="Martes";
+                          weekday['Wed']="Miércoles";
+                          weekday['Thu']="Jueves";
+                          weekday['Fri']="Viernes";
+                          weekday['Sat']="Sábado";
+                          weekday['Sun']="Domingo";
+                      var dayOfWeek = weekday[seldate[0]];
+                     console.log(dayOfWeek);
+                  }
+                })
              }
           })(marker, i));
           setTimeout(dropMarker(i), i * 250);
