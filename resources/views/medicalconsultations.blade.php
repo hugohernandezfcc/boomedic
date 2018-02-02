27c4831eff63876bb5371bfe942bfa78de16893d
GTRebbeca <rebbecagt@gmail.com>
2018-02-02 13:49:58 -0600

    cambios en wizard

--- resources/views/medicalconsultations.blade.php
+++ resources/views/medicalconsultations.blade.php
@@ -550,11 +550,11 @@ $(document).ready(function () {
     });
 
     $(".next-step").click(function (e) {
-
+      if (document.getElementById('dateSelectedForCite' != '').value) {
         var $active = $('.wizard .nav-tabs li.active');
         $active.next().removeClass('disabled');
         nextTab($active);
-
+          }
     });
     $(".prev-step").click(function (e) {
 
