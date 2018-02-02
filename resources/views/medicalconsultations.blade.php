48c1867e32ae1ab8bbd8df5f3d041df381c94c8f
unknown <axel.valle@doitcloud.consulting>
2018-02-02 13:13:53 -0600

    v0.1.2a

--- resources/views/medicalconsultations.blade.php
+++ resources/views/medicalconsultations.blade.php
@@ -403,25 +403,7 @@
                 </div>
               <hr width="100%">
                 <!-- Calendar -->
-          <div class="box box-solid bg-green-gradient">
-            <div class="box-header">
-              <i class="fa fa-calendar"></i>
-
-              <h3 class="box-title">Seleccionar día</h3>
-              <!-- tools box -->
-              <div class="pull-right box-tools">
-                <button type="button" class="btn btn-success btn-sm" data-widget="collapse"><i class="fa fa-minus"></i></button>
-              </div>
-              <!-- /. tools -->
-            </div>
-            <!-- /.box-header -->
-            <div class="box-body no-padding">
-              <!--The calendar -->
-              <div id="calendar" style="width: 80%"></div>
-            </div>
-            <!-- /.box-body -->
-            
-          </div>
+          
 
           <input type="hidden" id="dateSelectedForCite">
  
