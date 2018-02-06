46db53a3fc43e98adbe95f8f291640c88b7a6ebf
unknown <axel.valle@doitcloud.consulting>
2018-02-06 16:43:21 -0600

    v0.1.2

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -188,7 +188,7 @@
 	    			{{ csrf_field() }}
 
 	    			<div class="form-group has-feedback {{ $errors->has('email') ? 'has-error' : '' }}">
-	                    <label for="firstname" class="col-sm-2 control-label"><i class="fa fa-asterisk" style="vertical-align: top; "></i>Nombre</label>
+	                    <label for="firstname" class="col-sm-2 control-label">Nombre<i class="fa fa-asterisk text-red" style="vertical-align: top; font-size: 9px "></i></label>
 	                	<div class="col-sm-10">
 	                  		<input type="text" name="firstname" class="form-control" id="firstname" value="{{ $firstname }}">
 	                	</div>
