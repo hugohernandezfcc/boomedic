c530a853ff2e2236d20e8797f0bfdd0f2d0b22cc
GTRebbeca <rebbecagt@gmail.com>
2018-02-06 16:51:17 -0600

    propiedad required

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -183,7 +183,7 @@
 	    			<div class="form-group has-feedback {{ $errors->has('email') ? 'has-error' : '' }}">
 	                    <label for="firstname" class="col-sm-2 control-label">Nombre <span class="text-red fa fa-circle" style="font-size: 7px;"></span></label>
 	                	<div class="col-sm-10">
-	                  		<input type="text" name="firstname" class="form-control" id="firstname" value="{{ $firstname }}">
+	                  		<input type="text" name="firstname" class="form-control" id="firstname" value="{{ $firstname }}" required>
 	                	</div>
 	              	</div>
 
