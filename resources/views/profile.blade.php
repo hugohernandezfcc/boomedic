93cf1f08a3eed556d7239f89928d048a53d6db6d
GTRebbeca <rebbecagt@gmail.com>
2018-02-06 12:59:58 -0600

    colocando span de campo obligatorio

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -183,7 +183,10 @@
 	    			<div class="form-group has-feedback {{ $errors->has('email') ? 'has-error' : '' }}">
 	                    <label for="firstname" class="col-sm-2 control-label">Nombre</label>
 	                	<div class="col-sm-10">
+	                		<div class="input-group">
+                			<span class="input-group-addon text-red">*</span>
 	                  		<input type="text" name="firstname" class="form-control" id="firstname" value="{{ $firstname }}">
+	                  		 </div>
 	                	</div>
 	              	</div>
 
