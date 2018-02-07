3f42c796b2ca2977e813082f4c66703662ba120a
GTRebbeca <rebbecagt@gmail.com>
2018-02-07 17:36:38 -0600

    faltó quitar )

--- resources/views/medicalconsultations.blade.php
+++ resources/views/medicalconsultations.blade.php
@@ -637,7 +637,7 @@ function prevTab(elem) {
                   for (var i = result.length - 1; i >= 0; i--) {
                     
                     var option = document.createElement("option");
-                    }
+
                     if(result[i].provider != 'Paypal'){
                     option.text = result[i].provider + ": " + result[i].cardnumber;
                     option.value = result[i].id;
