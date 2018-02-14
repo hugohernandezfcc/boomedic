380eb420d8aabb5b68677370d728f9a39fec4d38
unknown <axel.valle@doitcloud.consulting>
2018-02-14 14:27:42 -0600

    mas cambios de estética

--- resources/views/developers.blade.php
+++ resources/views/developers.blade.php
@@ -19,14 +19,21 @@
 <br/>
 
 <div class="lockscreen-item" style="margin: 10px 0 30px auto;">
-	<div class="input-group">
-	    <div class="form-control" align="center"><label id="labeltext">Agregar</label></div>
-	    <div class="input-group-btn" id="div_profile2">
-		<button class="btn btn-default" type="button" data-toggle="modal" data-target="#modalAddApp">
-		    <i class="fa fa-plus text-muted" id="i_button2"></i>
-		</button>
-	   	</div>
-	</div>
+
+	    <!-- lockscreen credentials (contains the form) -->
+	    <form class="lockscreen-credentials" action="create" method="get" id="form_profile">
+	    	{{ csrf_field() }}
+	      	<div class="input-group">
+	        	<div class="form-control" align="left"><label id="labeltext">Agregar</label></div>
+	        	<input type="hidden" name="id" value="{{ $userId }}">
+	        	<div class="input-group-btn" id="div_profile">
+		          	<button class="btn btn-default" type="button" data-toggle="modal" data-target="#modalAddApp">
+		    			<i class="fa fa-plus text-muted" id="i_button2"></i>
+					</button>
+	        	</div>
+	      	</div>
+	    </form>
+	    <!-- /.lockscreen credentials -->
 </div>
 
 <div class="box">
