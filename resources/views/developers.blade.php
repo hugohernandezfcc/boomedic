23324e3f76e87f15810f2ba3df76400dacebbbdd
unknown <axel.valle@doitcloud.consulting>
2018-02-14 16:03:57 -0600

    cambios en estética del modal de agregar nueva APP

--- resources/views/developers.blade.php
+++ resources/views/developers.blade.php
@@ -75,12 +75,13 @@
               		<div class="input-group input-group-sm">
               			<div class="col-sm-5">
               				<label>Nombre de la Aplicación:</label>
+              				<label>URL de Redireccionamiento:</label>
                         </div>
                         <div class="col-sm-7">
                         	<input id="appName" type="textbox" class="form-control">
                     	</div><br/>
                     	<div class="col-sm-5">
-                        	<label>URL de Redireccionamiento:</label>
+                        	
                     	</div>
                     	<div class="col-sm-7">
                     		<input id="appURL" type="textbox" class="form-control">
