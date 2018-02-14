f4aa970a3ed08e8576eb82e2d59947f47231acf5
GTRebbeca <rebbecagt@gmail.com>
2018-02-14 16:51:54 -0600

    prueba 2 carga de foto

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -58,7 +58,7 @@
 				  setTimeout(function(){ 
 				  	document.getElementById('loadingGif').style.display = "none";
 				  	window.location.reload(true);
-				  },15000);
+				  },20000);
 				     	}
 			    //autoProcessQueue : false 
 			 };
@@ -67,7 +67,6 @@
 
 			 		setTimeout(function() {
 			 			 $('#modal').modal();
-			 			 $('#crop').append('<div class="modal-body" ><div align="center"><img src="{{ $photo }}" id="target" ><form enctype="multipart/form-data" action="/user/cropProfile/{{$userId}}" method="post" onsubmit="return checkCoords();"><input type="hidden" id="x" name="x" /><input type="hidden" id="y" name="y" /><input type="hidden" id="w" name="w" /><input type="hidden" id="h" name="h" /><br/><span class="input-group-btn"><input type="submit" class="btn btn-secondary btn-block btn-flat" value="Guardar"></span></form></div></div>');
 					}, 3000);	
 				}
 
@@ -95,11 +94,27 @@
 <div id="modal" class="modal fade" role="dialog">
               <div class="modal-dialog">
                 <!-- Modal content-->
-                <div class="modal-content" id="crop">
+                <div class="modal-content" >
                   <div class="modal-header">   
                     <label for="recorte">Recorte de imagen:</label>
                   </div>
+                  <div class="modal-body" >
 
+                        <div align="center">
+
+						<img src="{{ $photo }}" id="target">	
+                    
+                           <form enctype="multipart/form-data" action="/user/cropProfile/{{$userId}}" method="post" onsubmit="return checkCoords();">
+                           	<input type="hidden" id="x" name="x">
+							<input type="hidden" id="y" name="y">
+							<input type="hidden" id="w" name="w">
+							<input type="hidden" id="h" name="h"><br/>
+	                        <span class="input-group-btn">
+	                        <input type="submit" class="btn btn-secondary btn-block btn-flat" value="Guardar"></span>
+                          </form>
+                       </div>
+                     <!--<input id="submit" type="button" value="Buscar" class="map-marker text-muted">-->
+                  </div>
                 </div>
               </div>
  </div>
