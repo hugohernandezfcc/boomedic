923d7087cd475dc62e59264672bfd683861b25dc
GTRebbeca <rebbecagt@gmail.com>
2018-02-23 10:53:58 -0600

    subiendo cambios objet parse

--- resources/views/workboard.blade.php
+++ resources/views/workboard.blade.php
@@ -263,7 +263,7 @@ $(function() {
 
         var optionhour = @php echo $workboard;  @endphp;
         console.log(optionhour);
-        var hor = [];
+          var hor = [];
           var resp = Array();
           var resp2 = Array();
          for(var y = 0; y < optionhour.length; y++){ 
@@ -430,7 +430,62 @@ jQuery.noConflict(false);
 		editable: true,
 		eventLimit: true, // allow "more" link when too many events
 		navLinks: true,
-		events: hor
+		events: JSON.parse(hor) /*[
+			{
+				title: 'All Day Event',
+				start: YM + '-01'
+			},
+			{
+				title: 'Long Event',
+				start: YM + '-07',
+				end: YM + '-10'
+			},
+			{
+				id: 999,
+				title: 'Repeating Event',
+				start: YM + '-09T16:00:00'
+			},
+			{
+				id: 999,
+				title: 'Repeating Event',
+				start: YM + '-16T16:00:00'
+			},
+			{
+				title: 'Conference',
+				start: YESTERDAY,
+				end: TOMORROW
+			},
+			{
+				title: 'Meeting',
+				start: TODAY + 'T10:30:00',
+				end: TODAY + 'T12:30:00'
+			},
+			{
+				title: 'Lunch',
+				start: TODAY + 'T12:00:00'
+			},
+			{
+				title: 'Meeting',
+				start: TODAY + 'T14:30:00'
+			},
+			{
+				title: 'Happy Hour',
+				start: TODAY + 'T17:30:00'
+			},
+			{
+				title: 'Dinner',
+				start: TODAY + 'T20:00:00'
+			},
+			{
+				title: 'Birthday Party',
+				start: TOMORROW + 'T07:00:00'
+			},
+			{
+				title: 'Click for Google',
+				url: 'http://google.com/',
+				start: YM + '-28'
+			}
+		]*/
 	});
 });	 				
 </script>
