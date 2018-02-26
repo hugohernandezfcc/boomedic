b5b2f7cc750c6f63e1efcfc81360fa701e861357
GTRebbeca <rebbecagt@gmail.com>
2018-02-26 11:54:12 -0600

    como antes

--- resources/views/medicalconsultations.blade.php
+++ resources/views/medicalconsultations.blade.php
@@ -1429,57 +1429,6 @@ function prevTab(elem) {
         console.log(res);
         console.log(loc);
       }
-      function CustomMarker(latlng, map, imageSrc) {
-  this.latlng_ = latlng;
-  this.imageSrc = imageSrc;
-  // Once the LatLng and text are set, add the overlay to the map.  This will
-  // trigger a call to panes_changed which should in turn call draw.
-  this.setMap(map);
-}
-
-CustomMarker.prototype = new google.maps.OverlayView();
-
-CustomMarker.prototype.draw = function() {
-  // Check if the div has been created.
-  var div = this.div_;
-  if (!div) {
-    // Create a overlay text DIV
-    div = this.div_ = document.createElement('div');
-    // Create the DIV representing our CustomMarker
-    div.className = "customMarker"
-
-
-    var img = document.createElement("img");
-    img.src = this.imageSrc;
-    div.appendChild(img);
-    google.maps.event.addDomListener(div, "click", function(event) {
-      google.maps.event.trigger(me, "click");
-    });
-
-    // Then add the overlay to the DOM
-    var panes = this.getPanes();
-    panes.overlayImage.appendChild(div);
-  }
-
-  // Position the overlay 
-  var point = this.getProjection().fromLatLngToDivPixel(this.latlng_);
-  if (point) {
-    div.style.left = point.x + 'px';
-    div.style.top = point.y + 'px';
-  }
-};
-
-CustomMarker.prototype.remove = function() {
-  // Check if the overlay was on the map and needs to be removed.
-  if (this.div_) {
-    this.div_.parentNode.removeChild(this.div_);
-    this.div_ = null;
-  }
-};
-
-CustomMarker.prototype.getPosition = function() {
-  return this.latlng_;
-};
       function drop() {
         clearMarkers();
         for (var i = 0; i < loc.length; i++) {
@@ -1488,9 +1437,17 @@ CustomMarker.prototype.getPosition = function() {
           console.log(lat);
           console.log(lon);
           var doctor = loc[i][10];
-new CustomMarker(new google.maps.LatLng(loc[i][0],  loc[i][1], map,  loc[i][10]);
+          markers[i] = new google.maps.Marker({
+            position: new google.maps.LatLng(lat,lon),
+            animation: google.maps.Animation.DROP,
+                icon: {
+                    url: doctor,
+                    scaledSize: new google.maps.Size(20, 20),
+                    
+                  }
+          });
           var infowindow = new google.maps.InfoWindow();
-          var marker =new CustomMarker(new google.maps.LatLng(loc[i][0],  loc[i][1], map,  loc[i][10]);
+          var marker = markers[i];
           google.maps.event.addListener(marker, 'click', (function(marker, i) {
             return function() {
 
