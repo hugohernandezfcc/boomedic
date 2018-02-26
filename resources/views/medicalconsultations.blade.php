4362fd559946f69117c67e7126f3741ff1c97e52
GTRebbeca <rebbecagt@gmail.com>
2018-02-26 15:53:51 -0600

    marker i

--- resources/views/medicalconsultations.blade.php
+++ resources/views/medicalconsultations.blade.php
@@ -1439,14 +1439,14 @@ function prevTab(elem) {
           console.log(lat);
           console.log(lon);
           var doctor = loc[i][10];
-          markers = new RichMarker({
+          markers[i] = new RichMarker({
             position: new google.maps.LatLng(lat,lon),
             animation: google.maps.Animation.DROP,
             content: '<div class="mark"><img src="https://s3.amazonaws.com/abiliasf/profile-42914_640.png" class="img-circle" alt="User Image"></img></div>'
           });
           var infowindow = new google.maps.InfoWindow();
-          var marker = markers;
-          google.maps.event.addListener(marker, 'click', (function(marker, i) {
+          var marker = markers[i];
+          google.maps.event.addListener(markers[i], 'click', (function(marker, i) {
             return function() {
 
               $('#infDr').show();
