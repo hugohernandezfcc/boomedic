b5af8bf04a07aeed0fb35bde13074ce51b4644e1
GTRebbeca <rebbecagt@gmail.com>
2018-02-27 15:25:12 -0600

    cambios

--- resources/views/medicalconsultations.blade.php
+++ resources/views/medicalconsultations.blade.php
@@ -1441,6 +1441,7 @@ function prevTab(elem) {
           var doctor = "{{ asset('doctors.png') }}";
 
           markers[i] = new CustomMarker(new google.maps.LatLng(lat,lon), map, "https://s3.amazonaws.com/abiliasf/16.jpg");
+           markers[i].setMap(map);
         /*  markers[i] = new google.maps.Marker({
             position: new google.maps.LatLng(lat,lon),
             animation: google.maps.Animation.DROP,
@@ -1451,7 +1452,7 @@ function prevTab(elem) {
           console.log(marker.length);
           console.log(marker);
          
-         setTimeout(dropMarker(i), i * 250);
+   
         }
       }
       function dropMarker(i) {
