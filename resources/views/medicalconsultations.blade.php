dbfdf0739f4ff796e7aa9506d2d271b0798e9404
GTRebbeca <rebbecagt@gmail.com>
2018-02-27 13:09:58 -0600

    prueba custonMarker set map null

--- resources/views/medicalconsultations.blade.php
+++ resources/views/medicalconsultations.blade.php
@@ -1439,11 +1439,11 @@ function prevTab(elem) {
           console.log(lat);
           console.log(lon);
           var doctor = "{{ asset('doctors.png') }}";
-          markers[i] = new CustomMarker(new google.maps.LatLng(lat,lon), map, "http://placekitten.com/90/90");
+          markers[i] = new CustomMarker(new google.maps.LatLng(lat,lon), "http://placekitten.com/90/90");
           console.log(map);
           var infowindow = new google.maps.InfoWindow();
           var marker = markers[i];
-          
+
           google.maps.event.addListener(marker, 'click', (function(marker, i) {
             return function() {
 
