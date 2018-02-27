895fcc9563e62e2930ce8f6effc1af001b823c33
GTRebbeca <rebbecagt@gmail.com>
2018-02-27 11:27:44 -0600

    map prueba 2

--- resources/views/medicalconsultations.blade.php
+++ resources/views/medicalconsultations.blade.php
@@ -1439,8 +1439,18 @@ function prevTab(elem) {
           console.log(lat);
           console.log(lon);
           var doctor = "{{ asset('doctors.png') }}";
-          markers[i] = new CustomMarker(new google.maps.LatLng(lat,lon), map,  "http://placekitten.com/90/90");
-
+          markers[i] = new google.maps.Marker({
+            position: new google.maps.LatLng(lat,lon),
+            animation: google.maps.Animation.DROP,
+            map: map,
+            icon: doctor
+                /*icon: {
+                    url: doctor,
+                    scaledSize: new google.maps.Size(50, 50),
+                    shape:{coords:[17,17,18],type:'circle'},
+                    optimized:false
+                  }*/
+          });
           var infowindow = new google.maps.InfoWindow();
           var marker = markers[i];
           google.maps.event.addListener(marker, 'click', (function(marker, i) {
