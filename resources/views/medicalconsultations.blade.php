aee977785090f2c3e32cfec8724ce223243c0d23
GTRebbeca <rebbecagt@gmail.com>
2018-02-28 14:13:39 -0600

    initmap() dentro de la funcion

--- resources/views/medicalconsultations.blade.php
+++ resources/views/medicalconsultations.blade.php
@@ -1253,7 +1253,6 @@ function prevTab(elem) {
               rotateControl: false,
               fullscreenControl: false
             });
- 
             var input = document.getElementById('address');
               new google.maps.places.Autocomplete(input);
             var markerUser = "{{ asset('markerUser.png') }}";
@@ -1311,7 +1310,8 @@ function prevTab(elem) {
             infoWindow.setPosition({lat: 20.42, lng: -99.18});
             infoWindow.setContent(message03);
         }
-        //Filter geocode Address
+      }
+      //Filter geocode Address
         function geocodeAddress(geocoder, resultsMap, markerP) {
         var address = document.getElementById('address').value;
         geocoder.geocode({'address': address}, function(results, status) {
@@ -1429,69 +1429,8 @@ function prevTab(elem) {
         console.log(res);
         console.log(loc);
       }
-      function USGSOverlay(bounds, image, map) {
-
-  // Initialize all properties.
-  this.bounds_ = bounds;
-  this.image_ = image;
-
-
-  // Define a property to hold the image's div. We'll
-  // actually create this div upon receipt of the onAdd()
-  // method so we'll leave it null for now.
-  this.div_ = null;
-
-  // Explicitly call setMap on this overlay.
-  this.setMap(map);
-
-}
- USGSOverlay.prototype = new google.maps.OverlayView();
-
-USGSOverlay.prototype.onAdd = function() {
-
-  var div = document.createElement('div');
-   div.className = 'customMarker';
-
-
-  // Create the img element and attach it to the div.
-  var img = document.createElement('img');
-  img.src = this.image_;
-  div.appendChild(img);
-
-  this.div_ = div;
-
-  // Add the element to the "overlayLayer" pane.
-  var panes = this.getPanes();
-  panes.overlayLayer.appendChild(div);
-   this.getPanes().overlayMouseTarget.appendChild(div);
-        var me = this;
-        google.maps.event.addDomListener(div, "click", function(event) {       
-            google.maps.event.trigger(me, "click");
-        });
-        
-};
-
-USGSOverlay.prototype.draw = function() {
-
-  var overlayProjection = this.getProjection();
-
-  var point = overlayProjection.fromLatLngToDivPixel(this.bounds_);
-
-
-  // Resize the image's div to fit the indicated dimensions.
-  var div = this.div_;
-  div.style.left = point.x + 'px';
-  div.style.top = point.y + 'px';
-
-};
-
-USGSOverlay.prototype.onRemove = function() {
-  this.div_.parentNode.removeChild(this.div_);
-  this.div_ = null;
-};
       function drop() {
-      clearMarkers();
-
+        /*clearMarkers();*/
         for (var i = 0; i < loc.length; i++) {
           var lat = loc[i][0];
           var lon = loc[i][1];
@@ -1704,10 +1643,7 @@ USGSOverlay.prototype.onRemove = function() {
          setTimeout(dropMarker(i), i * 250);
         }
       }
-
-     
-      }
-            function dropMarker(i) {
+      function dropMarker(i) {
         return function() {
           markers[i].setMap(map);
         };
@@ -1721,6 +1657,67 @@ USGSOverlay.prototype.onRemove = function() {
       function showInfo(info){ 
         document.getElementById("info").innerHTML = '<strong>Información del médico:</strong> <br/>'+ info +'';
       }
+      function USGSOverlay(bounds, image, map) {
+
+  // Initialize all properties.
+  this.bounds_ = bounds;
+  this.image_ = image;
+
+
+  // Define a property to hold the image's div. We'll
+  // actually create this div upon receipt of the onAdd()
+  // method so we'll leave it null for now.
+  this.div_ = null;
+
+  // Explicitly call setMap on this overlay.
+  this.setMap(map);
+
+}
+initMap();
+ USGSOverlay.prototype = new google.maps.OverlayView();
+
+USGSOverlay.prototype.onAdd = function() {
+
+  var div = document.createElement('div');
+   div.className = 'customMarker';
+
+
+  // Create the img element and attach it to the div.
+  var img = document.createElement('img');
+  img.src = this.image_;
+  div.appendChild(img);
+
+  this.div_ = div;
+
+  // Add the element to the "overlayLayer" pane.
+  var panes = this.getPanes();
+  panes.overlayLayer.appendChild(div);
+   this.getPanes().overlayMouseTarget.appendChild(div);
+        var me = this;
+        google.maps.event.addDomListener(div, "click", function(event) {       
+            google.maps.event.trigger(me, "click");
+        });
+        
+};
+
+USGSOverlay.prototype.draw = function() {
+
+  var overlayProjection = this.getProjection();
+
+  var point = overlayProjection.fromLatLngToDivPixel(this.bounds_);
+
+
+  // Resize the image's div to fit the indicated dimensions.
+  var div = this.div_;
+  div.style.left = point.x + 'px';
+  div.style.top = point.y + 'px';
+
+};
+
+USGSOverlay.prototype.onRemove = function() {
+  this.div_.parentNode.removeChild(this.div_);
+  this.div_ = null;
+};
     </script>
 
     <!-- Calculate distance -->
