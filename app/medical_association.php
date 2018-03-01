42d3096f7f5171633edab1741273a433484c855a
GTRebbeca <rebbecagt@gmail.com>
2018-03-01 13:15:26 -0600

    subiendo nuevo modelo de medical_association

--- app/medical_association.php
+++ app/medical_association.php
@@ -4,12 +4,24 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class recipes_tests extends Model
+class medical_association extends Model
 {
     protected $table = "medical_association";
     protected $fillable = [
     	'id',
-    	'name'
+    	'name',
+    	'parent',
+    	'members',
+    	'facebook',
+    	'twitter',
+    	'email',
+    	'address',
+    	'phone',
+    	'mobile'
     ];
+        public function medical_association()
+    {
+        return $this->belongsTo('App\medical_association', 'parent');
+    }
 
 }
