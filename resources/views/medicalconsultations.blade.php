d98dc3804e7023d0ab74eda680f829a056c7242e
GTRebbeca <rebbecagt@gmail.com>
2018-02-28 18:01:25 -0600

    markers

--- resources/views/medicalconsultations.blade.php
+++ resources/views/medicalconsultations.blade.php
@@ -1437,7 +1437,7 @@ function prevTab(elem) {
           console.log(lat);
           console.log(lon);
           if(loc[i][10] != "https://s3.amazonaws.com/abiliasf/doctor-png-image-29624.png"){
-          var doctor = "https://s3.amazonaws.com/abiliasf/"' + loc[i][8] + '"-circle.png";
+          var doctor = "https://s3.amazonaws.com/abiliasf/" + loc[i][8] + "-circle.png";
         }else{
            var doctor = "https://s3.amazonaws.com/abiliasf/doctor-png-image-29624.png";
          /*markers[i] = new USGSOverlay(new google.maps.LatLng(lat , lon), "https://s3.amazonaws.com/abiliasf/16.jpg", map);*/
@@ -1446,8 +1446,9 @@ function prevTab(elem) {
         markers[i] = new google.maps.Marker({
             position: new google.maps.LatLng(lat,lon),
             animation: google.maps.Animation.DROP,
-            icon: { url : doctor, 
-                    scaledSize: new google.maps.Size(55, 55) }
+            icon: {
+              url: doctor
+            }
           });
           var infowindow = new google.maps.InfoWindow();
           var marker = markers[i];
@@ -1662,7 +1663,7 @@ function prevTab(elem) {
       function showInfo(info){ 
         document.getElementById("info").innerHTML = '<strong>Información del médico:</strong> <br/>'+ info +'';
       }
-   /*   function USGSOverlay(bounds, image, map) {
+      function USGSOverlay(bounds, image, map) {
 
   // Initialize all properties.
   this.bounds_ = bounds;
@@ -1718,7 +1719,7 @@ USGSOverlay.prototype.draw = function() {
 USGSOverlay.prototype.onRemove = function() {
   this.div_.parentNode.removeChild(this.div_);
   this.div_ = null;
-};*/
+};
     </script>
 
     <!-- Calculate distance -->
