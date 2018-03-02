bbaeb4b84aca4c1aaa4cf369ac15ff2ec8ca64d0
GTRebbeca <rebbecagt@gmail.com>
2018-03-02 11:12:27 -0600

    cambiando icono de doctor

--- resources/views/medicalconsultations.blade.php
+++ resources/views/medicalconsultations.blade.php
@@ -1436,14 +1436,14 @@ function prevTab(elem) {
           var lon = loc[i][1];
           console.log(lat);
           console.log(lon);
-          if(loc[i][10] != "https://s3.amazonaws.com/abiliasf/doctor-png-image-29624.png"){
+          if(loc[i][10] != "https://s3.amazonaws.com/abiliasf/doctor-1295571_960_720.png"){
           var doctor = {
               url:"https://s3.amazonaws.com/abiliasf/" + loc[i][8] + "-circle.png",
               scaledSize: new google.maps.Size(50, 50)
             };
         }else{
            var doctor = {
-              url:"https://s3.amazonaws.com/abiliasf/doctor-png-image-29624.png",
+              url:"https://s3.amazonaws.com/abiliasf/doctor-1295571_960_720.png",
               scaledSize: new google.maps.Size(45, 45)
             };
          /*markers[i] = new USGSOverlay(new google.maps.LatLng(lat , lon), "https://s3.amazonaws.com/abiliasf/16.jpg", map);*/
