1207661cd1520025e1bf381c6c685fad2cec1a46
GTRebbeca <rebbecagt@gmail.com>
2018-03-05 16:10:48 -0600

    subiendo icono nuevo

--- resources/views/medicalconsultations.blade.php
+++ resources/views/medicalconsultations.blade.php
@@ -1460,15 +1460,15 @@ function prevTab(elem) {
           var lon = loc[i][1];
           console.log(lat);
           console.log(lon);
-          if(loc[i][10] != "https://s3.amazonaws.com/abiliasf/doctor-1295571_960_720.png"){
+          if(loc[i][10] != "https://s3.amazonaws.com/abiliasf/icono_doc-01.png"){
           var doctor = {
               url:"https://s3.amazonaws.com/abiliasf/" + loc[i][8] + "-circle.png",
               scaledSize: new google.maps.Size(45, 45)
             };
         }else{
            var doctor = {
-              url:"https://s3.amazonaws.com/abiliasf/doctor-1295571_960_720.png",
-              scaledSize: new google.maps.Size(31, 35)
+              url:"https://s3.amazonaws.com/abiliasf/icono_doc-01.png",
+              scaledSize: new google.maps.Size(45, 45)
             };
          /*markers[i] = new USGSOverlay(new google.maps.LatLng(lat , lon), "https://s3.amazonaws.com/abiliasf/16.jpg", map);*/
         }
