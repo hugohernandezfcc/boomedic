568d9ea9952791b256b1a1f7ef88bc7aee59ec3c
GTRebbeca <rebbecagt@gmail.com>
2018-03-06 17:02:21 -0600

    primera vista historia clínica

---
+++ resources/views/clinicHistory.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'Boomedic')
+
+@section('content_header')
+
+@stop
+
+@section('content')
+
+
+<div class="box">
+  	<div class="box-header with-border">
+	    <h3 class="box-title">Historia Clínica</h3>
+  	</div>
+  	<div class="box-body">
+ 	</div>
+</div>	  	
+@stop
