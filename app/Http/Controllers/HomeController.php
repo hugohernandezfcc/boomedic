7405743f04074911e40810a335f78f6ec5775c53
GTRebbeca <rebbecagt@gmail.com>
2018-03-06 13:06:59 -0600

    prueba 1000

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -143,7 +143,7 @@ class HomeController extends Controller
         $recent = array();
         $json = json_decode($request);
     if($request->search != null){
-      if(!$user->recent_search){
+      if(count($userSearch) < 1){
             array_push($recent, $request->search);
              $user->recent_search  = json_encode($recent); 
       } 
