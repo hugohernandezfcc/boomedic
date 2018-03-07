0941850ce01c7ad637e94ec6bec3d48eaf79bacc
GTRebbeca <rebbecagt@gmail.com>
2018-03-07 11:44:23 -0600

    changes de wizard

--- resources/views/clinicHistory.blade.php
+++ resources/views/clinicHistory.blade.php
@@ -35,7 +35,6 @@ ol.linenums li {
 }
 </style>
     <script src="{{ asset('js/jquery.bootstrap.wizard.js') }}"></script>
-    <script src="{{ asset('js/jquery.bootstrap.wizard.min.js') }}"></script>
     <script src="{{ asset('js/prettify.js') }}"></script>
      <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script> 
 @stop
@@ -47,67 +46,67 @@ ol.linenums li {
 	    <h3 class="box-title">Historia Clínica</h3>
   	</div>
   	<div class="box-body">
-  		<div id="rootwizard">
-	<div class="navbar">
-	  <div class="navbar-inner">
-	    <div class="container">
-	<ul>
-	  	<li><a href="#tab1" data-toggle="tab">First</a></li>
-		<li><a href="#tab2" data-toggle="tab">Second</a></li>
-		<li><a href="#tab3" data-toggle="tab">Third</a></li>
-		<li><a href="#tab4" data-toggle="tab">Forth</a></li>
-		<li><a href="#tab5" data-toggle="tab">Fifth</a></li>
-		<li><a href="#tab6" data-toggle="tab">Sixth</a></li>
-		<li><a href="#tab7" data-toggle="tab">Seventh</a></li>
-	</ul>
-	 </div>
-	  </div>
-	</div>
-    <div id="bar" class="progress">
-      <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>
-    </div>
-	<div class="tab-content">
-	    <div class="tab-pane" id="tab1">
-	      1
-	    </div>
-	    <div class="tab-pane" id="tab2">
-	      2
-	    </div>
-		<div class="tab-pane" id="tab3">
-			3
-	    </div>
-		<div class="tab-pane" id="tab4">
-			4
-	    </div>
-		<div class="tab-pane" id="tab5">
-			5
-	    </div>
-		<div class="tab-pane" id="tab6">
-			6
-	    </div>
-		<div class="tab-pane" id="tab7">
-			7
-	    </div>
-		<ul class="pager wizard">
-			<li class="previous first" style="display:none;"><a href="#">First</a></li>
-			<li class="previous"><a href="#">Previous</a></li>
-			<li class="next last" style="display:none;"><a href="#">Last</a></li>
-		  	<li class="next"><a href="#">Next</a></li>
-		</ul>
-	</div>
-</div>
- 	</div>
-</div>	
+  	<div id="rootwizard">
+					<div class="navbar">
+					  <div class="navbar-inner">
+					    <div class="container">
+					<ul>
+					  	<li><a href="#tab1" data-toggle="tab">First</a></li>
+						<li><a href="#tab2" data-toggle="tab">Second</a></li>
+						<li><a href="#tab3" data-toggle="tab">Third</a></li>
+						<li><a href="#tab4" data-toggle="tab">Forth</a></li>
+						<li><a href="#tab5" data-toggle="tab">Fifth</a></li>
+						<li><a href="#tab6" data-toggle="tab">Sixth</a></li>
+						<li><a href="#tab7" data-toggle="tab">Seventh</a></li>
+					</ul>
+					 </div>
+					  </div>
+					</div>
+					<div id="bar" class="progress progress-striped active">
+					  <div class="progress-bar"></div>
+					</div>
+					<div class="tab-content">
+					    <div class="tab-pane" id="tab1">
+					      1
+					    </div>
+					    <div class="tab-pane" id="tab2">
+					      2
+					    </div>
+						<div class="tab-pane" id="tab3">
+							3
+					    </div>
+						<div class="tab-pane" id="tab4">
+							4
+					    </div>
+						<div class="tab-pane" id="tab5">
+							5
+					    </div>
+						<div class="tab-pane" id="tab6">
+							6
+					    </div>
+						<div class="tab-pane" id="tab7">
+							7
+					    </div>
+						<ul class="pager wizard">
+							<li class="previous first" style="display:none;"><a href="javascript:;">First</a></li>
+							<li class="previous"><a href="javascript:;">Previous</a></li>
+							<li class="next last" style="display:none;"><a href="javascript:;">Last</a></li>
+						  	<li class="next"><a href="javascript:;">Next</a></li>
+						</ul>
+					</div>
+				</div>
+            </div>
 
-<script type="text/javascript">
-jQuery.noConflict(false);
-$(document).ready(function () {
-  	$('#rootwizard').bootstrapWizard({onTabShow: function(tab, navigation, index) {
-		var $total = navigation.find('li').length;
-		var $current = index+1;
-		var $percent = ($current/$total) * 100;
-		$('#rootwizard .progress-bar').css({width:$percent+'%'});
-	}});
-});
-</script>  	
+
+	<script>
+	$(document).ready(function() {
+	  	$('#rootwizard').bootstrapWizard({onTabShow: function(tab, navigation, index) {
+			var $total = navigation.find('li').length;
+			var $current = index+1;
+			var $percent = ($current/$total) * 100;
+			$('#rootwizard').find('.progress-bar').css({width:$percent+'%'});
+		}});
+	});
+	</script>
+
 @stop
