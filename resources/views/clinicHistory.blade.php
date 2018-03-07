17c7b38f3a5b89cda72c05d47c6b7f6bf4fb7561
GTRebbeca <rebbecagt@gmail.com>
2018-03-07 12:26:15 -0600

    subiendo base de wizard nuevo para historia clínica

--- resources/views/clinicHistory.blade.php
+++ resources/views/clinicHistory.blade.php
@@ -2,40 +2,7 @@
 
 @section('title', 'Boomedic')
 @section('content_header')
-<style type="text/css">
-.com { color: #93a1a1; }
-.lit { color: #195f91; }
-.pun, .opn, .clo { color: #93a1a1; }
-.fun { color: #dc322f; }
-.str, .atv { color: #D14; }
-.kwd, .prettyprint .tag { color: #1e347b; }
-.typ, .atn, .dec, .var { color: teal; }
-.pln { color: #48484c; }
 
-.prettyprint {
-  padding: 8px;
-  background-color: #f7f7f9;
-  border: 1px solid #e1e1e8;
-}
-.prettyprint.linenums {
-  -webkit-box-shadow: inset 40px 0 0 #fbfbfc, inset 41px 0 0 #ececf0;
-     -moz-box-shadow: inset 40px 0 0 #fbfbfc, inset 41px 0 0 #ececf0;
-          box-shadow: inset 40px 0 0 #fbfbfc, inset 41px 0 0 #ececf0;
-}
-
-/* Specify class=linenums on a pre to get line numbering */
-ol.linenums {
-  margin: 0 0 0 33px; /* IE indents via margin-left */
-}
-ol.linenums li {
-  padding-left: 12px;
-  color: #bebec5;
-  line-height: 20px;
-  text-shadow: 0 1px 0 #fff;
-}
-</style>
-    <script src="{{ asset('js/jquery.bootstrap.wizard.js') }}"></script>
-    <script src="{{ asset('js/prettify.js') }}"></script>
 @stop
 @section('content')
 
@@ -45,67 +12,117 @@ ol.linenums li {
 	    <h3 class="box-title">Historia Clínica</h3>
   	</div>
   	<div class="box-body">
-  	<div id="rootwizard">
-					<div class="navbar">
-					  <div class="navbar-inner">
-					    <div class="container">
-					<ul>
-					  	<li><a href="#tab1" data-toggle="tab">First</a></li>
-						<li><a href="#tab2" data-toggle="tab">Second</a></li>
-						<li><a href="#tab3" data-toggle="tab">Third</a></li>
-						<li><a href="#tab4" data-toggle="tab">Forth</a></li>
-						<li><a href="#tab5" data-toggle="tab">Fifth</a></li>
-						<li><a href="#tab6" data-toggle="tab">Sixth</a></li>
-						<li><a href="#tab7" data-toggle="tab">Seventh</a></li>
-					</ul>
-					 </div>
-					  </div>
-					</div>
-					<div id="bar" class="progress progress-striped active">
-					  <div class="progress-bar"></div>
-					</div>
-					<div class="tab-content">
-					    <div class="tab-pane" id="tab1">
-					      1
-					    </div>
-					    <div class="tab-pane" id="tab2">
-					      2
-					    </div>
-						<div class="tab-pane" id="tab3">
-							3
-					    </div>
-						<div class="tab-pane" id="tab4">
-							4
-					    </div>
-						<div class="tab-pane" id="tab5">
-							5
-					    </div>
-						<div class="tab-pane" id="tab6">
-							6
-					    </div>
-						<div class="tab-pane" id="tab7">
-							7
-					    </div>
-						<ul class="pager wizard">
-							<li class="previous first" style="display:none;"><a href="javascript:;">First</a></li>
-							<li class="previous"><a href="javascript:;">Previous</a></li>
-							<li class="next last" style="display:none;"><a href="javascript:;">Last</a></li>
-						  	<li class="next"><a href="javascript:;">Next</a></li>
-						</ul>
-					</div>
-				</div>
+  <div class="container" id="myWizard">
+  
+   <h3>Bootstrap Wizard</h3>
+  
+   <hr>
+  
+   <div class="progress">
+     <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="1" aria-valuemin="1" aria-valuemax="5" style="width: 20%;">
+       Step 1 of 5
+     </div>
+   </div>
+  
+   <div class="navbar">
+      <div class="navbar-inner">
+            <ul class="nav nav-pills">
+               <li class="active"><a href="#step1" data-toggle="tab" data-step="1">Step 1</a></li>
+               <li><a href="#step2" data-toggle="tab" data-step="2">Step 2</a></li>
+               <li><a href="#step3" data-toggle="tab" data-step="3">Step 3</a></li>
+               <li><a href="#step4" data-toggle="tab" data-step="4">Step 4</a></li>
+               <li><a href="#step5" data-toggle="tab" data-step="5">Step 5</a></li>
+            </ul>
+      </div>
+   </div>
+   <div class="tab-content">
+      <div class="tab-pane fade in active" id="step1">
+         
+        <div class="well"> 
+          
+            <label>Security Question 1</label>
+            <select class="form-control input-lg">
+              <option value="What was the name of your first pet?">What was the name of your first pet?</option>
+              <option value="Where did you first attend school?">Where did you first attend school?</option>
+              <option value="What is your mother's maiden name?">What is your mother's maiden name?</option>
+              <option value="What is your favorite car model?">What is your favorite car model?</option>
+            </select>
+            <br>
+            <label>Enter Response</label>
+            <input class="form-control input-lg">
+            
+        </div>
+
+         <a class="btn btn-default btn-lg next" href="#">Continue</a>
+      </div>
+      <div class="tab-pane fade" id="step2">
+         <div class="well"> 
+          
+            <label>Security Question 2</label>
+            <select class="form-control  input-lg">
+              <option value="What was the name of your first pet?">What was the name of your first pet?</option>
+              <option selected="" value="Where did you first attend school?">Where did you first attend school?</option>
+              <option value="What is your mother's maiden name?">What is your mother's maiden name?</option>
+              <option value="What is your favorite car model?">What is your favorite car model?</option>
+            </select>
+            <br>
+            <label>Enter Response</label>
+            <input class="form-control  input-lg">
+            
+         </div>
+         <a class="btn btn-default next" href="#">Continue</a>
+      </div>
+      <div class="tab-pane fade" id="step3">
+        <div class="well"> <h2>Step 3</h2> Add another step here..</div>
+         <a class="btn btn-default next" href="#">Continue</a>
+      </div>
+      <div class="tab-pane fade" id="step4">
+        <div class="well"> <h2>Step 4</h2> Add another almost done step here..</div>
+         <a class="btn btn-default next" href="#">Continue</a>
+      </div>
+      <div class="tab-pane fade" id="step5">
+        <div class="well"> <h2>Step 5</h2> You're Done!</div>
+         <a class="btn btn-success first" href="#">Start over</a>
+      </div>
+   </div>
+  
+   <hr>
+  
+   <a href="http://www.bootply.com/wj9gWh8ulj">Edit on Bootply</a>
+  
+   <hr>
+  
+</div>
             </div>
+        </div>
 
+				<script>
+					$('.next').click(function(){
 
-	<script>
-		$(document).ready(function(e) {
-		  	$('#rootwizard').bootstrapWizard({onTabShow: function(tab, navigation, index) {
-				var $total = navigation.find('li').length;
-				var $current = index+1;
-				var $percent = ($current/$total) * 100;
-				$('#rootwizard').find('.progress-bar').css({width:$percent+'%'});
-			}});
-		});
-	</script>
+					  var nextId = $(this).parents('.tab-pane').next().attr("id");
+					  $('[href=#'+nextId+']').tab('show');
+					  return false;
+					  
+					})
+
+					$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+					  
+					  //update progress
+					  var step = $(e.target).data('step');
+					  var percent = (parseInt(step) / 5) * 100;
+					  
+					  $('.progress-bar').css({width: percent + '%'});
+					  $('.progress-bar').text("Step " + step + " of 5");
+					  
+					  //e.relatedTarget // previous tab
+					  
+					})
+
+					$('.first').click(function(){
+
+					  $('#myWizard a:first').tab('show')
+
+					})
+				</script>
 
 @stop
