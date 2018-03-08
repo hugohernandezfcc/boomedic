eb5e747d9d642dbacd7eb69888310b3c572d363e
GTRebbeca <rebbecagt@gmail.com>
2018-03-08 17:01:26 -0600

    volviendo a subir migracion de clinic_history

---
+++ database/migrations/2018_03_08_225408_create_clinic_history_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClinicHistoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clinic_history', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user')->unsigned();
+            $table->foreign('user')->references('id')->on('users');
+            $table->integer('question_id')->nullable();
+            $table->text('question')->nullable();
+            $table->integer('answer_id')->nullable();
+            $table->longText('answer')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clinic_history');
+    }
+}
