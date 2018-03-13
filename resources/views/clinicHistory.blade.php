bcebcacc43f4d9b49af009b6f6b0fb95ce790ef3
GTRebbeca <rebbecagt@gmail.com>
2018-03-13 17:20:09 -0600

    agregando título al iframe

--- resources/views/clinicHistory.blade.php
+++ resources/views/clinicHistory.blade.php
@@ -234,13 +234,14 @@
                   Recipe. {{ $test->folio}}.<br>
                   Detalles:<br>
                   {{ $test->details }}<br><br>
-                  <input type="hidden" id="">
+
                   <button class="btn btn-default btn-flat btn-sm" data-toggle="modal" data-target="#myModal">Ver estudio</button>
                   <!-- Modal -->
                         <div class="modal fade" id="myModal" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
                           <div class="modal-dialog">
                             <div class="modal-content">
                               <div class="modal-header">
+                               <span style="font-size: 15px;"><b> {{ $test->name }} </b></span>
                                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                               </div>
                               <div class="modal-body">
