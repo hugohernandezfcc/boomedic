3c2dbeeee7aa0e9e109c7d9092a12e6c06a9001e
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2018-03-14 15:50:06 -0600

    agregando estilo a citas

--- resources/views/homemedical.blade.php
+++ resources/views/homemedical.blade.php
@@ -12,6 +12,11 @@
 		.info-box .progress .progress-bar {
 		    background: #160404;
 		}
+
+		.info-box-icon > img {
+		    max-width: 75%;
+		}
+
 	</style>
 
 	<div>
@@ -26,24 +31,25 @@
             	  	@endforeach
             	</select>
           	</div>
-          	@foreach($medAppoints as $cite)
-	          	<div class="info-box ">
-			        <span class="info-box-icon">
+          	<div id="listMedicalAppointment">
+	          	@foreach($medAppoints as $cite)
+		          	<div class="info-box ">
+				        <span class="info-box-icon">
+				        	<img src="{{$cite->profile_photo}}" class="img-circle" alt="User Image">
+				        </span>
 
-			        	<img src="{{$cite->profile_photo}}" class="img-circle" alt="User Image">
-			        
-			        </span>
-
-			        <div class="info-box-content">
-			          	<span class="info-box-text">{{$cite->firstname}} {{$cite->lastname}}</span>
-			          	<span class="info-box-number">{{$cite->age}} de edad</span>
-			          	<div class="progress">
-			            	<div class="progress-bar" style="width: 20%"></div>
-			          	</div>
-			          	<a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-			        </div>
-			    </div>
-		    @endforeach
+				        <div class="info-box-content">
+				          	<span class="info-box-text">{{$cite->firstname}} {{$cite->lastname}}</span>
+				          	<span class="info-box-number">{{$cite->age}} de edad</span>
+				          	<div class="progress">
+				            	<div class="progress-bar" style="width: 20%"></div>
+				          	</div>
+				          	<a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+				        </div>
+				        <br/>
+				    </div>
+			    @endforeach
+		    </div>
 		</div>
 		<div class="col-sm-8">
 
@@ -55,6 +61,12 @@
             $('select').select2({
                 width: "100%",
             });
+
+            var height;
+			height = window.screen.availHeight;
+			height = window.screen.availHeight-115;
+			document.getElementById('listMedicalAppointment').setAttribute("style","height:" + height + "px;overflow-y: auto;");
+
          });
     </script>
 	
