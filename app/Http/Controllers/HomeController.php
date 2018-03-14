107bd643dc4656d763e9c14b2977dcfa43a955fb
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2018-03-14 14:59:25 -0600

    quitando ;

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -129,7 +129,7 @@ class HomeController extends Controller
                     'userId'    => $user->id,
                     'labor'     => $join,
                     'photo'     => $user->profile_photo,
-                    'workplaces'=> $this->getWorkPlaces();
+                    'workplaces'=> $this->getWorkPlaces()
                 ]);       
         
 
