d3fea80a88c2742ff20282e61841f8081eac2c9f
GTRebbeca <rebbecagt@gmail.com>
2018-03-14 16:29:19 -0600

    rodando boton

--- resources/views/profileDoctor.blade.php
+++ resources/views/profileDoctor.blade.php
@@ -1051,12 +1051,12 @@ $('#target').Jcrop({
 
 			</div>
 				<div class="col-sm-12 form-group" align="right">
-								<div class="col-sm-6" align="right">
+								
 									<br/>
 					    		<button type="button" class="btn btn-default" data-toggle="modal" data-target="#modal-default">
 						                Agregar servicios
 						        </button>	
-					            </div>
+				
 
 
 					            
