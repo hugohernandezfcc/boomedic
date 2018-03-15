d78be0ac42002670d6a4bed54e35966a7d56243f
GTRebbeca <rebbecagt@gmail.com>
2018-03-15 12:47:37 -0600

    volviendo a subier app provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(Dispatcher $events)
     {
-       Blade::setEchoFormat('e(utf8_encode(%s))');
+       
         Carbon::setLocale(config('app.locale'));
       
         \URL::forceScheme('https');
