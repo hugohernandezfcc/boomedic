0eb6fabbb79c19f8142aa7d0dbd84b61e6206d68
GTRebbeca <rebbecagt@gmail.com>
2018-03-28 17:30:32 -0600

    nuevo tamaño de vista family en el celular

--- app/Http/Controllers/profile.php
+++ app/Http/Controllers/profile.php
@@ -78,7 +78,7 @@ class profile extends Controller
       if(count($family) < 1){
          array_push( $nodes, ['name' => 'Yo', 'photo' => $users[0]->profile_photo. '?'. Carbon::now()->format('h:i'), 'id' => '0']);
           for($i = 1; $i < 2; $i++){
-                array_push($nodes, ['name' => 'Agregar familiar', 'target' => [0] , 'photo' => 'https://image.freepik.com/iconos-gratis/signo-de-interrogacion_318-52837.jpg' , 'id' => $i]);
+                array_push($nodes, ['name' => 'Agregar familiar', 'target' => [0] , 'photo' => 'https://image.freepik.com/iconos-gratis/signo-de-interrogacion_318-52837.jpg' , 'id' => 'n']);
             }
       }   else {      
       
