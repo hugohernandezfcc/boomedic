dc25f3c7edaf8030b5e25d118c9f915823eb0905
GTRebbeca <rebbecagt@gmail.com>
2018-04-06 16:14:40 -0500

    estilo ya quedo de correp de confirmacion

--- resources/views/confirme.blade.php
+++ resources/views/confirme.blade.php
@@ -5,11 +5,11 @@
 
 @section('content')
 <div class="box">
-	  		<div class="alert alert-info" style="background-color: #7d7d7d !important; border-color: #7d7d7d !important">
-		        <h5><i class="icon fa fa-info"></i>Por favor confirme la cuenta con el enlace que fue enviado a su correo</h5><br>
-        <div align="right">
-            <a style="text-decoration: none;"  href="{{ url('returnverify/') }}" class="btn btn-secondary btn-flat">
-                 Reenviar email de confirmación
+	  		<div class="alert alert-info" style="background-color: #222d32 !important; border-color: #222d32 !important">
+		        <h5><i class="icon fa fa-info"></i>Por favor, confirma tu nueva cuenta con el enlace que fue enviado a tu correo</h5><br>
+        <div>
+            <a style="text-decoration: none;"  href="{{ url('returnverify/') }}" class="btn btn-default btn-flat text-black">
+                 Reenviar correo de confirmación
             </a>
         </div>
 
