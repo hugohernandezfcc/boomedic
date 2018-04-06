614ddd438c9b9e0e64f17bb5280b966ac32830fa
GTRebbeca <rebbecagt@gmail.com>
2018-04-06 11:28:04 -0500

    cambios email

--- resources/views/emails/confirmation_code.blade.php
+++ resources/views/emails/confirmation_code.blade.php
@@ -6,10 +6,16 @@
 <body>
     <h2>Hola {{ $name }}, gracias por registrarte en <strong>Boomedic</strong> !</h2>
     <p>Por favor confirma tu correo electrónico.</p>
-    <p>Para ello simplemente debes hacer click en el siguiente enlace:</p>
+    <p>Para ello debes hacer click en el siguiente enlace:</p>
 
-    <a href="{{ url('verify/' . $confirmation_code) }}">
-        Clic para confirmar tu email
-    </a>
+		    <table>
+    <tr>
+        <td style="background-color: #4ecdc4;border-color: #4c5764;border: 2px solid #45b7af;padding: 10px;text-align: center;">
+            <a style="display: block;color: #ffffff;font-size: 12px;text-decoration: none;text-transform: uppercase;"  href="{{ url('verify/' . $confirmation_code) }}">
+                 Clic para confirmar tu email
+            </a>
+        </td>
+    </tr>
+</table>
 </body>
 </html>
