1ac3f2da995c92502b4f73a3eafa1f8909ded499
GTRebbeca <rebbecagt@gmail.com>
2018-04-11 11:53:41 -0500

    mensaje de registro de sessión en boomedic

--- resources/views/confirme.blade.php
+++ resources/views/confirme.blade.php
@@ -6,7 +6,7 @@
 @section('content')
 <div class="box">
 	  		<div class="alert alert-info" style="background-color: #222d32 !important; border-color: #222d32 !important">
-		        <h5><i class="icon fa fa-info"></i>Por favor, confirma tu nueva cuenta con el enlace que fue enviado a tu correo</h5><br>
+		        <h5><i class="icon fa fa-info"></i>Bienvenido a Boomedic {{ $name }}, solo te faltan algunos pasos más. Confirma tu nueva cuenta con el enlace que fue enviado a tu correo para continuar.</h5><br>
         <div>
             <a style="text-decoration: none;"  href="{{ url('returnverify/') }}" class="btn btn-default btn-flat text-black">
                  Reenviar correo de confirmación
@@ -14,6 +14,6 @@
         </div>
 
 		    </div> 
-		    </div>       				      
+</div>       				      
  	
 @stop
