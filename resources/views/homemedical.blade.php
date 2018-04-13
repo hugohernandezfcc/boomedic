0a4358d407c421bd8bfbdf5b897243a8bc239f58
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2018-04-13 18:05:23 -0500

    cambiando el home

--- resources/views/homemedical.blade.php
+++ resources/views/homemedical.blade.php
@@ -9,68 +9,47 @@
 @section('content')
 	
 	<section class="content">
-      <!-- Small boxes (Stat box) -->
       <div class="row">
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box bg-aqua">
-            <div class="inner">
-              <h3>150</h3>
 
-              <p>Solicitudes de cita</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-bag"></i>
-            </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
+        <div class="col-lg-4 col-xs-6">
+          	<div class="small-box bg-aqua">
+	            <div class="inner">
+	              	<h3>150</h3>
+	              	<p>Solicitudes de cita</p>
+	            </div>
+	            <div class="icon">
+	              	<i class="fa fa-calendar"></i>
+	            </div>
+	            <a href="#" class="small-box-footer">Ver detalle <i class="fa fa-arrow-circle-right"></i></a>
+          	</div>
         </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box bg-green">
-            <div class="inner">
-              <h3>53<sup style="font-size: 20px">%</sup></h3>
 
-              <p>Saldos</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-stats-bars"></i>
-            </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
+        <div class="col-lg-4 col-xs-6">
+          	<div class="small-box bg-green">
+            	<div class="inner">
+              		<h3>53<sup style="font-size: 20px">%</sup></h3>
+              		<p>Saldos</p>
+            	</div>
+            	<div class="icon">
+              		<i class="ion ion-stats-bars"></i>
+            	</div>
+            	<a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          	</div>
         </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box bg-yellow">
-            <div class="inner">
-              <h3>44</h3>
 
-              <p>Expedientes</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-person-add"></i>
-            </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
+        <div class="col-lg-4 col-xs-6">
+          	<div class="small-box bg-yellow">
+            	<div class="inner">
+              		<h3>44</h3>
+              		<p>Expedientes</p>
+            	</div>
+            	<div class="icon">
+              		<i class="ion ion-person-add"></i>
+            	</div>
+            	<a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          	</div>
         </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box bg-red">
-            <div class="inner">
-              <h3>65</h3>
 
-              <p>Unique Visitors</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-pie-graph"></i>
-            </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-        <!-- ./col -->
       </div>
   </section>
 
