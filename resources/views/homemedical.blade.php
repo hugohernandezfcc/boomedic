cf8d4fcae7a4a8a02c0c1ad315f0a0319f2475d2
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2018-04-13 19:25:05 -0500

    homemedical

--- resources/views/homemedical.blade.php
+++ resources/views/homemedical.blade.php
@@ -56,7 +56,7 @@
           	<div class="small-box bg-red">
             	<div class="inner">
               		<h3>44</h3>
-              		<p>Expedientes</p>
+              		<p>Interacciones</p>
             	</div>
             	<div class="icon">
               		<i class="fa fa-comments"></i>
