661046d1dc17e697537d7a7411659a2c44bb1340
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2018-04-18 17:17:04 -0500

    agregnado cosas

--- app/Http/Controllers/clinicHistory.php
+++ app/Http/Controllers/clinicHistory.php
@@ -22,7 +22,6 @@ class clinicHistory extends Controller
     public function __construct()
     {
         $this->middleware('auth');
-       
     }
 
     /**
--- resources/views/previousattention.blade.php
+++ resources/views/previousattention.blade.php
@@ -7,7 +7,18 @@
 @stop
 
 @section('content')
+	<div class="box">
+	  	<div class="box-header with-border">
+		    <h3 class="box-title">Atención previa</h3>
+	  	</div>
+	  	<div class="box-body">
+	  		<p align="justify">
+	  			El presente apartado pretende que puedas enriquecer la información que deseas ver al momento de atender al paciente.
+	  		</p>
+	  		<br/>
 
-	hugo
+	  		
 
+		</div>
+	</div>	
 @stop
