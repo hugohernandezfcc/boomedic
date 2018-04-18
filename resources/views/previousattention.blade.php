4cebb939fc2eddb98fe54c6c865d40744d1feca9
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2018-04-18 17:42:13 -0500

    previousattention

--- resources/views/previousattention.blade.php
+++ resources/views/previousattention.blade.php
@@ -22,12 +22,21 @@
     	<div class="col-sm-3">
     		<div class="box">
 			  	<div class="box-header with-border">
-				    <h3 class="box-title">Atención previa</h3>
+				    <h3 class="box-title">Información del paciente</h3>
 			  	</div>
 			  	<div class="box-body">
-			  		<p align="justify">
-			  			El presente apartado pretende que puedas enriquecer la información que deseas ver al momento de atender al paciente.
-			  		</p>
+			  		<ul class="list-group list-group-unbordered">
+		                <li class="list-group-item">
+		                  	<b>Followers</b> <a class="pull-right">1,322</a>
+		                </li>
+		                <li class="list-group-item">
+		                  	<b>Following</b> <a class="pull-right">543</a>
+		                </li>
+		                <li class="list-group-item">
+		                  	<b>Friends</b> <a class="pull-right">13,287</a>
+		                </li>
+	              	</ul>
+
 				</div>
 			</div>	
     	</div>
