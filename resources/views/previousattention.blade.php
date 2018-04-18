5d5592b225a5ebb9de307a2dc9f4ce5d17086d16
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2018-04-18 17:21:33 -0500

    agregando

--- resources/views/previousattention.blade.php
+++ resources/views/previousattention.blade.php
@@ -15,10 +15,33 @@
 	  		<p align="justify">
 	  			El presente apartado pretende que puedas enriquecer la información que deseas ver al momento de atender al paciente.
 	  		</p>
-	  		<br/>
-
-	  		
-
 		</div>
 	</div>	
+
+	<div class="row" >
+    	<div class="col-sm-3">
+    		<div class="box">
+			  	<div class="box-header with-border">
+				    <h3 class="box-title">Atención previa</h3>
+			  	</div>
+			  	<div class="box-body">
+			  		<p align="justify">
+			  			El presente apartado pretende que puedas enriquecer la información que deseas ver al momento de atender al paciente.
+			  		</p>
+				</div>
+			</div>	
+    	</div>
+    	<div class="col-sm-9">
+    		<div class="box">
+			  	<div class="box-header with-border">
+				    <h3 class="box-title">Atención previa</h3>
+			  	</div>
+			  	<div class="box-body">
+			  		<p align="justify">
+			  			El presente apartado pretende que puedas enriquecer la información que deseas ver al momento de atender al paciente.
+			  		</p>
+				</div>
+			</div>	
+    	</div>
+    </div>
 @stop
