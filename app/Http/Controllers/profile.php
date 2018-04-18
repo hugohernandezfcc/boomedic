35a20bc66475aedac9c170a9671f3d7cc36a95a6
GTRebbeca <rebbecagt@gmail.com>
2018-04-18 11:23:10 -0500

    cambiando imagen en familia

--- app/Http/Controllers/profile.php
+++ app/Http/Controllers/profile.php
@@ -81,7 +81,7 @@ class profile extends Controller
       if(count($family) < 1){
          array_push( $nodes, ['name' => 'Yo', 'photo' => $users[0]->profile_photo. '?'. Carbon::now()->format('h:i'), 'id' => '0']);
           for($i = 1; $i < 2; $i++){
-                array_push($nodes, ['name' => 'Agregar familiar', 'target' => [0] , 'photo' => 'https://image.freepik.com/iconos-gratis/signo-de-interrogacion_318-52837.jpg' , 'id' => 'n']);
+                array_push($nodes, ['name' => 'Agregar familiar', 'target' => [0] , 'photo' => 'https://s3.amazonaws.com/abiliasf/Arrow_up_font_awesome.svg.png' , 'id' => 'n']);
             }
       }   else {      
       
