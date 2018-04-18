49f6bd15fe8d358560b6ce870e864144e4b61436
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2018-04-18 18:10:34 -0500

    agregando previous

--- resources/views/previousattention.blade.php
+++ resources/views/previousattention.blade.php
@@ -20,7 +20,29 @@
 
 	<div class="row" >
     	<div class="col-sm-3">
-    		<div class="box">
+    		<div class="box box-widget widget-user-2">
+            <!-- Add the bg color to the header using any of the bg-* classes -->
+	            <div class="widget-user-header bg-yellow">
+	              	<div class="widget-user-image">
+	                	<img class="img-circle" src="https://s3.amazonaws.com/abiliasf/3.jpg?10:37" alt="User Avatar">
+	              	</div>
+	              
+	              	<h3 class="widget-user-username">Nadia Carmichael</h3>
+	              	<h5 class="widget-user-desc">Lead Developer</h5>
+	            </div>
+            	<div class="box-footer no-padding">
+              		<ul class="nav nav-stacked">
+                		<li><a href="#">Projects <span class="pull-right badge bg-blue">31</span></a></li>
+                		<li><a href="#">Tasks <span class="pull-right badge bg-aqua">5</span></a></li>
+                		<li><a href="#">Completed Projects <span class="pull-right badge bg-green">12</span></a></li>
+                		<li><a href="#">Followers <span class="pull-right badge bg-red">842</span></a></li>
+              		</ul>
+            	</div>
+          	</div>
+
+
+
+    		<!-- <div class="box">
 			  	<div class="box-header with-border">
 				    <h3 class="box-title">Paciente</h3>
 			  	</div>
@@ -38,17 +60,11 @@
 		                </li>
 		                <li class="list-group-item">
 		                  	<b>Historia clínica</b>
-		                  	<ul class="list-group list-group-unbordered">
-		                  		<li class="list-group-item"><b>Alergias</b></li>
-		                  		<li class="list-group-item"><b>Antecedentes familiares</b></li>
-		                  		<li class="list-group-item"><b>Antecedentes mórbidos</b></li>
-		                  		<li class="list-group-item"><b>Habitos</b></li>
-		                  	</ul>
 		                </li>
 	              	</ul>
 
 				</div>
-			</div>	
+			</div> -->	
     	</div>
     	<div class="col-sm-9">
     		<div class="box">
