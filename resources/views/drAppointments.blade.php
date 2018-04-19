4ba690725bed5750484abb3e5fe8dbd20796c51b
GTRebbeca <rebbecagt@gmail.com>
2018-04-19 12:20:42 -0500

    quitando propiedad de edición

--- resources/views/drAppointments.blade.php
+++ resources/views/drAppointments.blade.php
@@ -97,68 +97,13 @@ jQuery.noConflict(false);
 	$('#calendar').fullCalendar({
 		
 		header: {
-			left: 'prev,next today',
+			left: 'prev,next',
 			center: 'title',
 			right: 'month,agendaWeek,agendaDay'
 		},
-		editable: true,
+		editable: false,
 		lang: 'es',
-		events: hor, /* [
-			{
-				title: 'All Day Event',
-				start: YM + '-01'
-			},
-			{
-				title: 'Long Event',
-				start: YM + '-07',
-				end: YM + '-10'
-			},
-			{
-				id: 999,
-				title: 'Repeating Event',
-				start: YM + '-09T16:00:00'
-			},
-			{
-				id: 999,
-				title: 'Repeating Event',
-				start: YM + '-16T16:00:00'
-			},
-			{
-				title: 'Conference',
-				start: YESTERDAY,
-				end: TOMORROW
-			},
-			{
-				title: 'Meeting',
-				start: TODAY + 'T10:30:00',
-				end: TODAY + 'T12:30:00'
-			},
-			{
-				title: 'Lunch',
-				start: TODAY + 'T12:00:00'
-			},
-			{
-				title: 'Meeting',
-				start: TODAY + 'T14:30:00'
-			},
-			{
-				title: 'Happy Hour',
-				start: TODAY + 'T17:30:00'
-			},
-			{
-				title: 'Dinner',
-				start: TODAY + 'T20:00:00'
-			},
-			{
-				title: 'Birthday Party',
-				start: TOMORROW + 'T07:00:00'
-			},
-			{
-				title: 'Click for Google',
-				url: 'http://google.com/',
-				start: YM + '-28'
-			}
-		]*/
+		events: hor, 
 		  eventClick: function(calEvent, jsEvent, view) {
 		  	console.log(calEvent);
 		  	$('#userp').attr('src', calEvent.photo);
