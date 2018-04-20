9b0a6dc1eaa6c57c9e5407f2a85093f6b5395f96
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2018-04-20 17:04:01 -0500

    agregando botón de crear receta

--- resources/views/prescriptions.blade.php
+++ resources/views/prescriptions.blade.php
@@ -10,6 +10,8 @@
 	<div class="row">
         <div class="col-md-3">
         	
+        	<a href="/" class="btn btn-secondary btn-block btn-flat margin-bottom">Crear</a>
+
         	@if($isMobile)
 				<div class="box box-solid collapsed-box">
 			@else
@@ -28,7 +30,9 @@
 		        <div class="box-body no-padding">
 		            <ul class="nav nav-pills nav-stacked">
 		                <li class="active">
-		                	<a href="compose.html" class="btn btn-primary btn-block margin-bottom">Compose</a>
+		                	<a href="#">
+		                		<i class="fa fa-envelope-o"></i> Sent
+		                	</a>
 		                </li>
 		                <li>
 		                	<a href="#">
@@ -50,7 +54,7 @@
 		        </div>
 		    </div>
 
-		    
+
 		</div>
 		<div class="col-md-9">
 
