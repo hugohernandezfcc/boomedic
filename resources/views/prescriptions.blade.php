f9f757f3979caae6b35b4ee05c9ad17e18cd854e
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2018-04-20 15:23:16 -0500

    agregando prescriptions

--- resources/views/prescriptions.blade.php
+++ resources/views/prescriptions.blade.php
@@ -7,8 +7,49 @@
 @stop
 
 @section('content')
+	<div class="row">
+        <div class="col-md-3">
+			<div class="box box-solid">
+		        <div class="box-header with-border">
+		            <h3 class="box-title">Folders</h3>
 
-	hugo daniel
+		            <div class="box-tools">
+		                <button type="button" class="btn btn-box-tool" data-widget="collapse">
+		                	<i class="fa fa-minus"></i>
+		                </button>
+		            </div>
+		        </div>
+		        <div class="box-body no-padding">
+		            <ul class="nav nav-pills nav-stacked">
+		                <li class="active">
+		                	<a href="#">
+		                		<i class="fa fa-inbox"></i> Inbox <span class="label label-primary pull-right">12</span>
+		                	</a>
+		                </li>
+		                <li>
+		                	<a href="#">
+		                		<i class="fa fa-envelope-o"></i> Sent
+		                	</a>
+		                </li>
+		                <li>
+		                	<a href="#">
+		                		<i class="fa fa-file-text-o"></i> Drafts
+		                	</a>
+		                </li>
+		                <li>
+		                	<a href="#">
+		                		<i class="fa fa-filter"></i> Junk <span class="label label-warning pull-right">65</span>
+		                	</a>
+		                </li>
+		                <li><a href="#"><i class="fa fa-trash-o"></i> Trash</a></li>
+		            </ul>
+		        </div>
+		    </div>
+		</div>
+		<div class="col-md-9">
+			djoasdjfoaisdffjaosidfjisdf
+		</div>
+	</div>
 
 
 @stop
