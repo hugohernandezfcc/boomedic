99b94eca3575c819824d259dda577734e911eb82
GTRebbeca <rebbecagt@gmail.com>
2018-04-24 18:13:21 -0500

    prueba de logout 2

--- resources/views/vendor/adminlte/page.blade.php
+++ resources/views/vendor/adminlte/page.blade.php
@@ -100,21 +100,23 @@
                                   <li class="user-footer" style="background-color: #222;">
                                     <div class="pull-center">
                             @if(config('adminlte.logout_method') == 'GET' || !config('adminlte.logout_method') && version_compare(\Illuminate\Foundation\Application::VERSION, '5.3.0', '<'))
-                              @if(!session()->get('parental'))
+                              
                                 <a href="{{ url(config('adminlte.logout_url', 'auth/logout')) }}" class="btn btn-default btn-block btn-flat" style="background:white; color:black;">
                                     <i class="fa fa-sign-out"></i> {{ trans('adminlte::adminlte.log_out') }}
                                 </a>
-                                @else
-                               <a href="{{ url('logoutback') }}" class="btn btn-default btn-block btn-flat" style="background:white; color:black;">
-                                    <i class="fa fa-sign-out"></i> {{ trans('adminlte::adminlte.log_out') }}
-                                </a>
-                                @endif
+                              
                             @else
+                              @if(!session()->get('parental'))
                                 <a
                                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
                                  class="btn btn-default btn-block btn-flat" style="background:white; color: black;">
                                     <i class="fa fa-sign-out"></i> {{ trans('adminlte::adminlte.log_out') }}
                                 </a>
+                                  @else
+                               <a href="{{ url('logoutback') }}" class="btn btn-default btn-block btn-flat" style="background:white; color:black;">
+                                    <i class="fa fa-sign-out"></i> {{ trans('adminlte::adminlte.log_out') }}
+                                </a>
+                                @endif
                                 <form id="logout-form" action="{{ url(config('adminlte.logout_url', 'auth/logout')) }}" method="POST" style="display: none;">
                                     @if(config('adminlte.logout_method'))
                                         {{ method_field(config('adminlte.logout_method')) }}
