efdf171e2a99999d0929f217f0a0ee66afceedd9
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2018-04-25 16:31:46 -0500

    agregando librería y modificando textarea

--- resources/views/attentions/prescriptionsform.blade.php
+++ resources/views/attentions/prescriptionsform.blade.php
@@ -10,55 +10,52 @@
 
 
 <div class="modal fade" id="prescription-form-modal">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title"> <i class="fa fa-edit"></i> Receta médica</h4>
+   <div class="modal-dialog">
+      <div class="modal-content">
+         <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+               <span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title"> <i class="fa fa-edit"></i> Receta médica</h4>
+         </div>
+         <div class="modal-body">
+
+            <div class="form-group">
+               <textarea class="form-control" id="receta" rows="10" cols="30" placeholder="Describe la prescripción médica ..."></textarea>
+            </div>
+            <script type="text/javascript">
+           
+
+              $(document).ready(function(){
+                  jQuery.noConflict(false);
+
+                  $('#receta').textcomplete([{
+                     match: /(^|\b)(\w{2,})$/,
+                     search: function (term, callback) {
+                        var words = ['google', 'google1','google13','goog3le1','google1','google1', 'facebook', 'github', 'microsoft', 'yahoo'];
+                       
+                        callback($.map(words, function (word) {
+                              document.getElementById('textcomplete-dropdown-1').style.zIndex = "1100";
+
+                              if(word.indexOf(term) === 0)
+                                 return word;
+                              else
+                                 return null;
+                           }
+                        ));
+                     }, replace: function (word) {
+                        return word + ' ';
+                     }
+                  }]);
+               });
+            </script>
+         </div>
+         <div class="modal-footer">
+            <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+         </div>
       </div>
-      <div class="modal-body">
-
-       <textarea class="receta" id="receta" ></textarea>
-
-        <script type="text/javascript">
-        
-
-        $(document).ready(function(){
-          jQuery.noConflict(false);
-
-          $('#receta').textcomplete([{
-            match: /(^|\b)(\w{2,})$/,
-            search: function (term, callback) {
-              var words = ['google', 'google1','google13','goog3le1','google1','google1', 'facebook', 'github', 'microsoft', 'yahoo'];
-              
-              callback($.map(words, function (word) {
-
-                document.getElementById('textcomplete-dropdown-1').style.zIndex = "1100";
-
-                if(word.indexOf(term) === 0)
-                  return word;
-                else
-                  return null;
-                  
-              }));
-            },
-            replace: function (word) {
-              return word + ' ';
-            }
-          }]);
-        });
-
-        </script>
-
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
     <!-- /.modal-content -->
-  </div>
+   </div>
   <!-- /.modal-dialog -->
 </div>
 
