d5943b4806534a3570333d16e3d6367a35a2cdf8
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2018-04-25 15:30:39 -0500

    sdf

--- resources/views/prescriptions.blade.php
+++ resources/views/prescriptions.blade.php
@@ -14,7 +14,74 @@
         <div class="col-md-3">
         	
         	<a class="btn btn-secondary btn-block btn-flat margin-bottom" data-toggle="modal" data-target="#prescription-form-modal">Generar receta </a>
-        	@include('attentions.prescriptionsform',  ['isMobile' => $isMobile])
+        	
+
+
+@if(!$isMobile)
+  <style type="text/css">
+    .modal-dialog {
+      width: 90%;
+      margin: 30px auto;
+    }
+  </style>
+@endif
+
+
+
+
+
+
+
+<div class="modal fade" id="prescription-form-modal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title"> <i class="fa fa-edit"></i> Receta médica</h4>
+      </div>
+      <div class="modal-body">
+
+        legalicen la mota putos...
+        <br/>
+       <textarea class="editable" id="one"></textarea>
+
+        <script type="text/javascript">
+        
+
+        $(document).ready(function(){
+          jQuery.noConflict(false);
+
+          $('.editable').textcomplete([{
+            match: /(^|\b)(\w{2,})$/,
+            search: function (term, callback) {
+              var words = ['google', 'facebook', 'github', 'microsoft', 'yahoo'];
+              callback($.map(words, function (word) {
+                return word.indexOf(term) === 0 ? word : null;
+              }));
+            },
+            replace: function (word) {
+              return word + ' ';
+            }
+          }]);
+        });
+
+        </script>
+
+        
+
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+    <!-- /.modal-content -->
+  </div>
+  <!-- /.modal-dialog -->
+</div>
+
 
         	@if($isMobile)
 				<div class="box box-solid collapsed-box">
