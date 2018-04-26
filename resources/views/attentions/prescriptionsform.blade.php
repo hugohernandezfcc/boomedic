24567ecb56b3906e76d6111437f33b3b9497f2da
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2018-04-26 17:28:19 -0500

    test

--- resources/views/attentions/prescriptionsform.blade.php
+++ resources/views/attentions/prescriptionsform.blade.php
@@ -109,7 +109,6 @@
             </script>
          </div>
          <div class="modal-footer">
-            <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
             <button type="button" class="btn btn-secondary btn-block btn-flat margin-bottom">Prescribir</button>
          </div>
       </div>
