a8316d4784d147b4c8e20f0e52ae80521da6bf45
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2018-04-26 23:35:31 -0500

    test

--- resources/views/attentions/prescriptionsform.blade.php
+++ resources/views/attentions/prescriptionsform.blade.php
@@ -35,7 +35,7 @@
                   
                </div>
             </div>
-            <br />
+            <br /><br />
             <div class="row">
                <div class="col-md-12">
                   <div class="form-group">
