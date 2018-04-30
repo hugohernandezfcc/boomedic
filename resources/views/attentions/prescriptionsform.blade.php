83b39e788107a5c6c2394f9e54eb214095f0f1d2
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2018-04-30 18:28:57 -0500

    test

--- resources/views/attentions/prescriptionsform.blade.php
+++ resources/views/attentions/prescriptionsform.blade.php
@@ -106,20 +106,20 @@
                   $('#currentMedicalAppointment').select2({ width: "100%" });
 
 
-                  // $("#wizardPrescription").steps({
-                  //    headerTag: "h3",
-                  //    bodyTag: "section",
-                  //    transitionEffect: "slideLeft",
-                  //    cssClass: "wizard",
-                  //    autoFocus: true,
-                  //    labels: {
-                  //       pagination: "Paginación",
-                  //       finish:     "Prescribir",
-                  //       next:       "Imprimir",
-                  //       previous:   "Prescribir",
-                  //       loading:    "Cargando"
-                  //    }
-                  // });
+                  $("#wizardPrescription").steps({
+                     headerTag: "h3",
+                     bodyTag: "section",
+                     transitionEffect: "slideLeft",
+                     cssClass: "wizard",
+                     autoFocus: true,
+                     labels: {
+                        pagination: "Paginación",
+                        finish:     "Prescribir",
+                        next:       "Imprimir",
+                        previous:   "Prescribir",
+                        loading:    "Cargando"
+                     }
+                  });
                });
               
                function loadMedicines() {
