b38d789c8f65d3486bf6531c537566af53b58964
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2018-05-08 15:53:48 -0500

    la volvi a cagar

--- config/app.php
+++ config/app.php
@@ -234,7 +234,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Image' => Intervention\Image\Facades\Image::class,
-        'Agent' => Jenssegers\Agent\Facades\Agent::class
+        'Agent' => Jenssegers\Agent\Facades\Agent::class,
         'PDF' => Barryvdh\DomPDF\Facade::class,
     ],
 
