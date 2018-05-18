f59472b8e6967cfa8fb0c8506bbc9f44e5950afb
GTRebbeca <rebbecagt@gmail.com>
2018-05-18 15:03:36 -0500

    tambien id para el span para reutilizar componente empty en otras partes

--- resources/views/empty/emptyData.blade.php
+++ resources/views/empty/emptyData.blade.php
@@ -7,7 +7,7 @@
 		<div align="center" class="col-sm-12 enun">
 			        <img src="{{ asset('images/empty-box.png') }}" height="200" width="200">
 			        <h3>OMG!</h3>
-				    <h4>No tienes {{ $title }} agregados, <br> podrías generar tu primer registro aquí</h4><br>
+				    <h4>No tienes {{ $title }} agregados <span id="spanEmpty">, <br> podrías generar tu primer registro aquí</span></h4><br>
 				    <form action="create" method="get" id="form_profile2">
 				    <button type="submit" class="btn btn-default btn-flat buttonEmpty"> Generar {{ $title }} </button><a class="btn text-muted">Más información</a>
 				    </form>
