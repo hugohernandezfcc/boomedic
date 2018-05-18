237782546f587dbad2708e9d496266d5f86d046e
GTRebbeca <rebbecagt@gmail.com>
2018-05-18 14:14:12 -0500

    subiendo cambios componente con botón para agregar

--- resources/views/empty/emptyData.blade.php
+++ resources/views/empty/emptyData.blade.php
@@ -7,5 +7,8 @@
 		<div align="center" class="col-sm-12 enun">
 			        <h3>OMG!</h3>
 				    <h4>No tienes {{ $title }} agregados, <br> podrías generar tu primer registro aquí</h4><br>
-				    <button class="btn btn-default btn-flat"> Generar {{ $title }} </button><button class="btn btn-default btn-flat"> ¿Qué es esto? </button>
+				    <form action="create" method="get" id="form_profile2">
+				    <button type="submit" class="btn btn-default btn-flat"> Generar {{ $title }} </button><a class="btn btn-default btn-flat"> ¿Qué es esto? </a>
+				    </form>
+
 		</div>
