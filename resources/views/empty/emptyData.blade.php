385253e1bb9c5860918e0610960aef305b9f7673
GTRebbeca <rebbecagt@gmail.com>
2018-05-18 14:58:12 -0500

    colocando id a componente para manejarlo más facilmente

--- resources/views/empty/emptyData.blade.php
+++ resources/views/empty/emptyData.blade.php
@@ -9,7 +9,7 @@
 			        <h3>OMG!</h3>
 				    <h4>No tienes {{ $title }} agregados, <br> podrías generar tu primer registro aquí</h4><br>
 				    <form action="create" method="get" id="form_profile2">
-				    <button type="submit" class="btn btn-default btn-flat"> Generar {{ $title }} </button><a class="btn text-muted">Más información</a>
+				    <button type="submit" class="btn btn-default btn-flat buttonEmpty"> Generar {{ $title }} </button><a class="btn text-muted">Más información</a>
 				    </form>
 
 		</div>
