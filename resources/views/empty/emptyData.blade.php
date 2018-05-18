039964e08588207120fcfb088f9afda183d3eae9
GTRebbeca <rebbecagt@gmail.com>
2018-05-18 14:26:52 -0500

    cambios de vacío

--- resources/views/empty/emptyData.blade.php
+++ resources/views/empty/emptyData.blade.php
@@ -5,10 +5,11 @@
 </style>
 
 		<div align="center" class="col-sm-12 enun">
+			        <img src="{{ asset('images/empty-box.png') }}" height="200" width="200">
 			        <h3>OMG!</h3>
 				    <h4>No tienes {{ $title }} agregados, <br> podrías generar tu primer registro aquí</h4><br>
 				    <form action="create" method="get" id="form_profile2">
-				    <button type="submit" class="btn btn-default btn-flat"> Generar {{ $title }} </button><a class="btn btn-default btn-flat"> ¿Qué es esto? </a>
+				    <button type="submit" class="btn btn-default btn-flat"> Generar {{ $title }} </button><a class="btn text-muted">Más información</a>
 				    </form>
 
 		</div>
