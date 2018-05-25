da44261e3101755d344e7654f85e8213361ca39d
GTRebbeca <rebbecagt@gmail.com>
2018-05-25 12:55:57 -0500

    prueba para cargar modal al cerrar chat

--- resources/views/medicalconsultations.blade.php
+++ resources/views/medicalconsultations.blade.php
@@ -376,7 +376,7 @@
                                                                 <div class="modal-dialog">
                                                                     <div class="modal-content">
                                                                          <div class="modal-header">
-                                                                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                             <button type="button" class="close" data-target="#{{ $appo->id }}" data-dismiss="modal" data-toggle="modal" aria-label="Close">
                                                                                 <span aria-hidden="true">&times;</span></button>
                                                                              <h4 class="modal-title"> <i class="fa fa-comments"></i> Ventana de Conversación</h4>
                                                                              </div>
