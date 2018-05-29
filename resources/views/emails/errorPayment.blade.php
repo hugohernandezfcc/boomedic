01ed531774adc7001500b02cabdf2d4f20e51d5b
GTRebbeca <rebbecagt@gmail.com>
2018-05-29 16:29:01 -0500

    cambiando correo de error

--- resources/views/emails/errorPayment.blade.php
+++ resources/views/emails/errorPayment.blade.php
@@ -5,10 +5,19 @@
 </head>
 <body>
 
-    <h2>Tú pago no fue procesado, por favor verifica que tengas dinero en tu método de pago seleccionado</h2>
+    <h2>Tú pago no fue procesado, por favor verifica esa forma de pago o cambiala.</h2>
     <strong>Detalles del Pago</strong><br/>
     <strong>Tarjeta:</strong> {!! $provider !!} {!! $card !!} <br/>
     <strong>Monto:</strong> {!! $amount!!} <br/>
+    <table>
+    <tr>
+         <td style="background-color: black;border-color: black;border: 2px solid black;padding: 10px;text-align: center;">
+            <a style="display: block;color: #ffffff;font-size: 12px;text-decoration: none;text-transform: uppercase;"  href="{{ url('/payment/index') }}">
+                 Cambiar método de pago
+            </a>
+        </td>
+    </tr>
+    </table>
     
 </body>
 </html>
