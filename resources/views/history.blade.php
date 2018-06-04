6e3a1dc519f4a5d2922592ba483ce6647abf3d2d
GTRebbeca <rebbecagt@gmail.com>
2018-06-04 10:56:20 -0500

    cambiando a modal-sm history para ver como se comporta mapa

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -101,7 +101,7 @@
               </div>
             </li>
             <div class="modal fade" id="modalmap" role="dialog">
-          <div class="modal-dialog">
+          <div class="modal-dialog modal-sm">
           <div class="modal-content">  
                          <div class="modal-header">
                           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -205,7 +205,7 @@
               </div>
             </li>
             <div class="modal fade" id="modalmap" role="dialog">
-          <div class="modal-dialog">
+          <div class="modal-dialog modal-sm">
           <div class="modal-content"> 
                          <div class="modal-header">
                           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -310,7 +310,7 @@
               </div>
             </li>
             <div class="modal fade" id="modalmap" role="dialog">
-          <div class="modal-dialog">
+          <div class="modal-dialog modal-sm">
           <div class="modal-content">  
                          <div class="modal-header">
                           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -414,7 +414,7 @@
               </div>
             </li>
             <div class="modal fade" id="modalmap" role="dialog">
-          <div class="modal-dialog">
+          <div class="modal-dialog modal-sm">
           <div class="modal-content"> 
                          <div class="modal-header">
                           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -518,7 +518,7 @@
               </div>
             </li>
             <div class="modal fade" id="modalmap" role="dialog">
-          <div class="modal-dialog">
+          <div class="modal-dialog modal-sm">
           <div class="modal-content">  
                          <div class="modal-header">
                           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -620,7 +620,7 @@
               </div>
             </li>
             <div class="modal fade" id="modalmap" role="dialog">
-          <div class="modal-dialog">
+          <div class="modal-dialog modal-sm">
           <div class="modal-content">  
                          <div class="modal-header">
                           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -724,7 +724,7 @@
               </div>
             </li>
             <div class="modal fade" id="modalmap" role="dialog">
-          <div class="modal-dialog">
+          <div class="modal-dialog modal-sm">
           <div class="modal-content">  
                          <div class="modal-header">
                           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
