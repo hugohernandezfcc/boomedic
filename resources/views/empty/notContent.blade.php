3374de0a92c690b968085c8184b36996dc1142cc
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2018-06-06 14:25:01 -0500

    test

--- resources/views/empty/notContent.blade.php
+++ resources/views/empty/notContent.blade.php
@@ -5,13 +5,13 @@
 	@if($indicator == 'NOT_DATA_LIST')
 		<li>
     		<div class="row">
-    			<div class="col-sm-6">
+    			<div class="col-sm-6" align="right">
     				<i class="fa fa-fw fa-edit "></i>
     			</div>
     			<div class="col-sm-6">
-    					@isset($title)
-    						No contienes <b>{{ $title }}</b> que mostrar.
-    					@endisset
+					@isset($title)
+						No contienes <b>{{ $title }}</b> que mostrar.
+					@endisset
     			</div>
     		</div>
     	</li>
