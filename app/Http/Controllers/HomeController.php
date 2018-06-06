8b53d6bb5ae8d229a2101bd2340e27cbccc9d0c8
GTRebbeca <rebbecagt@gmail.com>
2018-06-06 13:52:48 -0500

    probando carbon now utc

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -354,7 +354,7 @@ class HomeController extends Controller
            ->join('professional_information', 'medical_appointments.user_doctor', '=', 'professional_information.user')
            ->join('labor_information', 'medical_appointments.workplace', '=', 'labor_information.id')
            ->where('medical_appointments.user', '=', Auth::id())
-           ->where('medical_appointments.when', '>', Carbon::now())
+           ->where('medical_appointments.when', '>', Carbon::now('UTC'))
            ->select('medical_appointments.id','medical_appointments.created_at','users.name', 'users.id as did','medical_appointments.when', 'medical_appointments.status', 'labor_information.*', 'professional_information.specialty','users.profile_photo')->get();
 
                  return view('appointments', [
