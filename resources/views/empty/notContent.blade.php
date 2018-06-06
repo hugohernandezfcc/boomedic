cbf08e9ca2eb082107f44d0abac9fa620f040520
Hugo Hernández <hugo@doitcloud.consulting>
2018-06-06 02:47:30 -0500

    agregando cambios diferentes porque el break tuvo problemas

--- resources/views/empty/notContent.blade.php
+++ resources/views/empty/notContent.blade.php
@@ -2,33 +2,22 @@
 @isset($indicator)
     
 
-	@switch($indicator)
-	    @case('NOT_DATA_LIST')
-
-
-	    	<li>
-	    		<div class="row">
-	    			<div class="col-sm-6">
-	    					<i class="fa fa-fw"></i>
-	    			</div>
-	    			<div class="col-sm-6">
-	    					@isset($title)
-	    						No contienes {{ $title }} que mostrar.
-	    					@endisset
-	    			</div>
-	    		</div>
-	    	</li>
-
-
-	        @break
-
-	    @case(2)
-	        Second case...
-	        @break
-
-	    @default
-	        Default case...
-	@endswitch
+	@if($indicator == 'NOT_DATA_LIST')
+		<li>
+    		<div class="row">
+    			<div class="col-sm-6">
+    					<i class="fa fa-fw"></i>
+    			</div>
+    			<div class="col-sm-6">
+    					@isset($title)
+    						No contienes {{ $title }} que mostrar.
+    					@endisset
+    			</div>
+    		</div>
+    	</li>
+	@else
+		other content...
+	@endif
 
 
 @endisset
