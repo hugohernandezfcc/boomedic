db541ae3863df71ebce25f8d2dddde3af6646c96
GTRebbeca <rebbecagt@gmail.com>
2018-06-08 14:47:12 -0500

    cambios icono en tools doctor

--- resources/views/vendor/adminlte/partials/menu-item-user.blade.php
+++ resources/views/vendor/adminlte/partials/menu-item-user.blade.php
@@ -128,7 +128,7 @@
                 <!-- Tools en doctor-->
                  @if(session()->get('utype') == "doctor")              
                     <li>
-                        <a data-toggle="control-sidebar"><i class="fa fa-gears"></i><span class="label label-warning"><i class="fa fa-bug"></i></span></a>
+                        <a data-toggle="control-sidebar"><i class="fa fa-child"></i><span class="label label-warning">¡HOLA!</span></a>
                     </li>
                 @endif   
                  <!-- Tools en doctor--> 
