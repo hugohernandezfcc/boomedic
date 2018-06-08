127f911fb0cdb83cdcb251d433e512060b338e0e
GTRebbeca <rebbecagt@gmail.com>
2018-06-08 17:01:15 -0500

    cambios citas ajas

--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -432,9 +432,11 @@ span.round-tab:hover {
                                     url: "{{ url('HomeController/listpatients') }}", 
                                     success: function(result2){
                                   if(result2 == "listo"){
-                                    console.log(result2.length);
+                                    console.log(result2);
+                                       $('#stateCite').html("@include('empty.emptyData', ['emptyc' => 'not_buttom','title'  => 'Expedientes','icon'   => 'adminlte.empty-calendar'])");
                                   }else{    
-                                  console.log(result2.length);
+                                  console.log(result2);
+                                  $('#stateCite').html('');
                                   for(var g =0; g < result2.length; g++){    
                                     $('#stateCite').append('<li><a href="javascript:void(0)"><i class="menu-icon fa fa-birthday-cake bg-red"></i><div class="menu-info"><h4 class="control-sidebar-subheading">Langdons Birthday</h4><p>Will be 23 on April 24th</p><p>Will be 23 on April 24th</p></div></a></li>');
                                   }
