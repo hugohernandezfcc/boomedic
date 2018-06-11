fb355cc7c5d7ab2eb7907e5cf5371a1b6c543f95
GTRebbeca <rebbecagt@gmail.com>
2018-06-11 13:20:17 -0500

    prueba en tiempo mostrandolo antes de comparar

--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -446,18 +446,23 @@ span.round-tab:hover {
                                           gender = 'Femenino';
                                        if(gender == 'male')
                                           gender = 'Masculino';
+                               var now = moment().format("MM/DD/YYYY HH:mm:ss");
+                               if(now < moment(result2[0][g]['when'])){
+                               var tim ='en ' + moment.utc(moment(now).diff(moment(result2[0][g]['when']))).format("HH:mm:ss");
+                                } else {
+                                 var tim = 'hace ' + moment.utc(moment(now).diff(moment(result2[0][g]['when']))).format("HH:mm:ss");
+                                }
 
-                                                   
                               $('#tool').append('<div class=modal fade" role="dialog" id="'+ result2[0][g]['id'] +'"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close"  data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title">Modal x</h4></div><div class="modal-body"></div></div></div></div>');
 
                                   if(result2[0][g]['status'] == 'No completed'){  
-                                    $('#stateCite').append('<li><a data-toggle="modal" data-target="#'+ result2[0][g]['id'] +'"><i class="menu-icon fa fa-calendar-check-o bg-red"></i><div class="menu-info"><h4 class="control-sidebar-subheading">'+ result2[0][g]['name'] +'</h4><p>'+ gender +', edad: '+ result2[0][g]['age'] +'</p></div></a></li>');
+                                    $('#stateCite').append('<li><a data-toggle="modal" data-target="#'+ result2[0][g]['id'] +'"><i class="menu-icon fa fa-calendar-check-o bg-red"></i><div class="menu-info"><h4 class="control-sidebar-subheading">'+ result2[0][g]['name'] +'</h4><p>'+ gender +', edad: '+ result2[0][g]['age'] +'</p><p>'+ tim +'</p></div></a></li>');
                                   }
                                   if(result2[0][g]['status'] == 'Taked'){  
-                                    $('#stateCite').append('<li><a data-toggle="modal" data-target="#'+ result2[0][g]['id'] +'"><i class="menu-icon fa fa-calendar-check-o bg-green"></i><div class="menu-info"><h4 class="control-sidebar-subheading">'+ result2[0][g]['name'] +'</h4><p>'+ gender+', edad: '+ result2[0][g]['age'] +'</p></div></a></li>');
+                                    $('#stateCite').append('<li><a data-toggle="modal" data-target="#'+ result2[0][g]['id'] +'"><i class="menu-icon fa fa-calendar-check-o bg-green"></i><div class="menu-info"><h4 class="control-sidebar-subheading">'+ result2[0][g]['name'] +'</h4><p>'+ gender+', edad: '+ result2[0][g]['age'] +'</p><p>'+ tim +'</p></div></a></li>');
                                   }
                                   if(result2[0][g]['status'] == 'Registered'){  
-                                    $('#stateCite').append('<li><a data-toggle="modal" data-target="#'+ result2[0][g]['id'] +'"><i class="menu-icon fa fa-calendar-check-o bg-gray"></i><div class="menu-info"><h4 class="control-sidebar-subheading">'+ result2[0][g]['name'] +'</h4><p>'+ gender +', edad: '+ result2[0][g]['age'] +'</p></div></a></li>');
+                                    $('#stateCite').append('<li><a data-toggle="modal" data-target="#'+ result2[0][g]['id'] +'"><i class="menu-icon fa fa-calendar-check-o bg-gray"></i><div class="menu-info"><h4 class="control-sidebar-subheading">'+ result2[0][g]['name'] +'</h4><p>'+ gender +', edad: '+ result2[0][g]['age'] +'</p><p>'+ tim +'</p></div></a></li>');
                                   }
                                   }
 
