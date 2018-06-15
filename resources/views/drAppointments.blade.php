10e2ec77844ba1fe60609fbdb7966427d979eb87
GTRebbeca <rebbecagt@gmail.com>
2018-06-15 15:40:22 -0500

    pasando eventos a box

--- resources/views/drAppointments.blade.php
+++ resources/views/drAppointments.blade.php
@@ -17,19 +17,21 @@
 .fc-toolbar.fc-header-toolbar {
   margin-bottom: 0;
 } 
-
+.text-black{
+	color: black !important;
+}
+.text-gray{
+	color: #777 !important;
+}
 </style>
 
 @stop
 
 @section('content')
 
-<div class="box">
-
-  	<div class="box-header with-border">
 	    <h3 class="box-title">Citas</h3>
-  	</div>
-  	<div class="box-body">
+
+
   		    <div class="modal fade" role="dialog" id="modalsuccess">
                     <div class="modal-dialog modal-sm">
 
@@ -96,9 +98,13 @@
                       </div> 
             </div>
         <div class="col-md-3">
-          <div class="box box-solid">
+          <div class="box">
             <div class="box-header with-border">
               <h4 class="box-title">Tablero de Eventos</h4>
+              <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                </button>
+              </div>  
             </div>
             <div class="box-body">
               <!-- the events -->
@@ -114,17 +120,19 @@
             <!-- /.box-body -->
           </div>
           <!-- /. box -->
-          <div class="box box-solid">
+          <div class="box">
             <div class="box-header with-border">
-              <h3 class="box-title">Crear Evento</h3>
+              <h4 class="box-title">Crear Evento</h4>
+                            <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                </button>
+              </div>  
             </div>
             <div class="box-body">
               <div class="btn-group" style="width: 100%; margin-bottom: 10px;">
                 <!--<button type="button" id="color-chooser-btn" class="btn btn-info btn-block dropdown-toggle" data-toggle="dropdown">Color <span class="caret"></span></button>-->
                 <ul class="fc-color-picker" id="color-chooser">
-                  <li><a class="text-aqua" href="#"><i class="fa fa-square"></i></a></li>
                   <li><a class="text-blue" href="#"><i class="fa fa-square"></i></a></li>
-                  <li><a class="text-light-blue" href="#"><i class="fa fa-square"></i></a></li>
                   <li><a class="text-teal" href="#"><i class="fa fa-square"></i></a></li>
                   <li><a class="text-yellow" href="#"><i class="fa fa-square"></i></a></li>
                   <li><a class="text-orange" href="#"><i class="fa fa-square"></i></a></li>
@@ -133,8 +141,8 @@
                   <li><a class="text-red" href="#"><i class="fa fa-square"></i></a></li>
                   <li><a class="text-purple" href="#"><i class="fa fa-square"></i></a></li>
                   <li><a class="text-fuchsia" href="#"><i class="fa fa-square"></i></a></li>
-                  <li><a class="text-muted" href="#"><i class="fa fa-square"></i></a></li>
-                  <li><a class="text-navy" href="#"><i class="fa fa-square"></i></a></li>
+                  <li><a class="text-gray" href="#"><i class="fa fa-square"></i></a></li>
+                  <li><a class="text-black" href="#"><i class="fa fa-square"></i></a></li>
                 </ul>
               </div>
               <!-- /btn-group -->
@@ -151,9 +159,10 @@
           </div>
         </div>
        <div class="col-md-9">
+       	<div class="box">
 		<div id='calendar'></div>
+	    </div>
 	   </div>
-	</div>
 <script type="text/javascript">
 
   $(function () {
