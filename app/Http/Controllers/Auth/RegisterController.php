76fe34d81df29496952787c65cf9899a6521448a
GTRebbeca <rebbecagt@gmail.com>
2018-06-27 15:09:20 -0500

    cambiando criterios de validacion device

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,15 +67,7 @@ class RegisterController extends Controller
     public function fcm($code)
     {
        $array = explode("&", $code);
-       $deviceOld = DB::table('devices')->where([
-                                                    ['token_registration', '=', $array[0]],
-                                                    ['uuid_device', '!=', $array[1]],
-                                                ])->get();
-       if(count($deviceOld) > 0){
-        $device = devices::find($deviceOld->id);
-        $device->uuid_device = $array[1];
-        $device->save();
-       } else{
+
             $deviceOld2 = DB::table('devices')->where([
                                                     ['token_registration', '=', $array[0]],
                                                     ['uuid_device', '=', $array[1]],
@@ -84,20 +76,20 @@ class RegisterController extends Controller
                                                     ['token_registration', '!=', $array[0]],
                                                     ['uuid_device', '=', $array[1]],
                                                 ])->get();
-
-              if(count($deviceOld2) == 0){
-                   $device = new devices;
-                   $device->token_registration = $array[0];
-                   $device->uuid_device = $array[1];
-                   $device->save();
-                 }else{
              if(count($deviceOld3) > 0){
                     $device = devices::find($deviceOld3->id);
                     $device->token_registration = $array[0];
                     $device->save();
-                 }    
+                 }  
+            else{
+                  if(count($deviceOld2) == 0){
+                   $device = new devices;
+                   $device->token_registration = $array[0];
+                   $device->uuid_device = $array[1];
+                   $device->save();
+                 }
              }
-     }
+     
 
         return response()->json("token");
     }
