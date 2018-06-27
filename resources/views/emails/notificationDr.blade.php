c1c97bc94bceff2ffefb226fd7c549de2aacdc8c
GTRebbeca <rebbecagt@gmail.com>
2018-06-27 16:44:08 -0500

    prueba url sin nada

--- resources/views/emails/notificationDr.blade.php
+++ resources/views/emails/notificationDr.blade.php
@@ -17,7 +17,7 @@
 <table>
     <tr>
          <td style="background-color: black;border-color: black;border: 2px solid black;padding: 10px;text-align: center;">
-            <a style="display: block;color: #ffffff;font-size: 12px;text-decoration: none;text-transform: uppercase;"  href="boomedic://{{ url('medicalconsultations') }}">
+            <a style="display: block;color: #ffffff;font-size: 12px;text-decoration: none;text-transform: uppercase;" href="{{ url('medicalconsultations') }}">
                  Agregar horarios
             </a>
         </td>
