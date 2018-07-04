a26bab514a84e524cea4b9fb4a178f2b160bd9ed
GTRebbeca <rebbecagt@gmail.com>
2018-07-04 11:14:37 -0500

    prueba ruta  completa en custom url

--- resources/views/homemedical.blade.php
+++ resources/views/homemedical.blade.php
@@ -86,13 +86,13 @@
 			setTimeout( function() {
 					goMarket();
 				}, 25);
-		location.href = "boomedic://user/profile/144";
+		location.href = "boomedic:user/profile/144";
 		}
 		else if(IS_IOS) {
 			setTimeout( function() {
 					goMarket();
 				}, 25);
-			location.href = "boomedic://medicalconsultations";
+			location.href = "boomedic:user/profile/144";
 			} else {
 				alert("android and iOS only");
 			}
