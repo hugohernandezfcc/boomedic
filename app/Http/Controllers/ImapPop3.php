a1f7f147b0fdc8e0673a43d72edd84ba11610d8b
GTRebbeca <rebbecagt@gmail.com>
2018-07-12 17:40:12 -0500

    pasando datos function falta almacenar en aws

--- app/Http/Controllers/ImapPop3.php
+++ app/Http/Controllers/ImapPop3.php
@@ -13,4 +13,146 @@ class ImapPop3 extends Controller {
 		return $imbox;
 	}
 
+	public function attachment($imbox){
+			$emails = imap_search($inbox, 'ALL', SE_UID);
+	    if ($hdr = imap_check($inbox)) 
+		    {
+		        $msgCount = $hdr->Nmsgs;
+		        echo 'Mensajes recibidos: ' . $msgCount .'<br>';
+		    }
+	    else 
+		    {
+		        echo "Failed to get mail";
+		    }
+		/* if any emails found, iterate through each email */
+		if($emails) {
+
+		    $count = 1;
+
+		    /* put the newest emails on top */
+		    rsort($emails);
+
+			    /* for every email... */
+			    foreach($emails as $email_number) 
+			    {
+
+			        /* get information specific to this email */
+			        $overview = imap_fetch_overview($inbox,$email_number,0);
+
+			        $message = imap_fetchbody($inbox,$email_number,2);
+
+			        /* get mail structure */
+			        $structure = imap_fetchstructure($inbox, $email_number);
+
+			        $attachments = array();
+
+			        /* if any attachments found... */
+			        if(isset($structure->parts) && count($structure->parts)) 
+			        {
+			            for($i = 0; $i < count($structure->parts); $i++) 
+			            {
+			                $attachments[$i] = array(
+			                    'is_attachment' => false,
+			                    'filename' => '',
+			                    'name' => '',
+			                    'attachment' => ''
+			                );
+
+			                if($structure->parts[$i]->ifdparameters) 
+			                {
+			                    foreach($structure->parts[$i]->dparameters as $object) 
+			                    {
+			                        if(strtolower($object->attribute) == 'filename') 
+			                        {
+			                            $attachments[$i]['is_attachment'] = true;
+			                            $attachments[$i]['filename'] = $object->value;
+			                        }
+			                    }
+			                }
+
+			                if($structure->parts[$i]->ifparameters) 
+			                {
+			                    foreach($structure->parts[$i]->parameters as $object) 
+			                    {
+			                        if(strtolower($object->attribute) == 'name') 
+			                        {
+			                            $attachments[$i]['is_attachment'] = true;
+			                            $attachments[$i]['name'] = $object->value;
+			                        }
+			                    }
+			                }
+
+			                if($attachments[$i]['is_attachment']) 
+			                {
+			                    $attachments[$i]['attachment'] = imap_fetchbody($inbox, $email_number, $i+1);
+
+			                    /* 3 = BASE64 encoding */
+			                    if($structure->parts[$i]->encoding == 3) 
+			                    { 
+			                        $attachments[$i]['attachment'] = base64_decode($attachments[$i]['attachment']);
+			                    }
+			                    /* 4 = QUOTED-PRINTABLE encoding */
+			                    elseif($structure->parts[$i]->encoding == 4) 
+			                    { 
+			                        $attachments[$i]['attachment'] = quoted_printable_decode($attachments[$i]['attachment']);
+			                    }
+			                }
+			            }
+			        }
+
+			        /* iterate through each attachment and save it */
+			        foreach($attachments as $attachment)
+			        {
+			            if($attachment['is_attachment'] == 1)
+			            {
+			            	/*  Debo cambiar este por almacenaja en aws s3  */
+
+			                $filename = $attachment['name'];
+			                if(empty($filename)) $filename = $attachment['filename'];
+
+			                if(empty($filename)) $filename = time() . ".dat";
+			                $folder = "attachment";
+			                if(!is_dir($folder))
+			                {
+			                     mkdir($folder);
+			                }
+			                $fp = fopen("./". $folder ."/". $email_number . "-" . $filename, "w+");
+			                fwrite($fp, $attachment['attachment']);
+			                echo '<br>' . $filename;
+			                fclose($fp);
+			                $file_parts = pathinfo($filename);
+			                $zip = new ZipArchive();
+			                $res = $zip->open('attachment/'. $email_number . "-" .$filename);
+								if ($res === TRUE) {
+									  $zip->extractTo('c:\xampp\htdocs\attachment');
+									  $zip->close();
+									  echo '<br>descomprimio ok <br>';
+									  // Archivo descomprimido correctamente
+									} else {
+
+								  if($file_parts['extension'] == "rar"){
+									$rar_file = RarArchive::open('attachment/'. $email_number . "-" .$filename);
+									if ($rar_file) {
+										$entries = $rar_file->getEntries();
+											foreach ($entries as $entry) {
+											    $entry->extract('c:\xampp\htdocs\attachment'); // extraer el directorio actual
+											}
+										rar_close($rar_file);
+										echo '<br>descomprimio rar ok <br>';
+									}
+									}else{		
+								   echo '<br>error descomprimiendo<br>';
+								}
+							}
+			            }
+			        }
+			    }
+			} 
+			/* close the connection */
+			imap_close($inbox);
+
+			return "all attachment Downloaded";
+
+	}
+
 }
