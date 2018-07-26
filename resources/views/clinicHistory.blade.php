5720bdc24ae86a3d55563941acf9c5e0c5bd9d05
GTRebbeca <rebbecagt@gmail.com>
2018-07-26 15:57:53 -0500

    btn-block para ampliar el margen del botón

--- resources/views/clinicHistory.blade.php
+++ resources/views/clinicHistory.blade.php
@@ -406,7 +406,7 @@
                         <div class="timeline-item">
                         <span class="time">
                            <div class="btn-group"> 
-                              <a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="text-muted fa fa-ellipsis-v" style="font-weight: 900 !important"></i></a>
+                              <a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><div class="btn-block"><i class="text-muted fa fa-ellipsis-v" style="font-weight: 800 !important"></i></div></a>
                               <ul class="dropdown-menu dropdown-menu-right" role="menu" style="float: left !important;">
                                 <li><a onclick="send('{{ $fi[0]->id }}');" href="javascript:void(0)">Reenviar a correo personal</a></li>
                               </ul>
@@ -540,7 +540,7 @@
               <div class="timeline-item">
                         <span class="time">
                            <div class="btn-group"> 
-                              <a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="text-muted fa fa-ellipsis-v" style="font-weight: 800 !important"></i></a>
+                              <a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><div class="btn-block"><i class="text-muted fa fa-ellipsis-v" style="font-weight: 800 !important"></i></div></a>
                               <ul class="dropdown-menu dropdown-menu-right" role="menu" style="float: left !important;">
                                 <li><a onclick="send('{{ $test->id }}');" href="javascript:void(0)">Reenviar a correo personal</a></li>
                               </ul>
