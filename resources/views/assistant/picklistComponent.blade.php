882aed8cc81208dc0b11b39905491f668a16fb5b
GTRebbeca <rebbecagt@gmail.com>
2018-09-04 13:28:10 -0500

    subiendo cambios para picklist con foto doctor

--- resources/views/assistant/picklistComponent.blade.php
+++ resources/views/assistant/picklistComponent.blade.php
@@ -2,7 +2,7 @@
 <style type="text/css">
 		.lockscreen-wrapper {
 	      max-width: 100% !important;
-	  	  margin-top: 1% !important;
+	  	  margin-top: 5% !important;
 		}
 </style>
 
@@ -10,7 +10,7 @@
 	<div class="lockscreen-item" style="width: 100% !important;">
     <!-- lockscreen image -->
     <div class="lockscreen-image">
-      <img src="https://s3.amazonaws.com/abiliasf/profile-42914_640.png" alt="User Image">
+      <img src="https://s3.amazonaws.com/abiliasf/profile-42914_640.png" alt="User Image" id="imgDrA">
     </div>
     <!-- /.lockscreen-image -->
 
@@ -19,7 +19,7 @@
       <div class="input-group" style="display: block;">
 	            <select class="select2" name="doctor" id="mySelectd"> 
 	            @foreach($as as $assi)	
-                  <option id="{{ $assi->iddr }}">{{ $assi->name }}</option>
+                  <option value="{{ $assi->iddr }}">{{ $assi->name }}</option>
                 @endforeach  
                 </select>
       </div>
@@ -29,4 +29,27 @@
   </div>
  </div>
  <br>
+ 	<script type="text/javascript">
+
+ 	$(document).ready(function () {
+ 	 var dr = JSON.stringify(@php echo $as; @endphp);
+  		dr =JSON.parse(dr);
+ 			for(var z=0; z < dr.length; z++){
+ 				console.log(dr[z]['iddr']);
+ 				if(dr[z]['iddr'] ==  $('#mySelectd option:selected').val()){
+ 					$('#imgDrA').attr('src', dr[z]['profile_photo']);
+ 				}	
+ 			}
+ 		$('#mySelectd').on('change', function() {
+ 			for(var z=0; z < dr.length; z++){
+ 				console.log(dr[z]['iddr']);
+ 				if(dr[z]['iddr'] ==  $('#mySelectd option:selected').val()){
+ 					$('#imgDrA').attr('src', dr[z]['profile_photo']);
+ 				}	
+ 			}
+
+ 			})
+ 		})
+
+ 	</script>
  @endif
