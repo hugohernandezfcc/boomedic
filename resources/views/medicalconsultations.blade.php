b5fab58a5ba46da064e8efd868b94560fbbe5842
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2018-09-11 18:04:11 -0500

    changend

--- resources/views/medicalconsultations.blade.php
+++ resources/views/medicalconsultations.blade.php
@@ -1319,7 +1319,7 @@ function prevTab(elem) {
 
             var input = document.getElementById('address');
               new google.maps.places.Autocomplete(input);
-            var markerUser = "{{ asset('test17.png') }}";
+            var markerUser = "{{ asset('finish.png') }}";
            
 
             //Marker
