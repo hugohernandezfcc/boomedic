015ac6ac46d020267d64381fa7c3e3c6673e91da
GTRebbeca <rebbecagt@gmail.com>
2018-09-13 15:24:17 -0500

    prueba encerrandolo en !

--- resources/views/medicalconsultations.blade.php
+++ resources/views/medicalconsultations.blade.php
@@ -1377,16 +1377,15 @@ function prevTab(elem) {
                             var complng2 = markerP.getPosition().lng() - 0.04;  
                         }
                         //console.log('marker'+ markerP.getPosition().lat() +' : ' + complat +'-'+complat2);
-                    if (strictBounds.contains(map.getCenter()))
-                        {  
+
                       if(this.center.lat() > complat || this.center.lng() > complng || this.center.lat() < complat2 || this.center.lng() < complng2){
 
                           $('#dragmap').fadeIn();
                           var latlng = new google.maps.LatLng(this.center.lat(),this.center.lng());
                           $('#dragbutton').prop('data-lng',latlng);
                         }
-                          return;
-                        }else{
+
+                    if (!strictBounds.contains(map.getCenter())){
                               map.setCenter(new google.maps.LatLng(pos));
                         }
                                     
