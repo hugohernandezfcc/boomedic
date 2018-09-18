d8a8420f018e93e7ed21f62bb8947efce1c58564
GTRebbeca <rebbecagt@gmail.com>
2018-09-18 11:16:51 -0500

    Cambiando a solo cargando

--- resources/views/medicalconsultations.blade.php
+++ resources/views/medicalconsultations.blade.php
@@ -567,11 +567,11 @@
 
 
 
-        <div id="loadingmodal" class="modal fade" role="dialog" style="background: rgba(0, 0, 0, 0.8);">
+<div id="loadingmodal" class="modal fade" role="dialog" style="background: rgba(0, 0, 0, 0.8);">
       <div class="modal-dialog">
           <div class="modal-content-2">
             <div align="center">
-          <h1><i class="fa fa-refresh fa-spin"></i><br/>Cargando...</h1><br/>(Esto podría tardar unos segundos)
+          <h1><i class="fa fa-refresh fa-spin"></i><br/>Cargando...</h1>
               </div>
           </div>
       </div>
