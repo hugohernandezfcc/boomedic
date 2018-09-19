2ca12a2f58a5eafe941ba067f35b989568cd049a
GTRebbeca <rebbecagt@gmail.com>
2018-09-19 15:09:59 -0500

    cambiando fotos si estan vacias

--- resources/views/vendor/adminlte/login.blade.php
+++ resources/views/vendor/adminlte/login.blade.php
@@ -181,7 +181,11 @@
                                       success: function(result){
                                         for(var z =0; z < result.length; z++){
                                              $('.formfast').show();
-                                            $('.formfast').append('<div class="widget-user-header" style="background: #2f2f2f;"><div class="widget-user-image"><img class="img-circle" src="'+ result[z]['profile_photo'] +'" alt="User Avatar" style="width: 35px !important;"></div><h4 class="widget-user-desc">'+ result[z]['name'] +'</h3></div>');
+                                             if(result[z]['profile_photo'] == "")
+                                                var photo = "https://s3.amazonaws.com/abiliasf/profile-42914_640.png";
+                                             else
+                                                var photo = result[z]['profile_photo'];
+                                            $('.formfast').append('<div class="widget-user-header" style="background: #2f2f2f;"><div class="widget-user-image"><img class="img-circle" src="'+ photo +'" alt="User Avatar" style="width: 35px !important;"></div><h4 class="widget-user-desc">'+ result[z]['name'] +'</h3></div>');
 
                                         }
                                         alert(result);
