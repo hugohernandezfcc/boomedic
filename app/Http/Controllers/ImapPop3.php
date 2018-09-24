6fa3a6577a709b83a588eba471dfdb9919614446
GTRebbeca <rebbecagt@gmail.com>
2018-09-24 17:49:32 -0500

    validando con false

--- app/Http/Controllers/ImapPop3.php
+++ app/Http/Controllers/ImapPop3.php
@@ -16,7 +16,7 @@ class ImapPop3 extends Controller {
 
 	public function connect($host, $port, $email, $pass){
 		$imbox = imap_open("{". $host .":".$port."/pop3/novalidate-cert}INBOX", $email, $pass);
-		if($imbox)
+		if($imbox != false)
 		return $imbox;
 		else
 		return 'error';	
