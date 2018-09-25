29e05a38d697ff547f409d490e61b4b83f443caa
GTRebbeca <rebbecagt@gmail.com>
2018-09-25 11:23:07 -0500

    pasando comprobación de errores con imap3

--- app/Http/Controllers/ImapPop3.php
+++ app/Http/Controllers/ImapPop3.php
@@ -15,7 +15,7 @@ class ImapPop3 extends Controller {
 	}
 
 	public function connect($host, $port, $email, $pass){
-		$imbox = imap_open("{". $host .":".$port."/pop3/novalidate-cert}INBOX", $email, $pass);
+		$imbox = imap_open("{". $host .":".$port."/pop3/novalidate-cert}INBOX", $email, $pass)or die(imap_last_error())or die("can't connect: ".imap_last_error());
 		return $imbox;
 
 	}
