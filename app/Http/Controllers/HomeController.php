9ff15eb120781fe024169b6902ad16e4e215ce30
GTRebbeca <rebbecagt@gmail.com>
2018-09-25 17:46:01 -0500

    pasando informacion de los correos

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -453,7 +453,10 @@ class HomeController extends Controller
                            $result2 = $result->unique('date_email');
                            if(count($result2) > 0){
                             foreach($result2 as $mail){
-                                array_push($array, ['type' => 'correo', 'title' => $mail->subject_email, 'name' => $mail->email, 'profile_photo' => 'https://icon-icons.com/icons2/936/PNG/512/inbox_icon-icons.com_73527.png', 'created_at' => $mail->created_at]);
+                                $details = $mail->subject;
+                                if($mail->subject = null)
+                                    $details = $mail->details;
+                                array_push($array, ['type' => 'correo', 'title' => $details, 'name' => $mail->email, 'profile_photo' => 'https://icon-icons.com/icons2/936/PNG/512/inbox_icon-icons.com_73527.png', 'created_at' => $mail->created_at]);
                               }
                            } 
                 
