61a75093e3ce2dcc0e701c9ca9ee67bf183106d3
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2018-12-19 03:38:43 -0600

    adding map

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -591,10 +591,265 @@
 	                    	</script>
 
 		         	    </div>
-		         	    <div class="tab-pane" id="address">Settings123</div>
+		         	    <div class="tab-pane" id="address">
+
+		         	    	<div align="center">
+		         	    		
+		         	    		@if($latitude == "" && $longitude == "")
+		                    	         @include('empty.emptyData', 
+		                                    [
+		                                      'emptyc' => 'buttom',
+		                                      'title'  => 'No se ha agregado dirección',
+		                                      'icon'   => 'adminlte.empty-house'
+		                                    ]
+		                                  )
+		                                       <script type="text/javascript">
+		                                          $('#form_profile2').attr("action", "/user/edit/complete");
+		                                          $('.buttonEmpty').text('Agregar dirección');
+		                                          $('.spanEmpty1').html('No se ha agregado dirección');
+		                                       </script>   
+		                    	   <input type="hidden" id="nullmap" value="true">
+		                    	@else   
+		                          <div id="mapAddressUser"></div>
+		                          <input type="hidden" id="nullmap" value="false">
+		                        @endif
+
+		         	    	</div>
+
+		         	    </div>
 		         	</div>
 		        </div>
         	</div>
     	</div>
     </section>
+
+    <script type="text/javascript">
+		function fun(a) {
+		    document.getElementById('sea').value = a.getAttribute("data-value");
+		    document.getElementById('idfam').value = a.getAttribute("data-id");
+		    document.getElementById("resp").innerHTML = "";
+		    $("#sav").removeAttr("disabled");   
+		}
+
+
+
+				$(document).ready(function(){
+					    $("#alertf").fadeTo(3000, 500).fadeOut(500, function(){
+						    $("#alertf").fadeOut(500);
+						});
+ 						 	$("#sea").on("keyup", function(e) {
+
+						    		if(e.which == 32) {
+					                   $.ajaxSetup({
+				                        headers: {
+				                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+				                        }
+				                    });
+				                 var sea = document.getElementById('sea').value;
+				                           $.ajax({     
+				                             type: "POST",                 
+				                             url: "{{ url('user/userSearch') }}",  
+				                              data: { "search" : sea }, 
+				                              dataType: 'json',                
+				                             success: function(data)             
+				                             {
+				                             if(data.length == 0){
+				                             	document.getElementById("resp").innerHTML = "No existe usuario registrado...";
+				                             	
+    											}else {
+
+    													document.getElementById("resp").innerHTML = "Coincidencias: ";
+    												for(var i= 0; i < data.length; i++){
+				                     				if(data[i]['profile_photo'] == null){
+				                     				$('#resp').append('<div style="margin-left:5%;"><img src="https://s3.amazonaws.com/abiliasf/profile-42914_640.png" class="img-circle" style="width:25px; height:25px;"><a data-id="'+ data[i]['id'] +'" data-value="'+ data[i]['name'] +'" onclick="fun(this);" class="btn text-muted" style="text-align: left;white-space: normal;">'+ data[i]['name'] +'</a></div>');
+				                     				}else{
+				                     				 $('#resp').append('<div style="margin-left:5%;"><img src="'+ data[i]['profile_photo'] +'" class="img-circle" style="width:25px; height:25px;"><a data-id="'+ data[i]['id'] +'" data-value="'+ data[i]['name'] +'" onclick="fun(this);" class="btn text-muted" style="text-align: left;white-space: normal;">'+ data[i]['name'] +'</a></div>');
+				                     				}
+				                     				}
+				                             	} 
+    											}
+				                            
+				                         });
+					            } else{
+					        	 var value = $(this).val().toLowerCase();
+						   		 $("#resp div").filter(function() {
+						    	  $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+						    });
+					            }
+						  });
+
+					                   
+					});
+
+			window.onload = function(){
+
+				        if("@php echo $agent->isMobile(); @endphp"){
+				            height = window.screen.availHeight;
+				                       // alert("Altura: "+height);
+				                        //Para Android Puro
+				            if(height >= 1000 && height <= 1300){
+				                var h = height*0.30;
+				                height = Math.floor(h);
+				            }else if(height >=1800){ //para android con capa personalizada
+				              height -= 1600;
+				            }else
+				            {
+				              height -=315; //android avierto desde chrome
+				            }
+				        }else{
+				          height = window.screen.availHeight-315;
+				        }
+				       @if(!empty($latitude) && !empty($longitude))
+				        document.getElementById('mapAddressUser').setAttribute("style","height:" + height + "px");
+				        @endif
+
+
+					@if(!empty($status))
+    				initAutocomplete();
+    				@endif
+    				@if(empty($status) && !empty($latitude))
+    					initMapAddressUser();
+					@endif
+    			};
+		      // This example displays an address form, using the autocomplete feature
+		      // of the Google Places API to help users fill in the information.
+
+		      // This example requires the Places library. Include the libraries=places
+		      // parameter when you first load the API. For example:
+		      // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
+		      var placeSearch, autocomplete;
+		      var componentForm = {
+		        street_number: 'short_name',
+		        route: 'long_name',
+		        locality: 'long_name',
+		        administrative_area_level_1: 'short_name',
+		        country: 'long_name',
+		        postal_code: 'short_name'
+		      };
+
+		      function initAutocomplete() {
+		        // Create the autocomplete object, restricting the search to geographical
+		        // location types.
+		        autocomplete = new google.maps.places.Autocomplete(
+		         (document.getElementById('autocomplete')),
+		            {types: ['geocode']});
+
+								        // When the user selects an address from the dropdown, populate the address
+		        // fields in the form.
+		        autocomplete.addListener('place_changed', fillInAddress);
+
+		      }
+
+		      function fillInAddress() {
+		        // Get the place details from the autocomplete object.
+		        var place = autocomplete.getPlace();
+
+		        for (var component in componentForm) {
+		          document.getElementById(component).value = '';
+		          document.getElementById(component).disabled = false;
+		        }
+		            document.getElementById('latitudeFend').value = place.geometry.location.lat();
+		            document.getElementById('longitudeFend').value = place.geometry.location.lng();
+		        // Get each component of the address from the place details
+		        // and fill the corresponding field on the form.
+		        for (var i = 0; i < place.address_components.length; i++) {
+		          var addressType = place.address_components[i].types[0];
+		          if (componentForm[addressType]) {
+		            var val = place.address_components[i][componentForm[addressType]];
+		            document.getElementById(addressType).value = val;
+		          }
+		        }
+		      }
+
+		      // Bias the autocomplete object to the user's geographical location,
+		      // as supplied by the browser's 'navigator.geolocation' object.
+		      function geolocate() {
+
+		        if (navigator.geolocation) {
+		          navigator.geolocation.getCurrentPosition(function(position) {
+		            var geolocation = {
+		              lat: position.coords.latitude,
+		              lng: position.coords.longitude
+		            };
+		            document.getElementById('latitudeFend').value = geolocation.lat; 
+		            document.getElementById('longitudeFend').value = geolocation.lng;
+		            var circle = new google.maps.Circle({
+		              center: geolocation,
+		              radius: position.coords.accuracy
+		            });
+		            autocomplete.setBounds(circle.getBounds());
+		          });
+		        }
+		      }	
+
+		    </script>
+
+	    @if( empty($status) )
+
+	    	<script type="text/javascript">
+	    	
+
+	    		var counter = -1;
+		      	function initMapAddressUser() {	
+			      	if(!counter > 0){
+			      		var map = new google.maps.Map(document.getElementById('mapAddressUser'), {
+				          zoom: 14,
+				          center: {lat: {{ $latitude }}  , lng: {{ $longitude }} }
+				        });
+
+
+				        var image = "https://s3.amazonaws.com/abiliasf/markerCasa.png";
+				        
+				        var beachMarker = new google.maps.Marker({
+				          position: {lat: {{ $latitude }}  , lng: {{ $longitude }} },
+				          map: map,
+				          icon: image
+				        });
+				    }
+			        counter++;
+		      	
+		      }
+	    	</script>
+		@endif
+
+		<link rel="stylesheet" href="{{ asset('css/jquery.Jcrop.css') }}" type="text/css" />
+		<script type="text/javascript" src="{{ asset('js/jquery.color.js') }}"></script>
+		<script type="text/javascript" src="{{ asset('js/jquery.Jcrop.js') }}"></script>
+		<script type="text/javascript">
+
+
+					$('#target').Jcrop({
+						    aspectRatio: 1,  
+					        boxWidth: 300,
+					        boxHeight: 300,
+					        setSelect: [0,0,300,300],
+					        bgOpacity: .9,
+					         bgColor:     'black',
+					        onSelect: updateCoords,
+					        onChange: updateCoords
+					    }, function(){
+					        CropAPI = this;
+					    });
+
+			     function updateCoords(c){
+			      $('#x').val(c.x);
+			      $('#y').val(c.y);
+			      $('#w').val(c.w);
+			      $('#h').val(c.h);
+			     };
+			     function checkCoords()
+			    {
+			      if (parseInt($('#w').val())>0) return true;
+			      alert('Seleccione una coordenada para subir');
+			      return false;
+			    };
+
+	    </script>
+
+
+
+@stop
+
+    
 @stop
