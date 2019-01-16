7e9ec9c0c537d51aec27f2bfbf6bb1a2f98c342f
GTRebbeca <rebbecagt@gmail.com>
2019-01-16 17:21:53 -0600

    subiendo tabla

--- resources/views/paymentsdoctor.blade.php
+++ resources/views/paymentsdoctor.blade.php
@@ -48,10 +48,47 @@
 </div>        
        <div class="box">
 	  <div class="box-header with-border">
-	      <h3 class="box-title">Saldos</h3>  
+	      <h3 class="box-title">Histórico de Saldos</h3>  
 	  </div> 
-	  <div class="box-body content">
 
+	  <div class="box-body content">
+	  					<table id="paymentmethodtable" class="display responsive nowrap table" cellspacing="0" width="100%">
+				                <thead>
+				                    <tr>
+				                    	 <th></th>
+				                        <th class="all">Nro. Transacción</th>
+				                        <th class="desktop">Fecha</th>
+				                        <th class="desktop">Lugar</th>
+				                        <th class="desktop">Paciente</th>
+				                        <th class="desktop">Monto</th>
+				                    </tr>
+				                </thead>
+
+				                <tbody>
+					     	@foreach ($transaction as $tr)
+					     		@if($tr->type_doctor == 'Owed')
+								     <tr>
+								     	<td></td>
+						             	<td style="color: red;">{{ $tr->transaction }} <br/></td>
+						             	<td style="color: red;">{{ $tr->when }}</td>
+						             	<td style="color: red;">{{ $tr->place }}</td>
+						             	<td style="color: red;">{{ $tr->name }}</td>
+						             	<td style="color: red;">{{ $tr->amount }}</td>
+						             </tr>
+						        @endif  
+						        @if($tr->type_doctor == 'Paid')
+								     <tr>
+								     	<td></td>
+						             	<td style="color: green;">{{ $tr->transaction }} <br/></td>
+						             	<td style="color: green;">{{ $tr->when }}</td>
+						             	<td style="color: green;">{{ $tr->place }}</td>
+						             	<td style="color: green;">{{ $tr->name }}</td>
+						             	<td style="color: green;">{{ $tr->amount }}</td>
+						             </tr>
+						        @endif        
+			             	@endforeach
+								<tbody>
+				    	 </table>
 	  </div>
 </div>	  	
 @stop
