65906d2df9f59e1c4d8051138bd0d8e9f6ad7485
GTRebbeca <rebbecagt@gmail.com>
2019-01-18 14:10:25 -0600

    correo comentado momentaneamente.

--- resources/views/drAppointments.blade.php
+++ resources/views/drAppointments.blade.php
@@ -424,6 +424,7 @@
            }
            },
 		  eventClick: function(calEvent, jsEvent, view) {
+        $('#buttondetail').html('');
 		   	if(calEvent.typ != "3"  && calEvent.typ != "2"){
 		   	$('#userp').css('display','block');	
 		  	$('#userp').attr('src', calEvent.photo + '?1');
