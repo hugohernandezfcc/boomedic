aa69bfa9a08a1bb00bdfbd3b55f5c3bb6ad49893
GTRebbeca <rebbecagt@gmail.com>
2019-01-30 17:44:29 -0600

    create class scheduller para posología de tratamientos

---
+++ app/Console/Commands/medicationExecute.php
@@ -0,0 +1,46 @@
+<?php
+namespace App\Console\Commands;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+use App\User;
+use App\Medications;
+use App\email;
+use Mail;
+use App\Http\Controllers\payments;
+
+class medicationExecute extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'MedicationExecute:send';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send an email to payment no procesed';
+    /**
+     * Create a new command instance.
+     *
+
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+
+        
+    }
+}
