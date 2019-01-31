e66305c1c2084ba7069e0bcdfe046e500099fc16
GTRebbeca <rebbecagt@gmail.com>
2019-01-31 13:53:57 -0600

    kernel add task scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,7 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use Illuminate\Console\Command;
+use Illuminate\Console\Commands;
 
 class Kernel extends ConsoleKernel
 {
@@ -15,6 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
          '\App\Console\Commands\paymentExecute',
+         '\App\Console\Commands\MedicationExecute',
     ];
 
     /**
