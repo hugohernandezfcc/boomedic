687fc970bdab49d54b3a3ffc7cf797d88464ec69
GTRebbeca <rebbecagt@gmail.com>
2019-02-11 17:25:05 -0600

    test del kernel

--- app/Console/Commands/medicationExecute.php
+++ app/Console/Commands/medicationExecute.php
@@ -73,10 +73,10 @@ class medicationExecute extends Command
                             $countact = $countact + 1;
                     else {  
                            $countinac = $countinac + 1;
-                           if($countinac == 1){
+                          /* if($countinac == 1){
                                  $current = Carbon::now()->timezone('America/Mexico_City')->diffInSeconds($hour);
                                  $interval = 1;
-                             }
+                             } */
                            }
                 }
 
@@ -85,12 +85,12 @@ class medicationExecute extends Command
                 $Change->scheduller_inactive = $countinac;
                 $Change->interval_hour = $interval;
                 $Change->save();
-                             if($current != null){
+                           /*  if($current != null){
                                     Artisan::$fn('schedule:run');
                                     $this->info('completed, sleeping..');
                                     sleep($current);
                                     $this->runSchedulersleep($current, $med->id);
-                            } 
+                            } */
 
         }
     }
