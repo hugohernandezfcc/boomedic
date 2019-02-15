a10c704495698fde81b5412648b1217129201d4d
GTRebbeca <rebbecagt@gmail.com>
2019-02-15 14:06:54 -0600

    otra apariencia

--- resources/views/emails/cancelAppointment.blade.php
+++ resources/views/emails/cancelAppointment.blade.php
@@ -1,182 +1,66 @@
 <!DOCTYPE html>
 <html lang="es">
-<head>
-    <meta charset="utf-8">
-    <style type="text/css">
+	<head>
+	    <meta charset="utf-8">
+		    <style type="text/css">
 
-.container {
-  margin: 80px auto;
-  width: 400px;
-  text-align: center;
-}
+					.custom-select {
+					    -webkit-appearance: none;
+					    -moz-appearance: none;
+					    appearance: none;
+					    height: 45px;
+					    width: 300px;
+					    padding: 10px 38px 10px 16px;
+					    transition: border-color .1s ease-in-out,box-shadow .1s ease-in-out;
+					    border: 1px solid #ddd;
+					    border-radius: 25px;
+					}
+					.custom-select:hover {
+					    border: 1px solid #999;
+					}
+					.custom-select:focus {
+					    border: 1px solid #999;
+					    box-shadow: 0 3px 5px 0 rgba(0,0,0,.2);
+					    outline: none;
+					}
+					/* remove default arrow in IE */
+					select::-ms-expand {
+					    display:none;
+					}
+					.custom-select {
+					    font-family: 'Source Sans Pro', sans-serif;
+					    font-size: 16px;
+					}
+		    </style>
+	</head>
+	<body>
 
-.container > .dropdown {
-  margin: 0 20px;
-  vertical-align: top;
-}
-
-.dropdown {
-  display: inline-block;
-  position: relative;
-  overflow: hidden;
-  height: 45px;
-  width: 300px;
-  background: #f2f2f2;
-  border: 1px solid;
-  border-color: white #f7f7f7 whitesmoke;
-  border-radius: 3px;
-  background-image: -webkit-linear-gradient(top, transparent, rgba(0, 0, 0, 0.06));
-  background-image: -moz-linear-gradient(top, transparent, rgba(0, 0, 0, 0.06));
-  background-image: -o-linear-gradient(top, transparent, rgba(0, 0, 0, 0.06));
-  background-image: linear-gradient(to bottom, transparent, rgba(0, 0, 0, 0.06));
-  -webkit-box-shadow: 0 1px 1px rgba(0, 0, 0, 0.08);
-  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.08);
-}
-
-.dropdown:before, .dropdown:after {
-  content: '';
-  position: absolute;
-  z-index: 2;
-  top: 9px;
-  right: 10px;
-  width: 0;
-  height: 0;
-  border: 4px dashed;
-  border-color: #888888 transparent;
-  pointer-events: none;
-}
-
-.dropdown:before {
-  border-bottom-style: solid;
-  border-top: none;
-}
-
-.dropdown:after {
-  margin-top: 7px;
-  border-top-style: solid;
-  border-bottom: none;
-}
-
-.dropdown-select {
-  position: relative;
-  width: 130%;
-  margin: 0;
-  padding: 6px 8px 6px 10px;
-  height: 45px;
-  line-height: 14px;
-  font-size: 18px;
-  color: white !important;
-  text-shadow: 0 1px white;
-  background: #f2f2f2; /* Fallback for IE 8 */
-  background: rgba(0, 0, 0, 0) !important; /* "transparent" doesn't work with Opera */
-  border: 0;
-  border-radius: 0;
-  -webkit-appearance: none;
-}
-
-.dropdown-select:focus {
-  z-index: 3;
-  width: 100%;
-  color: #394349;
-  outline: 2px solid #49aff2;
-  outline: 2px solid -webkit-focus-ring-color;
-  outline-offset: -2px;
-}
-
-.dropdown-select > option {
-  margin: 3px;
-  padding: 6px 8px;
-  text-shadow: none;
-  background: #f2f2f2;
-  border-radius: 3px;
-  cursor: pointer;
-}
-
-/* Fix for IE 8 putting the arrows behind the select element. */
-
-.lt-ie9 .dropdown {
-  z-index: 1;
-}
-
-.lt-ie9 .dropdown-select {
-  z-index: -1;
-}
-
-.lt-ie9 .dropdown-select:focus {
-  z-index: 3;
-}
-
-/* Dirty fix for Firefox adding padding where it shouldn't. */
-
-@-moz-document url-prefix() {
-  .dropdown-select {
-    padding-left: 6px;
-  }
-}
-
-.dropdown-dark {
-  background: #444;
-  border-color: #111111 #0a0a0a black;
-  background-image: -webkit-linear-gradient(top, transparent, rgba(0, 0, 0, 0.4));
-  background-image: -moz-linear-gradient(top, transparent, rgba(0, 0, 0, 0.4));
-  background-image: -o-linear-gradient(top, transparent, rgba(0, 0, 0, 0.4));
-  background-image: linear-gradient(to bottom, transparent, rgba(0, 0, 0, 0.4));
-  -webkit-box-shadow: inset 0 1px rgba(255, 255, 255, 0.1), 0 1px 1px rgba(0, 0, 0, 0.2);
-  box-shadow: inset 0 1px rgba(255, 255, 255, 0.1), 0 1px 1px rgba(0, 0, 0, 0.2);
-}
-
-.dropdown-dark:before {
-  border-bottom-color: #aaa;
-}
-
-.dropdown-dark:after {
-  border-top-color: #aaa;
-}
-
-.dropdown-dark .dropdown-select {
-  color: #aaa;
-  text-shadow: 0 1px black;
-  background: #444;  /* Fallback for IE 8 */
-}
-
-.dropdown-dark .dropdown-select:focus {
-  color: #ccc;
-}
-
-.dropdown-dark .dropdown-select > option {
-  background: #444;
-  text-shadow: 0 1px rgba(0, 0, 0, 0.4);
-}
-    </style>
-</head>
-<body>
-
-    <h2>El dr. {!! $dr !!} te ha cancelado la cita...</h2>
-    <hr>
-       Motivo: {!! $reason !!} <br>
-       Definitivo: {!! $definitive !!}  <br>   
+	    <h2>El dr. {!! $dr !!} te ha cancelado la cita...</h2>
+	    <hr>
+	       Motivo: {!! $reason !!} <br>
+	       Definitivo: {!! $definitive !!}  <br>   
 
 
-<div class="dropdown dropdown-dark" align="center">
-    <select name="two" class="dropdown-select">
-    <optgroup label="Próximos días después de la cita">  	
-		       @foreach($array as $a)
-		       	   <option value="{!! $a !!}"> {!! $a !!} </option>
-		       @endforeach
-     </optgroup>
+	<div align="center">
+	    <select class="custom-select">
+	    <optgroup label="Próximos días después de la cita">  	
+			       @foreach($array as $a)
+			       	   <option value="{!! $a !!}"> {!! $a !!} </option>
+			       @endforeach
+	     </optgroup>
 
 
-    <optgroup label="Semana siguiente mismo horario">  
-		       @foreach($array2 as $a2) 
-		       	   <option value="{!! $a2 !!}"> {!! $a2 !!} </option>
-		       @endforeach
-	</optgroup>	       
-    <optgroup label="El resto del día de la cita">  
-		       @foreach($array3 as $a3)
-		       		<option value="{!! $a3 !!}">{!! $a3 !!} </option>
-		       @endforeach
-	</optgroup>	       
-	</select>
-</div>	       
-</body>
+	    <optgroup label="Semana siguiente mismo horario">  
+			       @foreach($array2 as $a2) 
+			       	   <option value="{!! $a2 !!}"> {!! $a2 !!} </option>
+			       @endforeach
+		</optgroup>	       
+	    <optgroup label="El resto del día de la cita">  
+			       @foreach($array3 as $a3)
+			       		<option value="{!! $a3 !!}">{!! $a3 !!} </option>
+			       @endforeach
+		</optgroup>	       
+		</select>
+	</div>	       
+	</body>
 </html>
