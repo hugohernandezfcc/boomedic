f60cd7a40d3990e79bd5d5d39f28c86ba1456efd
GTRebbeca <rebbecagt@gmail.com>
2019-02-20 15:58:57 -0600

    cambios

--- app/Http/Controllers/VisaAPIClient.php
+++ app/Http/Controllers/VisaAPIClient.php
@@ -1,11 +1,9 @@
 <?php 
 namespace App\Http\Controllers;
-
 //Class where the CURL connection is made with VISA
 class VisaAPIClient extends Controller {
 	
 	public function __construct() {
-
 		$this->timeout = 80;
 		$this->connectTimeout = 30;
 	}
