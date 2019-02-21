c8081868d91ccb0477e0f2b59d03a54dc9bbeba6
GTRebbeca <rebbecagt@gmail.com>
2019-02-21 14:00:21 -0600

    cambios payment

--- app/Http/Controllers/VisaAPIClient.php
+++ app/Http/Controllers/VisaAPIClient.php
@@ -161,9 +161,9 @@ class VisaAPIClient extends Controller {
 			if (empty($body) == false && $body != '') {
 				$json = json_decode($body);
 				print_r($body);
-				//$json = json_encode($json->responseStatus->details[0]->message, JSON_PRETTY_PRINT);
+				$json = json_encode($json->responseStatus->details[0]->message, JSON_PRETTY_PRINT);
 				//The quotation marks are removed so that the code is clean and can be found in the trans.
-				//$resp = str_replace('"','', $json);
+				$resp = str_replace('"','', $json);
 				return $body;
 			}
 		
