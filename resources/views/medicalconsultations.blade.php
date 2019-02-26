c2bacf3250660f25f8d01440e2c92120a0074c71
GTRebbeca <rebbecagt@gmail.com>
2019-02-26 14:52:30 -0600

    cambios

--- resources/views/medicalconsultations.blade.php
+++ resources/views/medicalconsultations.blade.php
@@ -1293,21 +1293,23 @@ function prevTab(elem) {
             if('{{ session()->get("latitude") }}'.length > 0){
 
             var pos = {
-                        lat: '{{ session()->get("latitude") }}',
-                        lng: '{{ session()->get("longitude") }}'
+                        lat: "{{ session()->get('latitude') }}",
+                        lng: "{{ session()->get('longitude') }}"
                       };
             alert(pos);
+            var center = pos;
             }else{
              var pos = {
                 lat: position.coords.latitude,
                 lng: position.coords.longitude
               };
+              var center = new google.maps.LatLng(pos);
            }
 
             map = new google.maps.Map(document.getElementById('map'), {
               zoom: 14,
               gestureHandling: 'greedy',
-              center: new google.maps.LatLng(pos),
+              center: center,
               styles: [
                         {
                           "elementType": "labels",
