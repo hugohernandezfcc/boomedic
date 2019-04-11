f2ea734e5800c5fa1c2573cfcb9d3c15d496cd0d
GTRebbeca <rebbecagt@gmail.com>
2019-04-11 18:39:19 -0500

    colocandoo lapicito para mobile en edición de foto de perfil

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -42,15 +42,17 @@
   -ms-transform: rotate(-90deg);
   transform: rotate(-90deg);
 }
-			.dropzone {
+			.dropzone  {
 			     min-height: 10px !important; 
 			    border-style: dotted  !important;
 			    /* background: white; */
-			     padding: 0 !important;
+			    padding: 5px !important;
 			}
-			.dropzone .dz-message {
+			.dropzone .gen .dz-message {
 			    margin: 1em 0 !important;
 			}
+
+
 			.modal-content-2 {
 			    position: relative;
 			    background-color: transparent;
@@ -65,11 +67,39 @@
         width: 95%;
         z-index: 30;
       }
-.cut{
-  text-overflow:ellipsis;
-  white-space:nowrap; 
-  overflow:hidden; 
-}
+		.cut{
+		  text-overflow:ellipsis;
+		  white-space:nowrap; 
+		  overflow:hidden; 
+		}
+		.container {
+		  position: relative;
+		  width: 90%;
+		}
+
+		/* Make the image responsive */
+		.container img {
+		  width: 80%;
+		  height: auto;
+		}
+
+		/* Style the button and place it in the middle of the container/image */
+		.container .btn {
+		  position: absolute;
+		  top: -5%;
+		  left: 78%;
+		}
+		.btn-circle {
+		  width: 40px;
+		  height: 40px;
+		  text-align: center;
+		  padding: 6px 0;
+		  font-size: 16px;
+		  line-height: 1.428571429;
+		  border-radius: 19px;
+		}
+
+
     </style>
     <meta name="csrf-token" content="{{ csrf_token() }}">
 @stop
@@ -89,6 +119,26 @@
 			    dictRemoveFile: "Eliminar",
 			    dictCancelUpload: "Cancel",
 			    dictDefaultMessage: "Arraste y suelte una nueva foto de perfil...",
+			     success: function(file, response){
+				        //alert(response);
+				 $('#loadingmodal').modal({backdrop: 'static', keyboard: false})
+				  setTimeout(function(){ 
+				  	$('#loadingmodal').modal('toggle');
+				  	window.location.reload(true);
+				  },21000);
+				     	}
+			    //autoProcessQueue : false 
+			 };
+			Dropzone.options.myDropzone = { 
+			 
+			 // set following configuration
+			 	paramName: "file",
+			    maxFiles: 1,
+			    acceptedFiles: "image/*",
+			    addRemoveLinks: true,
+			    dictRemoveFile: "Eliminar",
+			    dictCancelUpload: "Cancel",
+			    dictDefaultMessage: "",
 			     success: function(file, response){
 				        //alert(response);
 				 $('#loadingmodal').modal({backdrop: 'static', keyboard: false})
@@ -157,7 +207,57 @@
 		                <p>Confirma y completa la información que esta debajo</p>
 		            </div>
 	    		@endif
-          <label class="col-sm-2 control-label" style="text-align: right;">Foto de perfil</label>
+          <label class="col-sm-2 control-label" style="text-align: right;">Foto de perfil</label><br>
+
+          @if($agent->isMobile())
+	    		<div class="row" align="center">
+	    		
+	    		<div class="col-sm-3" align="center">
+	    			@if($photo == '')
+			    	 	@if($gender == 'male') 
+			    	 		<div class="container"><img class="profile-user-img img-responsive img-circle" src="{{ asset('profile-42914_640.png') }}" alt="User Image"  style="width:150px; height: 150px;"><form enctype="multipart/form-data" action="/user/updateProfile/{{$userId}}"  method="post" class="dropzone btn btn-default btn-circle" id="myDropzone" >{{ csrf_field() }}<span class="fa fa-pencil" ></span></form>
+			    	 		</div>
+			    	 	@endif
+			    	 	@if($gender == 'female') 
+			    	 		<div class="container"><img class="profile-user-img img-responsive img-circle" src="{{ asset('profile-female.png') }}" alt="User Image"  style="width:150px; height: 150px;"><form enctype="multipart/form-data" action="/user/updateProfile/{{$userId}}"  method="post" class="dropzone btn btn-default btn-circle" id="myDropzone" >{{ csrf_field() }}<span class="fa fa-pencil" ></span></form>
+			    	 		</div>
+			    	 	@endif
+			    	 	 @if($gender == 'other' || $gender == '') 
+			    	 		<div class="container"><img class="profile-user-img img-responsive img-circle" src="{{ asset('profile-other.png') }}" alt="User Image"  style="width:150px; height: 150px;"><form enctype="multipart/form-data" action="/user/updateProfile/{{$userId}}"  method="post" class="dropzone btn btn-default btn-circle" id="myDropzone" >{{ csrf_field() }}<span class="fa fa-pencil" ></span></form>
+			    	 		</div>
+		    	 	@endif
+					@else
+					@php 
+					  $imagen = getimagesize($photo);    //Sacamos la información
+			          $width = $imagen[0];              //Ancho
+			          $height = $imagen[1];  
+			          if($height > '500' || $width > '500'){
+			            $height = $height / 2.8;
+			            $width = $width / 2.8;
+			        }
+			        if($height > '800' || $width > '800'){
+			            $height = $height / 4;
+			            $width = $width / 4;
+			        }
+			      if($height > '800' || $width > '1200'){
+			            $height = $height / 6;
+			            $width = $width / 6;
+			        }
+			          if($height < '400' || $width < '400'){
+			            $height = $height / 1.6;
+			            $width = $width / 1.6;
+			        }
+					@endphp
+					
+						<div class="container"><img src="{{ $photo }}?{{ \Carbon\Carbon::now()->format('h:i') }}" style="width:{{ $width }}px; height: {{ $height }}px;" ><form enctype="multipart/form-data" action="/user/updateProfile/{{$userId}}"  method="post" class="dropzone btn btn-default btn-circle" id="myDropzone" >{{ csrf_field() }}<span class="fa fa-pencil" ></span></form>
+
+			    	 	</div>
+			    			
+			    	@endif 
+	    			
+	    		</div>
+	    	</div>
+          @else
 	    		<div class="row" align="center">
 	    		
 	    		<div class="col-sm-3" align="center">
@@ -197,8 +297,10 @@
 			    	@endif 
 	    			
 	    		</div>
-	    		<div class="col-sm-2" align="center" style="width: 240px;"><form enctype="multipart/form-data" action="/user/updateProfile/{{$userId}}" method="post" class="dropzone" id="myAwesomeDropzone">{{ csrf_field() }}</form></div>
-	    	</div><br/>
+	    		<div class="col-sm-2" align="center" style="width: 240px;"><form enctype="multipart/form-data" action="/user/updateProfile/{{$userId}}" method="post" class="dropzone gen" id="myAwesomeDropzone">{{ csrf_field() }}</form></div>
+	    	</div>
+	    	@endif 
+	    	<br/>
 	    		<form enctype="multipart/form-data" action="/user/update/{{$userId}}" method="post" class="form-horizontal">
 	    			{{ csrf_field() }}
 
