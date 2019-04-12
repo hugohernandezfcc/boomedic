64ab9a10d9f6ced8d0854456dd22cedf9a28a64e
GTRebbeca <rebbecagt@gmail.com>
2019-04-12 13:04:23 -0500

    correción show div

--- resources/views/profileDoctor.blade.php
+++ resources/views/profileDoctor.blade.php
@@ -1223,6 +1223,7 @@
 				                                  	$(".alert-success").fadeTo(2000, 500).slideUp(500, function(){
 														    $(".alert-success").slideUp(500);
 														});
+				                                  	  $('#searchQuestions div').show();
 				                                  }
 				                                else{
 				                                	$(".alert-warning").show();
