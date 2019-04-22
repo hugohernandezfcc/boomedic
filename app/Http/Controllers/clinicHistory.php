9e8a73aec300d8514c8df7080b457744571b4704
GTRebbeca <rebbecagt@gmail.com>
2019-04-22 11:42:28 -0500

    subiendo cambios en el lugar del replace

--- app/Http/Controllers/clinicHistory.php
+++ app/Http/Controllers/clinicHistory.php
@@ -333,9 +333,9 @@ class clinicHistory extends Controller
         ->where('userid', $user->id)->where('question_id', $request->question)->first();
         $newArray = array();
         $answers = json_decode($request->answers);
-        $answers = str_replace("_", " ", $answers);
+        $answersguion = str_replace("_", " ", $answers);
 
-        
+
             if (in_array("Fallecido", $answers) && in_array("Vivo", $answers)) {
                 $z = 1;
                for($i = 0; $i < count($answers); $i++){
@@ -354,7 +354,7 @@ class clinicHistory extends Controller
                 $clinic = clinic_history::find($history->id);
                 $clinic->answer_id = $request->ansId;
                 if(count($newArray) == 0){
-                $clinic->answer = $request->answers;
+                $clinic->answer = json_encode($answersguion);
             }else {
                 $clinic->answer = json_encode($newArray);
             }
@@ -366,7 +366,7 @@ class clinicHistory extends Controller
             $clinic->question_id =  $request->question;
             $clinic->question = $q->question;
             if(count($newArray) == 0){
-            $clinic->answer = $request->answers;
+            $clinic->answer = json_encode($answersguion);
               }else {
                 $clinic->answer = json_encode($newArray);
             }
