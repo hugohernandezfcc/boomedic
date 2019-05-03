12f23adc3ca6daaf857c53e6cec369e4122274e7
GTRebbeca <rebbecagt@gmail.com>
2019-05-03 12:20:46 -0500

    Middleware validate

--- app/Http/Controllers/AssistantController.php
+++ app/Http/Controllers/AssistantController.php
@@ -15,6 +15,17 @@ use Mailgun\Mailgun;
 
 class AssistantController extends Controller
 {
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct(){
+        $this->middleware('auth');
+    }
+
+
     /**
      * Display a listing of the resource.
      *
