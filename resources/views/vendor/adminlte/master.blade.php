fdd61d0444be2238d51328aba81a6b4597d89595
GTRebbeca <rebbecagt@gmail.com>
2019-05-03 11:03:34 -0500

    corriendo desde el master el script

--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -819,7 +819,7 @@ $(document).ready(function() {
                 console.log('socket');
             });*/
 
-            var par = "@php echo session()->get('parental'); @endphp";
+     var par = "@php echo session()->get('parental'); @endphp";
       if(!par){
           $("body").removeClass("skin-black-light");
           $("body").addClass("skin-black");
@@ -874,16 +874,6 @@ $(document).ready(function() {
           };
 
 
-                    //Date picker
-       /* $('#datepicker').datepicker({
-            format: "mm/dd/yyyy",
-            language: "es",
-            autoclose: true
-        });
-        $('#datepicker').datepicker().on('show', function(e) {
-            $('div.datepicker').removeClass( "datepicker-dropdown" );
-        });*/
-        //$('#mobile').inputmask({"mask": "(999) 999-9999"});
         $('form').on('keyup keypress', function(e) {
             var keyCode = e.keyCode || e.which;
             if (keyCode === 13) { 
@@ -955,6 +945,121 @@ $(document).ready(function() {
         
     });
 </script>
+ <script type="text/javascript">
+
+                       function  check(a){
+                        
+                            var id = a.attr('id'); 
+
+                            if($(a).prop('checked'))
+                               $('#'+id+'w').removeAttr('disabled');
+
+                            else{
+                                 $('#'+id+'w').attr('disabled','disabled');
+                                 $('#'+id+'w').prop( "checked", false );
+                            }
+                        }
+
+                        function attribute(id, atr){
+                            var result;
+                             if($('#'+id+atr).prop('checked')){
+                                  if($('#'+id+atr+'w').prop('checked'))
+                                       result = "write";
+                                  else 
+                                       result = "read";   
+                              }
+                              else
+                                  result = "none";
+
+                                  return result;
+                          }
+
+                        function saveSettings(id){
+                          var profile = attribute(id, "profile");
+                          var calendar = attribute(id, "calendar");
+                          var workboard = attribute(id, "workboard");
+                          var chat = attribute(id, "chat");
+                          var assistant = attribute(id, "assistant");
+                          $.ajaxSetup({
+                                      headers: {
+                                          'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                                      }
+                                  });
+
+                          $.ajax({     
+                                         type: "POST",                 
+                                         url: "{{ url('AssistantController/save') }}/" + id,  
+                                         data: { "profile" : profile,
+                                                  "calendar" : calendar,
+                                                  "workboard" : workboard,
+                                                  "chat" : chat,
+                                                  "assistant" : assistant
+                                                }, 
+                                         dataType: 'json',                
+                                         success: function(data)             
+                                         {  
+                                            if(data != 'Error')
+                                                 $('.notifications').notify({
+                                                    message: { text: "Se guardaron los cambios correctamente para el asistente " + data + ". " }
+                                                  }).show();
+                                            else 
+                                                 $('.notifications').notify({
+                                                    message: { text: "Ha ocurrido un error, vuelve a intentarlo o levanta un caso si el error persiste." },
+                                                    type:'danger'
+                                                  }).show();
+                                          }
+                                        
+                                     });
+                        }
+
+                        $.ajax({
+                             type: "GET",                 
+                             url: "{{ url('doctor/settingAss') }}",  
+                             success: function(result){ 
+                                  if(result.length > 0){
+                                      $('#accordion2').html('');
+                                      for(var z= 0; z < result.length; z++){
+
+                                        //Profile options                                                                
+                                        var checkRead = (result[z]["profile"] === "read" || result[z]["profile"] === "write") ? 'checked' : '';
+                                        var checkWrite = (result[z]["profile"] === "write") ? 'checked' : 'disabled="disabled"';
+
+                                        var profile = '<tr><td>Perfil</td><td><input type="checkbox" id="'+ result[z]["idass"] +'profile" onclick="check($(this));" '+ checkRead +'></td><td><input type="checkbox" id="'+ result[z]["idass"] +'profilew" '+ checkWrite +'></td></tr>';
+
+                                        //Calendar options  
+                                        checkRead = (result[z]["calendar"] === "read" || result[z]["calendar"] === "write") ? 'checked' : '';
+                                        checkWrite = (result[z]["calendar"] === "write") ? 'checked' : 'disabled="disabled"';
+
+                                        var calendar = '<tr><td>Agenda</td><td><input type="checkbox" onclick="check($(this));" id="'+ result[z]["idass"] +'calendar" '+ checkRead +'></td><td><input type="checkbox" id="'+ result[z]["idass"] +'calendarw" '+ checkWrite +'></td></tr>';
+
+                                        //Workboard options  
+                                        checkRead = (result[z]["workboard"] === "read" || result[z]["workboard"] === "write") ? 'checked' : '';
+                                        checkWrite = (result[z]["workboard"] === "write") ? 'checked' : 'disabled="disabled"';
+
+                                        var workboard = '<tr><td>Horarios</td><td><input type="checkbox" id="'+ result[z]["idass"] +'workboard" onclick="check($(this));" '+ checkRead +'></td><td><input type="checkbox" id="'+ result[z]["idass"] +'workboardw" '+ checkWrite +'></td></tr>';
+
+                                        //Chat options  
+                                        checkRead = (result[z]["chat"] === "read" || result[z]["chat"] === "write") ? 'checked' : '';
+                                        checkWrite = (result[z]["chat"] === "write") ? 'checked' : 'disabled="disabled"';
+
+                                        var chat = '<tr><td>Chat</td><td><input type="checkbox" id="'+ result[z]["idass"] +'chat" onclick="check($(this));" '+ checkRead +'></td><td><input type="checkbox" id="'+ result[z]["idass"] +'chatw" '+ checkWrite +'></td></tr>';
+
+                                        //Chat options  
+                                        checkRead = (result[z]["assistant"] === "read" || result[z]["assistant"] === "write") ? 'checked' : '';
+                                        checkWrite = (result[z]["assistant"] === "write") ? 'checked' : 'disabled="disabled"';
+
+                                        var assistant = '<tr><td>Asistentes</td><td><input type="checkbox" id="'+ result[z]["idass"] +'assistant" onclick="check($(this)); '+ checkRead +'"></td><td><input type="checkbox" id="'+ result[z]["idass"] +'assistantw" '+ checkWrite +'></td></tr>';                                                              
+
+                                      $('#accordion2').append('<div class="panel box tit"><a data-toggle="collapse" data-parent="#accordion2" href="#'+ result[z]['idass'] +'"><div class="box-header with-border"><h5 class="box-title tit">'+ result[z]['name'] +'</h5></div></a><div id="'+ result[z]['idass'] +'" class="panel-collapse collapse"><div class="box-body"><div class="table-responsive"><table class="table table-condensed"><thead><tr><th scope="col">Permisos</th><th scope="col">Ver</th><th scope="col">Editar</th></tr></thead><tbody>'+ profile + calendar + workboard + chat + assistant +'<tr><td colspan="3"><button class="btn btn-xs btn-secondary pull-right" onclick="saveSettings('+ result[z]["idass"] +');">Guardar</button></td></tr></tbody></table></div></div></div></div>');
+                                    
+                                    }
+                                  }else{
+                                    $('#accordion2').html('');
+                                  }
+                              }
+                          });
+
+                </script>
 
 
 @if(config('adminlte.plugins.datatables'))
