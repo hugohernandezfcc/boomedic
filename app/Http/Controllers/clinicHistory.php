876b962dcbee9045a7a2a72d42a130f8796c6f65
Hugo Hernández <hugo@doitcloud.consulting>
2019-05-07 01:05:49 -0500

    deleting cpanel messages in clinic history

--- app/Http/Controllers/clinicHistory.php
+++ app/Http/Controllers/clinicHistory.php
@@ -129,13 +129,11 @@ class clinicHistory extends Controller
                                                                             ['diagnostic_test','=',null]
                                                                         ])->get();
 
-    // for ($i=0; $i < count($result); $i++)  {
-    //     if(strpos($result[$i]->subject_email, 'Email configuration settings') === true){
-    //         dd($result[$i]);
-    //         unset($result[$i]);
-    //     }
-    // }
-    dd($result);
+    for ($i=0; $i < count($result); $i++)  
+        if($result[$i]->email == 'cpanel@iscoapp.com')
+            unset($result[$i]);
+        
+    
                            $result2 = $result->groupBy('date_email'); 
            
         if(count($clinic_history_general) == 0){
