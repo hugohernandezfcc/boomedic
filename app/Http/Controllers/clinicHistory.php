7e34563a63c6e80eb52354368648eb3e4c9a6bc8
Hugo Hernández <hugo@doitcloud.consulting>
2019-05-07 00:57:35 -0500

    s

--- app/Http/Controllers/clinicHistory.php
+++ app/Http/Controllers/clinicHistory.php
@@ -129,18 +129,13 @@ class clinicHistory extends Controller
                                                                             ['diagnostic_test','=',null]
                                                                         ])->get();
 
-    for ($i=0; $i < count($result); $i++)  
+    for ($i=0; $i < count($result); $i++)  {
         if(strpos($result[$i]->subject_email, 'Asunto: [iscoapp.com] Email configuration settings') === true){
+            dd($result[$i]);
             unset($result[$i]);
         }
-        
-    
-
-    dd($result);
+    }
                            $result2 = $result->groupBy('date_email'); 
-
-    //  
-
            
         if(count($clinic_history_general) == 0){
             $mode = "null";
