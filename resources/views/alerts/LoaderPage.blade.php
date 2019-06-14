845a27ac513037c1ca817a279183b44533445d8d
Hugo Hernández <hugo@doitcloud.consulting>
2019-06-14 17:55:46 -0500

    s

--- resources/views/alerts/LoaderPage.blade.php
+++ resources/views/alerts/LoaderPage.blade.php
@@ -1,9 +1,64 @@
-<div id="loadingmodal" class="modal fade" role="dialog" style="background: rgba(0, 0, 0, 0.8);">
-    <div class="modal-dialog">
-        <div class="modal-content-2">
+<div id="loadingmodal" class="modal fade" role="dialog" style="opacity: 0.5;">
+      <div class="modal-dialog">
+          <div class="modal-content-2">
             <div align="center">
-                <h1><i class="fa fa-refresh fa-spin"></i><br/>Cargando...</h1>
-            </div>
-        </div>
-    </div>
-</div>
+              <!-- <h1><i class="fa fa-refresh fa-spin"></i><br/>Cargando...</h1> -->
+
+                <svg width="135" height="140" viewBox="0 0 135 140" xmlns="http://www.w3.org/2000/svg" fill="#222">
+                <rect y="10" width="15" height="120" rx="6">
+                    <animate attributeName="height"
+                         begin="0.5s" dur="1s"
+                         values="120;110;100;90;80;70;60;50;40;140;120" calcMode="linear"
+                         repeatCount="indefinite" />
+                    <animate attributeName="y"
+                         begin="0.5s" dur="1s"
+                         values="10;15;20;25;30;35;40;45;50;0;10" calcMode="linear"
+                         repeatCount="indefinite" />
+                </rect>
+                <rect x="30" y="10" width="15" height="120" rx="6">
+                    <animate attributeName="height"
+                         begin="0.25s" dur="1s"
+                         values="120;110;100;90;80;70;60;50;40;140;120" calcMode="linear"
+                         repeatCount="indefinite" />
+                    <animate attributeName="y"
+                         begin="0.25s" dur="1s"
+                         values="10;15;20;25;30;35;40;45;50;0;10" calcMode="linear"
+                         repeatCount="indefinite" />
+                </rect>
+                <rect x="60" width="15" height="140" rx="6">
+                    <animate attributeName="height"
+                         begin="0s" dur="1s"
+                         values="120;110;100;90;80;70;60;50;40;140;120" calcMode="linear"
+                         repeatCount="indefinite" />
+                    <animate attributeName="y"
+                         begin="0s" dur="1s"
+                         values="10;15;20;25;30;35;40;45;50;0;10" calcMode="linear"
+                         repeatCount="indefinite" />
+                </rect>
+                <rect x="90" y="10" width="15" height="120" rx="6">
+                    <animate attributeName="height"
+                         begin="0.25s" dur="1s"
+                         values="120;110;100;90;80;70;60;50;40;140;120" calcMode="linear"
+                         repeatCount="indefinite" />
+                    <animate attributeName="y"
+                         begin="0.25s" dur="1s"
+                         values="10;15;20;25;30;35;40;45;50;0;10" calcMode="linear"
+                         repeatCount="indefinite" />
+                </rect>
+                <rect x="120" y="10" width="15" height="120" rx="6">
+                    <animate attributeName="height"
+                         begin="0.5s" dur="1s"
+                         values="120;110;100;90;80;70;60;50;40;140;120" calcMode="linear"
+                         repeatCount="indefinite" />
+                    <animate attributeName="y"
+                         begin="0.5s" dur="1s"
+                         values="10;15;20;25;30;35;40;45;50;0;10" calcMode="linear"
+                         repeatCount="indefinite" />
+                </rect>
+            </svg>
+
+
+              </div>
+          </div>
+      </div>
+  </div>
