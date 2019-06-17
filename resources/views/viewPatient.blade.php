90dbe0a28f4f6ec5064e97ba03a75f2b6d7faa78
Hugo Hernández <hugo@doitcloud.consulting>
2019-06-17 00:09:54 -0500

    a

--- resources/views/viewPatient.blade.php
+++ resources/views/viewPatient.blade.php
@@ -84,163 +84,161 @@
     <section class="content">
 
       	<div class="row">
-        <div class="col-md-3">
+	        <div class="col-md-3">
 
-          <!-- Profile Image -->
-          <div class="box box-default">
-            <div class="box-body box-profile">
+	          <!-- Profile Image -->
+	          <div class="box box-default">
+	            <div class="box-body box-profile">
 
-            	@if($pphoto == '')
-		    	 		<img class="profile-user-img img-responsive img-circle" src="{{ asset('profile-42914_640.png') }}" alt="User Image"  style="width:150px; height: 150px;">
-					@else
-						@php 
-						  $imagen = getimagesize($pphoto);    //Sacamos la información
-				          $width = $imagen[0];              //Ancho
-				          $height = $imagen[1];  
+	            	@if($pphoto == '')
+			    	 		<img class="profile-user-img img-responsive img-circle" src="{{ asset('profile-42914_640.png') }}" alt="User Image"  style="width:150px; height: 150px;">
+						@else
+							@php 
+							  $imagen = getimagesize($pphoto);    //Sacamos la información
+					          $width = $imagen[0];              //Ancho
+					          $height = $imagen[1];  
 
-				          if($height > '500' || $width > '500'){
-				            $height = $height / 2.8;
-				            $width = $width / 2.8;
-				        }
-				        if($height > '800' || $width > '800'){
-				            $height = $height / 4;
-				            $width = $width / 4;
-				        }
-				      if($height > '800' || $width > '1200'){
-				            $height = $height / 6;
-				            $width = $width / 6;
-				        }
+					          if($height > '500' || $width > '500'){
+					            $height = $height / 2.8;
+					            $width = $width / 2.8;
+					        }
+					        if($height > '800' || $width > '800'){
+					            $height = $height / 4;
+					            $width = $width / 4;
+					        }
+					      if($height > '800' || $width > '1200'){
+					            $height = $height / 6;
+					            $width = $width / 6;
+					        }
 
 
-				          if($height < '400' || $width < '400'){
-				            $height = $height / 1.6;
-				            $width = $width / 1.6;
-				        }
+					          if($height < '400' || $width < '400'){
+					            $height = $height / 1.6;
+					            $width = $width / 1.6;
+					        }
 
-						@endphp
-						<img class="profile-user-img img-responsive img-circle" src="{{ $pphoto }}?{{ \Carbon\Carbon::now()->format('h:i') }}" style="width:{{ $width }}px; height: {{ $height }}px;" >			
-			    	@endif 
+							@endphp
+							<img class="profile-user-img img-responsive img-circle" src="{{ $pphoto }}?{{ \Carbon\Carbon::now()->format('h:i') }}" style="width:{{ $width }}px; height: {{ $height }}px;" >			
+				    	@endif 
 
-              	
+	              	
 
-              	<h3 class="profile-username text-center">{{ $pfirstname }}</h3>
+	              	<h3 class="profile-username text-center">{{ $pfirstname }}</h3>
 
-              	@if($gender == "female")
-              		<p class="text-muted text-center">{{ trans('adminlte::adminlte.female') }}</p>
-	            @endif
-	            @if($gender == "male")
-	            	<p class="text-muted text-center">{{ trans('adminlte::adminlte.male') }}</p>
-	            @endif
-	            @if($gender == "other")
-	            	<p class="text-muted text-center">{{ trans('adminlte::adminlte.other') }}</p>
-	            @endif
+	              	@if($gender == "female")
+	              		<p class="text-muted text-center">{{ trans('adminlte::adminlte.female') }}</p>
+		            @endif
+		            @if($gender == "male")
+		            	<p class="text-muted text-center">{{ trans('adminlte::adminlte.male') }}</p>
+		            @endif
+		            @if($gender == "other")
+		            	<p class="text-muted text-center">{{ trans('adminlte::adminlte.other') }}</p>
+		            @endif
 
-              
+	              
 
-	            <ul class="list-group list-group-unbordered">
-	                <li class="list-group-item">
-	                 	<b>Familiares</b> <a class="pull-right">{{ $countfamily }}</a>
-	                </li>
-	                <li class="list-group-item">
-	                  	<b>No. de citas</b> <a class="pull-right">{{ $countappo }}</a>
-	                </li>
-	                <li class="list-group-item">
-	                	<!--
-						 	##HDHM#### tengo que validar que cuando el médico no tenga preguntas agregadas se deshabilite este botón
-						-->
-	                	@foreach($questions as $quest)
-                            @if($loop->iteration == 1)
-                                <a data-target="#modalhistoryappointments-{{ $patientId}}" data-toggle="modal" class="btn btn-secondary btn-flat btn-block">Historia clínica previa cita</a>
-                            @endif    
-                        @endforeach
-	                </li>
-	            </ul>
+		            <ul class="list-group list-group-unbordered">
+		                <li class="list-group-item">
+		                 	<b>Familiares</b> <a class="pull-right">{{ $countfamily }}</a>
+		                </li>
+		                <li class="list-group-item">
+		                  	<b>No. de citas</b> <a class="pull-right">{{ $countappo }}</a>
+		                </li>
+		                <li class="list-group-item">
+		                	<!--
+							 	##HDHM#### tengo que validar que cuando el médico no tenga preguntas agregadas se deshabilite este botón
+							-->
+		                	@foreach($questions as $quest)
+	                            @if($loop->iteration == 1)
+	                                <a data-target="#modalhistoryappointments-{{ $patientId}}" data-toggle="modal" class="btn btn-secondary btn-flat btn-block">Historia clínica previa cita</a>
+	                            @endif    
+	                        @endforeach
+		                </li>
+		            </ul>
 
-            </div>
-            <!-- /.box-body -->
-          </div>
-      		@include('modals.clinichistoryappointments', 
-                    [
-                      	'id' => $patientId,
-                      	'dr' => Auth::id(), 
-                      	'questions'  => $questions_appointments,
-                      	'clinic_historyappo'  => $clinic_history_appointments
-                    ]
-                  )
-          <!-- /.box -->
+	            </div>
+	            <!-- /.box-body -->
+	          </div>
+	      		@include('modals.clinichistoryappointments', 
+	                    [
+	                      	'id' => $patientId,
+	                      	'dr' => Auth::id(), 
+	                      	'questions'  => $questions_appointments,
+	                      	'clinic_historyappo'  => $clinic_history_appointments
+	                    ]
+	                  )
+	          <!-- /.box -->
 
-          <!-- About Me Box -->
-          <div class="box box-default">
-            <div class="box-header with-border">
-              <h3 class="box-title">Información adicional</h3>
-            </div>
-            <!-- /.box-header -->
-            <div class="box-body">
-              <strong><i class="fa fa-book margin-r-5"></i> Educación</strong>
+	          <!-- About Me Box -->
+	          <div class="box box-default">
+	            <div class="box-header with-border">
+	              <h3 class="box-title">Información adicional</h3>
+	            </div>
+	            <!-- /.box-header -->
+	            <div class="box-body">
+	              <strong><i class="fa fa-book margin-r-5"></i> Educación</strong>
 
-              <p class="text-muted">
-              	@if(empty($scholarship) && empty($occupation))
-                	<a href="#">Agregar información</a>
-                <!-- @elseif(empty($scholarship) && !empty($occupation))
-                	<a href="#">Agregar Escolaridad</a> / {{ $occupation }}
-                @elseif(!empty($scholarship) && empty($occupation))
-                	{{ $scholarship }} / <a href="#">Agregar ocupación</a> -->
-                @endif
-              </p>
-
-              <hr>
-
-              <strong><i class="fa fa-map-marker margin-r-5"></i> Dirección</strong>
- 
-              	<p class="text-muted">
-	              	@if(empty($colony) && empty($state))
-	                	<a href="#">Agregar dirección</a>
-	                @elseif(empty($colony) && !empty($state))
-	                	<a href="#">Agregar colonia</a>, {{$state}} {{$country}}
-	                @elseif(!empty($colony) && empty($state))
-	                	{{ $colony }}, <a href="#">Agregar estado</a> {{$country}}
+	              <p class="text-muted">
+	              	@if(empty($scholarship) && empty($occupation))
+	                	<a href="#">Agregar información</a>
+	                <!-- @elseif(empty($scholarship) && !empty($occupation))
+	                	<a href="#">Agregar Escolaridad</a> / {{ $occupation }}
+	                @elseif(!empty($scholarship) && empty($occupation))
+	                	{{ $scholarship }} / <a href="#">Agregar ocupación</a> -->
 	                @endif
-				</p>
+	              </p>
 
-              <hr>
+	              <hr>
 
-              <strong><i class="fa fa-pencil margin-r-5"></i> Prescripción médica actual</strong>
+	              <strong><i class="fa fa-map-marker margin-r-5"></i> Dirección</strong>
+	 
+	              	<p class="text-muted">
+		              	@if(empty($colony) && empty($state))
+		                	<a href="#">Agregar dirección</a>
+		                @elseif(empty($colony) && !empty($state))
+		                	<a href="#">Agregar colonia</a>, {{$state}} {{$country}}
+		                @elseif(!empty($colony) && empty($state))
+		                	{{ $colony }}, <a href="#">Agregar estado</a> {{$country}}
+		                @endif
+					</p>
 
-              <p>
-                <span class="label label-info">{{$current_prescription}}</span>
-              </p>
+	              <hr>
 
-              <hr>
+	              <strong><i class="fa fa-pencil margin-r-5"></i> Prescripción médica actual</strong>
 
-              <strong><i class="fa fa-file-text-o margin-r-5"></i> Miembro desde</strong>
-              <p>{{$created_at}}</p>
-            </div>
-            <!-- /.box-body -->
-          </div>
-          <!-- /.box -->
+	              <p>
+	                <span class="label label-info">{{$current_prescription}}</span>
+	              </p>
+
+	              <hr>
+
+	              <strong><i class="fa fa-file-text-o margin-r-5"></i> Miembro desde</strong>
+	              <p>{{$created_at}}</p>
+	            </div>
+	            <!-- /.box-body -->
+	          </div>
+	          <!-- /.box -->
+	        </div>
+	        <!-- /.col -->
+	       	<div class="col-md-9">
+
+				@include('tabs.tabsviewpatient', [
+
+	    			'latitude' 		=>  $latitude,
+	    			'longitude'		=>  $longitude,
+	    			'nodes' 		=>  $nodes,
+	    			'pusername'		=>	$pusername,
+					'agep'			=>	$agep,
+					'occupation'	=>	$occupation,
+					'gender'		=>	$gender,
+					'maritalstatus'	=>	$maritalstatus,
+					'scholarship'	=>	$scholarship,
+					'updated_at'	=>	$updated_at,
+	    			]
+	    		)
+			</div>
         </div>
-        <!-- /.col -->
-        	<div class="col-md-9">
 
-        			@include('tabs.tabsvertical', [
-
-	        			'latitude' 		=>  $latitude,
-	        			'longitude'		=>  $longitude,
-	        			'nodes' 		=>  $nodes,
-	        			'pusername'		=>	$pusername,
-						'agep'			=>	$agep,
-						'occupation'	=>	$occupation,
-						'gender'		=>	$gender,
-						'maritalstatus'	=>	$maritalstatus,
-						'scholarship'	=>	$scholarship,
-						'updated_at'	=>	$updated_at,
-	        			]
-
-	        		)
-
-        		
-        	</div>
-    	</div>
     </section>
 @endif
     <script type="text/javascript">
