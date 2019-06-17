9a7c7659dede5878ec47f46908a2d9aa08c0f5ec
Hugo Hernández <hugo@doitcloud.consulting>
2019-06-17 03:00:36 -0500

    s

--- resources/views/tabs/tabsviewpatient.blade.php
+++ resources/views/tabs/tabsviewpatient.blade.php
@@ -37,13 +37,13 @@
 						 	<div class="input-group">
 						    	<div class="input-group-btn">
 						        	<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-						        		Action <span class="caret"></span>
+						        		Modo <span class="caret"></span>
 						        	</button>
 						        	<ul class="dropdown-menu">
-						          		<li><a href="#">Action</a></li>
-						          		<li><a href="#">Another action</a></li>
-						          		<li><a href="#">Something else here</a></li>
-						          		<li><a href="#">Separated link</a></li>
+						          		<li><a href="#">Oral</a></li>
+						          		<li><a href="#">Axilar</a></li>
+						          		<li><a href="#">Rectal</a></li>
+						          		<li><a href="#">Frontal</a></li>
 						        	</ul>
 						      	</div>
 					      	</div>
