3fb6dd6ee69557eb68a23e6fe3ebbfc33a0c17e5
Hugo Hernández <hugo@doitcloud.consulting>
2019-10-01 02:38:19 -0500

    s

--- resources/views/empty/emptyData.blade.php
+++ resources/views/empty/emptyData.blade.php
@@ -7,7 +7,7 @@
 
 		<div align="center" class="col-sm-12 enun">
 			        <img src="{{ asset(config($icon)) }}" height="180" width="180" id="imgEmpty">
-			        <h3>OMG!</h3>
+			        <h3>Nada que mostrar!</h3>
 				      <h4> <span class="spanEmpty1"> No tienes {{ $title }} agregados </span>
 				   @if($emptyc != "not_buttom")
 				    <span class="spanEmpty">, <br> podrías generar tu primer registro aquí</span></h4><br>
